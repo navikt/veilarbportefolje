817156381350c89031d3f5a8daba072398d9fea9
Johan Rusvik <johan.rusvik@nav.no>
2016-12-15 13:32:07 +0100

    Fjerner ubrukt import

--- web/src/main/java/no/nav/sbl/fo/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/sbl/fo/config/ApplicationConfig.java
@@ -5,7 +5,6 @@ import no.nav.sbl.fo.internal.IsAliveServlet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 
