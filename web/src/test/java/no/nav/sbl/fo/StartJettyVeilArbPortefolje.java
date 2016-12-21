6e0f66ab61019e17fba55c3ca7271cfb7bfda69f
Christian Finstad <christian.finstad@nav.no>
2016-12-21 12:38:44 +0100

    Tar i bruk eldre versjon av jetty og legger på innlogging når appen kjøres opp lokalt

--- web/src/test/java/no/nav/sbl/fo/StartJettyVeilArbPortefolje.java
+++ web/src/test/java/no/nav/sbl/fo/StartJettyVeilArbPortefolje.java
@@ -1,10 +1,18 @@
 package no.nav.sbl.fo;
 
+import no.nav.modig.security.loginmodule.DummyRole;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
+import org.eclipse.jetty.jaas.JAASLoginService;
 
+import java.io.File;
+
+import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
+import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
+import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
+import static no.nav.modig.lang.collections.RunnableUtils.first;
+import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
-import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
 
 public class StartJettyVeilArbPortefolje {
 
@@ -13,12 +21,24 @@ public class StartJettyVeilArbPortefolje {
         setupKeyAndTrustStore();
 
         //Må ha https for csrf-token
-        final Jetty jetty = Jetty.usingWar()
-                .at("Portefølje-serverside")
+        final Jetty jetty = Jetty.usingWar(WEBAPP_SOURCE)
+                .at("portefolje")
                 .sslPort(9594)
                 .port(9595)
-                .overrideWebXml()
+                .overrideWebXml(new File(TEST_RESOURCES,"override-web.xml" ))
+                .withLoginService(createLoginService())
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
+
+
+
+    public static JAASLoginService createLoginService() {
+        JAASLoginService jaasLoginService = new JAASLoginService("Simple Login Realm");
+        jaasLoginService.setLoginModuleName("simplelogin");
+        jaasLoginService.setRoleClassNames(new String[]{DummyRole.class.getName()});
+        return jaasLoginService;
+    }
+
+
 }
