a33048b8b2ac0ef8db5c47d09500658573b82e08
Johan Rusvik <johan.rusvik@nav.no>
2017-01-16 14:46:36 +0100

    Legger til pingable for solrserver

--- web/src/main/java/no/nav/fo/config/SolrConfig.java
+++ web/src/main/java/no/nav/fo/config/SolrConfig.java
@@ -1,6 +1,7 @@
 package no.nav.fo.config;
 
 import no.nav.fo.service.SolrService;
+import no.nav.sbl.dialogarena.types.Pingable;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +17,18 @@ public class SolrConfig {
     @Bean
     public SolrService solrService() {
         return new SolrService();
+    }
+
+    @Bean
+    public Pingable solrServerPing() {
+        HttpSolrServer server = new HttpSolrServer(System.getProperty("no.nav.fo.brukercore.url"));
+        return () -> {
+            try {
+                server.ping();
+                return Pingable.Ping.lyktes("SolrServer");
+            } catch (Exception e) {
+                return Pingable.Ping.feilet("Feilet", e);
+            }
+        };
     }
 }
