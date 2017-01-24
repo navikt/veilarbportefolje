72dad54c92745b19e91ef4b08d8be572bcd38dd0
Johan Rusvik <johan.rusvik@nav.no>
2017-01-24 15:54:10 +0100

    Endrer path

--- web/src/main/java/no/nav/fo/provider/rest/RestConfig.java
+++ web/src/main/java/no/nav/fo/provider/rest/RestConfig.java
@@ -11,7 +11,7 @@ public class RestConfig extends ResourceConfig {
         super(
                 DateTimeObjectMapperProvider.class,
                 JSLoggerController.class,
-                HentPortefoljeForEnhetController.class,
+                EnhetController.class,
                 SolrController.class
         );
     }
