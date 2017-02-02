fbd859b1621f2c1ac3265890e97a4bd3c2036686
Christian Finstad <christian.finstad@nav.no>
2017-02-02 14:50:44 +0100

    Leser meldinger fra kø

--- web/src/test/java/no/nav/fo/config/LocalApplicationConfig.java
+++ web/src/test/java/no/nav/fo/config/LocalApplicationConfig.java
@@ -1,20 +1,21 @@
 package no.nav.fo.config;
 
+import no.nav.fo.consumer.EndringAvVeilederListener;
 import no.nav.fo.internal.IsAliveServlet;
 import no.nav.fo.service.ServiceConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @Configuration
 @Import({
+        EndringAvVeilederListener.class,
         DatabaseConfig.class,
         VirksomhetEnhetEndpointConfig.class,
         ServiceConfig.class,
         SolrConfig.class,
-        PortefoljeMock.class
+        PortefoljeMock.class,
+        MQMockConfig.class
 })
 public class LocalApplicationConfig {
     @Bean
