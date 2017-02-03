eb0b3cc21e09ca0a1a2badae964b4fb3851f95de
Peder Korsveien <peder.korsveien@nav.no>
2017-02-03 13:33:42 +0100

    Legg til flere felter i Bruker-test

--- web/src/test/java/no/nav/fo/domene/BrukerTest.java
+++ web/src/test/java/no/nav/fo/domene/BrukerTest.java
@@ -14,8 +14,10 @@ public class BrukerTest {
         document.addField("fornavn", "Rudolf");
         document.addField("etternavn", "Blodstrupmoen");
         document.addField("veileder_id", "XXXXXX");
-        document.addField("egen_ansatt", "false");
+        document.addField("egen_ansatt", false);
         document.addField("sikkerhetstiltak", "foo");
+        document.addField("er_doed", false);
+        document.addField("diskresjonskode", "6");
 
         Bruker bruker = Bruker.of(document);
         assertThat(bruker.getFodselsnr(), notNullValue());
@@ -24,5 +26,7 @@ public class BrukerTest {
         assertThat(bruker.getVeilderId(), notNullValue());
         assertThat(bruker.getEgenAnsatt(), notNullValue());
         assertThat(bruker.getSikkerhetstiltak(), notNullValue());
+        assertThat(bruker.getDiskresjonskode(), notNullValue());
+        assertThat(bruker.getErDoed(), notNullValue());
     }
 }
