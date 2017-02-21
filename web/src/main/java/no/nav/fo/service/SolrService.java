1c395b0eb30789f9fbe6e5cd3c19d1b795fdaba0
Peder Korsveien <peder.korsveien@nav.no>
2017-02-21 14:46:59 +0100

    [kaizen] Logg tid brukt på indeksering

--- web/src/main/java/no/nav/fo/service/SolrService.java
+++ web/src/main/java/no/nav/fo/service/SolrService.java
@@ -18,6 +18,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,6 +29,9 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class SolrService {
 
     private static final Logger logger = getLogger(SolrService.class);
+
+    private static final String HOVEDINDEKSERING = "Hovedindeksering";
+    private static final String DELTAINDEKSERING = "Deltaindeksering";
 
     @Inject
     private HttpSolrServer server;
@@ -43,16 +47,15 @@ public class SolrService {
         }
 
         logger.info("Starter hovedindeksering");
-        Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+        LocalDateTime t0 = LocalDateTime.now();
 
         List<SolrInputDocument> dokumenter = brukerRepository.retrieveAlleBrukere();
         deleteAllDocuments();
         addDocuments(dokumenter);
         commit();
-        brukerRepository.updateTidsstempel(timestamp);
+        brukerRepository.updateTidsstempel(Timestamp.valueOf(t0));
 
-        logger.info("Hovedindeksering fullført!");
-        logger.info(dokumenter.size() + " dokumenter ble lagt til i solrindeksen");
+        logFerdig(t0, dokumenter.size(), HOVEDINDEKSERING);
     }
 
 
@@ -64,7 +67,8 @@ public class SolrService {
         }
 
         logger.info("Starter deltaindeksering");
-        Timestamp timestamp = Timestamp.valueOf(LocalDateTime.now());
+        LocalDateTime t0 = LocalDateTime.now();
+        Timestamp timestamp = Timestamp.valueOf(t0);
 
         java.util.List<Map<String, Object>> rader = brukerRepository.retrieveOppdaterteBrukere();
         if (rader.isEmpty()) {
@@ -77,8 +81,7 @@ public class SolrService {
         brukerRepository.updateTidsstempel(timestamp);
         commit();
 
-        logger.info("Deltaindeksering fullført!");
-        logger.info(dokumenter.size() + " dokumenter ble oppdatert/lagt til i solrindeksen");
+        logFerdig(t0, dokumenter.size(), DELTAINDEKSERING);
     }
 
     public List<Bruker> hentBrukere(String enhetId, String sortOrder) {
@@ -145,6 +148,15 @@ public class SolrService {
         if (statusCode != 0) {
             throw new SolrUpdateResponseCodeException(String.format("Solr returnerte med statuskode %s", statusCode));
         }
+    }
+
+    private void logFerdig(LocalDateTime t0, int antall, String indekseringstype) {
+        Duration duration = Duration.between(t0, LocalDateTime.now());
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes();
+        long seconds = duration.getSeconds();
+        String logString = String.format("%s fullført! | Tid brukt(hh:mm:ss): %02d:%02d:%02d | Dokumenter oppdatert: %d", indekseringstype, hours, minutes, seconds, antall);
+        logger.info(logString);
     }
 
 }
