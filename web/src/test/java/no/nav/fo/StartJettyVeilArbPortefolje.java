7a37e745db18f70d50518f6a8d192c3eae8bf259
Christian Finstad <christian.finstad@nav.no>
2017-02-22 08:37:34 +0100

    Legger til broker i startJetty

--- web/src/test/java/no/nav/fo/StartJettyVeilArbPortefolje.java
+++ web/src/test/java/no/nav/fo/StartJettyVeilArbPortefolje.java
@@ -3,6 +3,7 @@ package no.nav.fo;
 import no.nav.fo.security.jwt.context.JettySubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
+import org.apache.activemq.broker.BrokerService;
 import org.eclipse.jetty.jaas.JAASLoginService;
 
 
@@ -15,13 +16,20 @@ import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustSto
 
 public class StartJettyVeilArbPortefolje {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SystemProperties.setFrom("veilarbportefolje.properties");
         setupKeyAndTrustStore();
         setupJndiLocalContext();
         setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", JettySubjectHandler.class.getName());
         JAASLoginService jaasLoginService = new JAASLoginService("JWT Realm");
         jaasLoginService.setLoginModuleName("jwtLogin");
+
+        final BrokerService broker = new BrokerService();
+        broker.getSystemUsage().getTempUsage().setLimit(100 * 1024 * 1024 * 100);
+        broker.getSystemUsage().getStoreUsage().setLimit(100 * 1024 * 1024 * 100);
+        broker.addConnector("tcp://localhost:61616");
+        broker.start();
+
 
         //Må ha https for csrf-token
         final Jetty jetty = Jetty.usingWar()
@@ -33,4 +41,5 @@ public class StartJettyVeilArbPortefolje {
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
+
 }
