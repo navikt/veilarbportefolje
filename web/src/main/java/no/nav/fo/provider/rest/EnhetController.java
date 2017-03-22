8f2b5bb44a69555b2ace360e387c8fc6eb19da5c
Johan Rusvik <johan.rusvik@nav.no>
2017-03-22 13:52:10 +0100

    Endrer variabelnavn ihht. instansieringen

--- web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
+++ web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
@@ -60,9 +60,9 @@ public class EnhetController {
 
                 List<Bruker> brukere = solrService.hentBrukereForEnhet(enhet, sortDirection, filtervalg);
                 List<Bruker> brukereSublist = PortefoljeUtils.getSublist(brukere, fra, antall);
-                List<Bruker> filtrertBrukereSublist = PortefoljeUtils.sensurerBrukere(brukereSublist,ident, pepClient);
+                List<Bruker> sensurerteBrukereSublist = PortefoljeUtils.sensurerBrukere(brukereSublist,ident, pepClient);
 
-                Portefolje portefolje = PortefoljeUtils.buildPortefolje(brukere, filtrertBrukereSublist, enhet, fra);
+                Portefolje portefolje = PortefoljeUtils.buildPortefolje(brukere, sensurerteBrukereSublist, enhet, fra);
 
                 return Response.ok().entity(portefolje).build();
             } else {
