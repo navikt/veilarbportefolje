2e043c7f23f400b99f7954c921dbeec98fc237b6
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-04-08 10:13:03 +0200

    Bytter til InternbrukerSubjectHandler

--- web/src/test/java/no/nav/fo/StartJettyVeilArbPortefolje.java
+++ web/src/test/java/no/nav/fo/StartJettyVeilArbPortefolje.java
@@ -1,5 +1,6 @@
 package no.nav.fo;
 
+import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
 import no.nav.brukerdialog.security.context.JettySubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
@@ -21,13 +22,17 @@ public class StartJettyVeilArbPortefolje {
         SystemProperties.setFrom("veilarbportefolje.properties");
         setupJndiLocalContext();
         System.setProperty("develop-local", "true");
-        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", JettySubjectHandler.class.getName());
+
+        InternbrukerSubjectHandler.setVeilederIdent("***REMOVED***");
+        InternbrukerSubjectHandler.setServicebruker("srvveilarbveileder");
+        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
+
         System.setProperty("org.apache.geronimo.jaspic.configurationFile", "src/test/resources/jaspiconf.xml");
         Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
 
 //        final BrokerService broker = new BrokerService();
 //        broker.getSystemUsage().getTempUsage().setLimit(100 * 1024 * 1024 * 100);
-//        broker.getSystemUsage().getStoreUsage().setLimit(100 * 1024 * 1024 * 100);
+//        broker.getSystemUsage().getStoreUsage().setLimit(100 * 1024 * 1024 N* 100);
 //        broker.addConnector("tcp://localhost:61616");
 //        broker.start();
 
@@ -38,7 +43,6 @@ public class StartJettyVeilArbPortefolje {
                 .sslPort(9594)
                 .port(9595)
                 .overrideWebXml()
-                .configureForJaspic()
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
