499cf3c34da230c0b454868d33e1ad27737c76c3
Nicklas Utgaard <nicklas.utgaard@nav.no>
2017-04-12 12:22:34 +0200

    Lager dbms scheduler for sync

--- config/src/main/resources/manuellSetupDB.sql
+++ config/src/main/resources/manuellSetupDB.sql
@@ -12,5 +12,31 @@ CREATE OR REPLACE PROCEDURE REFRESH_MV AS
 CREATE OR REPLACE TRIGGER restartmateralizedviews
 AFTER STARTUP ON DATABASE
   BEGIN
-    REFRESH_MV();
-  END;
+    DECLARE
+      numberOfJobs number;
+    BEGIN
+      SELECT COUNT(*) INTO numberOfJobs FROM USER_SCHEDULER_JOBS WHERE JOB_NAME = 'REFRESH_MV_JOB';
+
+      if numberOfJobs = 0 then
+        dbms_scheduler.create_schedule(
+            schedule_name => 'REFRESH_MV_POLLER',
+            start_date => trunc(sysdate)+18/24,
+            repeat_interval => 'freq=MINUTELY;interval=1',
+            comments => 'Kjorer hvert minutt for a refreshe mview mot arena'
+        );
+        dbms_scheduler.create_program(
+            program_name => 'REFRESH_MV_PROGRAM',
+            program_type => 'STORED_PROCEDURE',
+            program_action => 'REFRESH_MV',
+            enabled => true
+        );
+        dbms_scheduler.create_job(
+            job_name => 'REFRESH_MV_JOB',
+            program_name => 'REFRESH_MV_PROGRAM',
+            schedule_name => 'REFRESH_MV_POLLER',
+            enabled => true,
+            AUTO_DROP => FALSE
+        );
+      end if;
+    END;
+  END restartmateralizedviews;/
