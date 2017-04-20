087b1e6948a415e25fae890fae070356c6c4c5a8
Christian Finstad <christian.finstad@nav.no>
2017-04-20 12:19:27 +0200

    Legger til isso i selftesten.

--- web/src/main/java/no/nav/fo/internal/PingConfig.java
+++ web/src/main/java/no/nav/fo/internal/PingConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Configuration
 public class PingConfig {
@@ -25,4 +28,21 @@ public class PingConfig {
             }
         };
     }
+
+    @Bean
+    public Pingable portefoljePing() throws IOException {
+        return () -> {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(System.getProperty("isso.isalive.url")).openConnection();
+                connection.connect();
+                if (connection.getResponseCode() == 200) {
+                    return Pingable.Ping.lyktes("ISSO");
+                }
+                return Pingable.Ping.feilet("ISSO", new Exception("Statuskode: " + connection.getResponseCode()));
+            } catch (Exception e) {
+                return Pingable.Ping.feilet("ISSO", e);
+            }
+        };
+    }
+
 }
