126eb77b3f5b8b0070a18b04fa9d7881c900e806
Peder Korsveien <peder.korsveien@nav.no>
2017-05-08 08:57:30 +0200

    Sjekk responskode fra produsent

--- web/src/main/java/no/nav/fo/consumer/feed/FeedClient.java
+++ web/src/main/java/no/nav/fo/consumer/feed/FeedClient.java
@@ -1,12 +1,10 @@
 package no.nav.fo.consumer.feed;
 
 import javaslang.control.Try;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -23,13 +21,25 @@ public class FeedClient {
     @Value("${feed.consumer.path.callback}")
     private String feedConsumerPathCallback;
 
+    @Scheduled(cron="${feed.consumer.pollingrate.cron}")
     public void registerWebhook() {
         String json = String.format("\"callbackUrl\": \"%s\"", feedConsumerPathCallback);
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder().url(feedProducerPathWebhook).post(body).build();
 
-        Try.of(() -> client.newCall(request).execute())
-                .onFailure(e -> LOG.warn("Kunne ikke oprette webhook. Prøv igjen senere"));
+        Try.of(() -> {
+            Response response = client.newCall(request).execute();
+            if (response.code() == 201) {
+                LOG.info("Webhook opprettet hos produsent!");
+            } else if (isNotSuccessful(response)) {
+                LOG.warn("Produsent returnerte feilkode ", response.code());
+            }
+            return response;
+        }).onFailure(e -> LOG.warn("Kunne ikke opprette webhook. Prøv igjen senere"));
+    }
+
+    private boolean isNotSuccessful(Response response) {
+        return !response.isSuccessful();
     }
 }
