456735ec199a443db80899ed98a153a40c2248f5
Torstein Gjengedal <torstein.gjengedal@nav.no>
2017-07-04 15:33:51 +0200

    Bruker Set.contains() i stedet for gjentatte equals

--- web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
+++ web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
@@ -1,12 +1,17 @@
 package no.nav.fo.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UnderOppfolgingRegler {
 
+    private static final Set<String> IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER = new HashSet<>(Arrays.asList(
+            "BKART", "IVURD", "KAP11", "VARIG", "VURDI"));
+    
     public static boolean erUnderOppfolging(String formidlingsgruppekode, String servicegruppekode) {
         return !(formidlingsgruppekode.equals("ISERV") ||
-                (formidlingsgruppekode.equals("IARBS") && (servicegruppekode.equals("BKART")
-                        || servicegruppekode.equals("IVURD") || servicegruppekode.equals("KAP11")
-                        || servicegruppekode.equals("VARIG") || servicegruppekode.equals("VURDI"))));
+                (formidlingsgruppekode.equals("IARBS") && IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER.contains(servicegruppekode)));
     }
 
 }
