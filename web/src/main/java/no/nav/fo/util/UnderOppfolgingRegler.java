8304455170f5ae946acd342b332f8a5f09d74ee8
Torstein Gjengedal <torstein.gjengedal@nav.no>
2017-07-05 10:38:32 +0200

    Legger til tester av oppfølgingslogikk for alle kombinasjoner av statuser. Endrer defaultoppførsel ved null-verdier

--- web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
+++ web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
@@ -1,5 +1,8 @@
 package no.nav.fo.util;
 
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,8 +15,10 @@ public class UnderOppfolgingRegler {
     // Logikken som utleder om en bruker er under oppfolging kjøres også i VeilArbSituasjon.
     // Endringer i logikken må implementeres begge steder
     public static boolean erUnderOppfolging(String formidlingsgruppekode, String servicegruppekode) {
-        return !(formidlingsgruppekode.equals("ISERV") ||
-                (formidlingsgruppekode.equals("IARBS") && IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER.contains(servicegruppekode)));
+        return isNotEmpty(formidlingsgruppekode) &&
+                !(formidlingsgruppekode.equals("ISERV") ||
+                (formidlingsgruppekode.equals("IARBS") && (isEmpty(servicegruppekode) 
+                        || IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER.contains(servicegruppekode))));
     }
 
 }
--- web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
+++ web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
@@ -1,7 +1,8 @@
 package no.nav.fo.util;
 
 import static java.util.Arrays.asList;
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static no.nav.fo.util.UnderOppfolgingRegler.erUnderOppfolging;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,9 +17,60 @@ public class UnderOppfolgingReglerTest {
 
     @Test
     public void skalVareOppfolgningsbrukerPgaArenaStatus() throws Exception {
-        assertThat(UnderOppfolgingRegler.erUnderOppfolging("IARBS", "BATT")).isTrue();
+        assertThat(erUnderOppfolging("IARBS", "BATT")).isTrue();
     }
+
+    @Test
+    public void erUnderOppfolging_default_false(){
+        assertThat(erUnderOppfolging(null, null)).isFalse();
+    }
+
+    @Test
+    public void erUnderOppfolging_ARBS_true() {
+        alleKombinasjonerErTrue("ARBS");
+    }
+
+    private void alleKombinasjonerErTrue(String formidlingsgruppeKode) {
+        assertThat(erUnderOppfolging(formidlingsgruppeKode, null)).isTrue();
+        for (String kgKode : KVALIFISERINGSGRUPPEKODER) {
+            assertThat(erUnderOppfolging(formidlingsgruppeKode, kgKode)).isTrue();
+        }
+    }
+
+    @Test
+    public void erUnderOppfolging_PARBS_true() {
+        alleKombinasjonerErTrue("PARBS");
+    }
+
+    @Test
+    public void erUnderOppfolging_RARBS_true() {
+        alleKombinasjonerErTrue("RARBS");
+    }    
     
+    @Test
+    public void erUnderOppfolging_ISERV_false() {
+        assertThat(erUnderOppfolging("ISERV", null)).isFalse();
+        for (String kgKode : KVALIFISERINGSGRUPPEKODER) {
+            assertThat(erUnderOppfolging("ISERV", kgKode)).isFalse();
+        }
+    }    
+    
+    @Test
+    public void erUnderOppfolging_IARBS_true_for_BATT_BFORM_IKVAL_VURDU_OPPFI() {
+        for (String kgKode : asList("BATT", "IKVAL", "VURDU", "OPPFI", "BFORM")) {
+            assertThat(erUnderOppfolging("IARBS", kgKode)).isTrue();
+        }
+    }
+
+    @Test
+    public void erUnderOppfolging_IARBS_False_for_KAP11_IVURD_VURDI_VARIG_BKART() {
+        assertThat(erUnderOppfolging("IARBS", null)).isFalse();
+        for (String kgKode : asList("KAP11", "IVURD", "VURDI", "VARIG", "BKART")) {
+            assertThat(erUnderOppfolging("IARBS", kgKode)).isFalse();
+        }
+    }
+
+
     @Test
     public void sjekkUnderOppfolging_AlleKombinasjoner() {
         for (String fgKode : FORMIDLINGSGRUPPEKODER) {
