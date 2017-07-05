684b9993861ae3f61d3554c4f2e4b3e3011afa64
Torstein Gjengedal <torstein.gjengedal@nav.no>
2017-07-05 09:14:32 +0200

    Sjekker utfall for alle kombinasjoner av formidlings- og kval.gruppekode

--- web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
+++ web/src/main/java/no/nav/fo/util/UnderOppfolgingRegler.java
@@ -9,6 +9,8 @@ public class UnderOppfolgingRegler {
     private static final Set<String> IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER = new HashSet<>(Arrays.asList(
             "BKART", "IVURD", "KAP11", "VARIG", "VURDI"));
     
+    // Logikken som utleder om en bruker er under oppfolging kjøres også i VeilArbSituasjon.
+    // Endringer i logikken må implementeres begge steder
     public static boolean erUnderOppfolging(String formidlingsgruppekode, String servicegruppekode) {
         return !(formidlingsgruppekode.equals("ISERV") ||
                 (formidlingsgruppekode.equals("IARBS") && IKKE_UNDER_OPPFOLGING_SERVICEGRUPPEKODER.contains(servicegruppekode)));
--- web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
+++ web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
@@ -1,13 +1,31 @@
 package no.nav.fo.util;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class UnderOppfolgingReglerTest {
 
+    private static final Set<String> FORMIDLINGSGRUPPEKODER = new HashSet<>(asList("ISERV", "IARBS", "RARBS", "ARBS", "PARBS"));
+    private static final Set<String> KVALIFISERINGSGRUPPEKODER = new HashSet<>(
+            asList("BATT", "KAP11", "IKVAL", "IVURD", "VURDU", "VURDI", "VARIG", "OPPFI", "BKART", "BFORM"));
+
     @Test
     public void skalVareOppfolgningsbrukerPgaArenaStatus() throws Exception {
         assertThat(UnderOppfolgingRegler.erUnderOppfolging("IARBS", "BATT")).isTrue();
+    }
+    
+    @Test
+    public void sjekkUnderOppfolging_AlleKombinasjoner() {
+        for (String fgKode : FORMIDLINGSGRUPPEKODER) {
+            for (String kgKode : KVALIFISERINGSGRUPPEKODER) {
+                System.out.println(String.format("[%s] [%s] - [%s]", fgKode, kgKode, UnderOppfolgingRegler.erUnderOppfolging(fgKode, kgKode)));
+            }
+            System.out.println("-------");
+        }
     }
 }
