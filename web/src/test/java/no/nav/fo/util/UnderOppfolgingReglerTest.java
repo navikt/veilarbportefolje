f6024a5ec1a8db3db2b1c0b8c5b0dbe46b820a8c
Torstein Gjengedal <torstein.gjengedal@nav.no>
2017-07-06 13:57:08 +0200

    Fjernet ubrukt test som sjekker alle kombinasjoner

--- web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
+++ web/src/test/java/no/nav/fo/util/UnderOppfolgingReglerTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 
 public class UnderOppfolgingReglerTest {
 
-    private static final Set<String> FORMIDLINGSGRUPPEKODER = new HashSet<>(asList("ISERV", "IARBS", "RARBS", "ARBS", "PARBS"));
     private static final Set<String> KVALIFISERINGSGRUPPEKODER = new HashSet<>(
             asList("BATT", "KAP11", "IKVAL", "IVURD", "VURDU", "VURDI", "VARIG", "OPPFI", "BKART", "BFORM"));
 
@@ -70,14 +69,4 @@ public class UnderOppfolgingReglerTest {
         }
     }
 
-
-//    @Test
-    public void sjekkUnderOppfolging_AlleKombinasjoner() {
-        for (String fgKode : FORMIDLINGSGRUPPEKODER) {
-            for (String kgKode : KVALIFISERINGSGRUPPEKODER) {
-                System.out.println(String.format("[%s] [%s] - [%s]", fgKode, kgKode, erUnderOppfolging(fgKode, kgKode)));
-            }
-            System.out.println("-------");
-        }
-    }
 }
