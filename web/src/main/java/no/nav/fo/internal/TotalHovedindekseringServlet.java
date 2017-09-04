d679f6fe72d44f3ed858ff5a57f6db05d76cc56e
Christian Finstad <christian.finstad@nav.no>
2017-09-04 19:40:29 +0200

    Revert: @Component slik at scheduled blir plukket opp

--- web/src/main/java/no/nav/fo/internal/TotalHovedindekseringServlet.java
+++ web/src/main/java/no/nav/fo/internal/TotalHovedindekseringServlet.java
@@ -6,7 +6,6 @@ import no.nav.fo.service.SolrService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletException;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
 public class TotalHovedindekseringServlet extends HttpServlet {
     private static Logger logger = LoggerFactory.getLogger(TotalHovedindekseringServlet.class);
 
