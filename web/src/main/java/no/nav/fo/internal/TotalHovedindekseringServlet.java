c488d6f9c6cc6d76ab271342d870c02164a71153
Christian Finstad <christian.finstad@nav.no>
2017-09-05 08:48:41 +0200

    [BUG] Må kalle hovedindeksering i servleten

--- web/src/main/java/no/nav/fo/internal/TotalHovedindekseringServlet.java
+++ web/src/main/java/no/nav/fo/internal/TotalHovedindekseringServlet.java
@@ -21,7 +21,7 @@ public class TotalHovedindekseringServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        hovedindekseringScheduler.prosessScheduler();
     }
 
 }
