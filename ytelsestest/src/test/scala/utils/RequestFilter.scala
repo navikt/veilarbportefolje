5d13a2cc5017ec3735fe46b75f944da76bb9e9c2
Hagen <H134988@ADEO.NO>
2017-10-05 11:05:07 +0200

    Endret ytelsestest for ny release. Fjernet test mot personoversikt og akt.plan. Lagt til flere tester mot portefolje-filtre.

--- ytelsestest/src/test/scala/utils/RequestFilter.scala
+++ ytelsestest/src/test/scala/utils/RequestFilter.scala
@@ -1,15 +1,16 @@
 package utils
 
 case class RequestFilter(
-    alder: List[String] = List(),
+    aktiviteter: AktivitetRequestFilter = AktivitetRequestFilter(),
+    alder: List[String] = List (),
     brukerstatus: String = "NYE_BRUKERE",
-    fodselsdagIMnd: List[String] = List(),
-    formidlingsgruppe: List[String] = List(),
-    innsatsgruppe: List[String] = List(),
-    kjonn: List[String] = List(),
-    rettighetsgruppe: List[String] = List(),
-    servicegruppe: List[String] = List(),
-    veiledere: List[String] = List(),
+    fodselsdagIMnd: List[String] = List (),
+    formidlingsgruppe: List[String] = List (),
+    innsatsgruppe: List[String] = List (),
+    kjonn: List[String] = List (),
+    rettighetsgruppe: List[String] = List (),
+    servicegruppe: List[String] = List (),
+    veiledere: List[String] = List (),
     ytelse: String = null
 )
 
