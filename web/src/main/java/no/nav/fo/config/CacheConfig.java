cfad8623a8bf2636b4a648acc680d388fef6966d
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2017-10-10 10:13:46 +0200

    Opprydding

--- web/src/main/java/no/nav/fo/config/CacheConfig.java
+++ web/src/main/java/no/nav/fo/config/CacheConfig.java
@@ -8,8 +8,7 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.System.getProperty;
+import static net.sf.ehcache.store.MemoryStoreEvictionPolicy.LRU;
 import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CACHE;
 
 
@@ -17,15 +16,12 @@ import static no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext.ABAC_CA
 @EnableCaching
 public class CacheConfig {
 
-    private static final String CACHE_TTL_PROPERTY = "veilarbportefolje.tilgangtilbrukercache.seconds";
-    private static final int CACHE_TIME_TO_LIVE = parseInt(getProperty(CACHE_TTL_PROPERTY, "3600"));
-
     public static final String TILGANG_TIL_ENHET = "tilgangTilEnhet";
     private static final CacheConfiguration TILGANG_TIL_ENHET_CACHE =
             new CacheConfiguration(TILGANG_TIL_ENHET, 5000)
-                    .memoryStoreEvictionPolicy("LRU")
+                    .memoryStoreEvictionPolicy(LRU)
                     .timeToIdleSeconds(3600)
-                    .timeToLiveSeconds(CACHE_TIME_TO_LIVE);
+                    .timeToLiveSeconds(3600);
 
     @Bean
     public CacheManager cacheManager() {
