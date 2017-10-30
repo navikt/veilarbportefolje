6772599d3437d52a95b2a8e2d99fb3b4217515d9
Christian Finstad <christian.finstad@nav.no>
2017-10-30 10:49:30 +0100

    Skriver om feilende testmigrering hsql syntax

--- web/src/test/resources/testmigration/V2_7__renamesituasjon.sql
+++ web/src/test/resources/testmigration/V2_7__renamesituasjon.sql
@@ -1,1 +1,1 @@
-ALTER TABLE METADATA RENAME COLUMN situasjon_sist_oppdatert TO oppfolging_sist_oppdatert;
+ALTER TABLE METADATA ALTER COLUMN situasjon_sist_oppdatert RENAME TO oppfolging_sist_oppdatert;
