6c5d1ba433a178e07a0d5ffc75a3339278a72be2
Christian Finstad <christian.finstad@nav.no>
2017-11-03 11:03:44 +0100

    Må bruker Timestamp ved oppdatering av databasen

--- web/src/main/java/no/nav/fo/feed/DialogFeedRepository.java
+++ web/src/main/java/no/nav/fo/feed/DialogFeedRepository.java
@@ -6,6 +6,10 @@ import no.nav.fo.util.sql.SqlUtils;
 import no.nav.fo.util.sql.where.WhereClause;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
 import static no.nav.fo.database.BrukerRepository.BRUKERDATA;
 
 public class DialogFeedRepository {
@@ -17,10 +21,17 @@ public class DialogFeedRepository {
 
     public void upsertDialogdata(DialogDataFraFeed dialog, PersonId personId) {
         SqlUtils.upsert(db, BRUKERDATA)
-                .set("VENTERPASVARFRABRUKER", dialog.getTidspunktEldsteVentende())
-                .set("VENTERPASVARFRANAV", dialog.getTidspunktEldsteUbehandlede())
+                .set("VENTERPASVARFRABRUKER", toTimestamp(dialog.getTidspunktEldsteVentende()))
+                .set("VENTERPASVARFRANAV", toTimestamp(dialog.getTidspunktEldsteUbehandlede()))
                 .set("AKTOERID", dialog.getAktorId())
                 .set("PERSONID", personId.toString())
                 .where(WhereClause.equals("PERSONID", personId.toString())).execute();
+    }
+
+    private Timestamp toTimestamp(Date date) {
+        if(Objects.isNull(date)) {
+            return null;
+        }
+        return new Timestamp(date.toInstant().toEpochMilli());
     }
 }
