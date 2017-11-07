d8a19929ed61506566f992b5a7daa62d446b1fe5
Christian Finstad <christian.finstad@nav.no>
2017-11-07 16:48:22 +0100

    Tvinger brukere uten veileder å havne først

--- web/src/main/java/no/nav/fo/util/SolrSortUtils.java
+++ web/src/main/java/no/nav/fo/util/SolrSortUtils.java
@@ -16,6 +16,7 @@ import static no.nav.fo.util.AktivitetUtils.addPrefixForAktivitetUtlopsdato;
 public class SolrSortUtils {
 
     static SolrQuery addSort(SolrQuery solrQuery, String sortOrder, String sortField, Filtervalg filtervalg) {
+        solrQuery.addSort("exists(veileder_id)", SolrQuery.ORDER.asc);
         SolrQuery.ORDER order = "ascending".equals(sortOrder) ? SolrQuery.ORDER.asc : SolrQuery.ORDER.desc;
         if("ikke_satt".equals(sortField)) {
             return solrQuery.addSort("person_id", SolrQuery.ORDER.asc);
