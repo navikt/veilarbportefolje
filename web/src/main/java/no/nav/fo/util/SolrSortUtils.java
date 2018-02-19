9356317819a4940903c751f1e0f4509a2c1b1686
alexander-svendsen <alexander.svendsen@nav.no>
2018-02-19 13:07:45 +0100

    FO-211: Fiks skriveleif

--- web/src/main/java/no/nav/fo/util/SolrSortUtils.java
+++ web/src/main/java/no/nav/fo/util/SolrSortUtils.java
@@ -13,15 +13,15 @@ import static no.nav.fo.util.AktivitetUtils.addPrefixForAktivitetUtlopsdato;
 
 public class SolrSortUtils {
 
-    private static final String NY_FOR_ENEHET = "exists(veileder_id)";
-//    private static final String NY_FOR_ENEHET = "ny_for_enhet"; //TODO use me
+    private static final String NY_FOR_ENHET = "exists(veileder_id)";
+//    private static final String NY_FOR_ENHET = "ny_for_enhet"; //TODO use me
 
     static SolrQuery addSort(SolrQuery solrQuery, boolean minoversikt, String sortOrder, String sortField, Filtervalg filtervalg) {
 
         if(minoversikt){
             solrQuery.addSort("ny_for_veileder", SolrQuery.ORDER.desc);
         } else {
-            solrQuery.addSort(NY_FOR_ENEHET, SolrQuery.ORDER.asc);
+            solrQuery.addSort(NY_FOR_ENHET, SolrQuery.ORDER.asc);
         }
         SolrQuery.ORDER order = "ascending".equals(sortOrder) ? SolrQuery.ORDER.asc : SolrQuery.ORDER.desc;
         if("ikke_satt".equals(sortField)) {
