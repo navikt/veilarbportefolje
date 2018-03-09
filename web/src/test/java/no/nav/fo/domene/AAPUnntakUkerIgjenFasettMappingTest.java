7a93a12ac17d561f2bc922acc972495cd130ceeb
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-03-09 14:17:56 +0100

    [FO-302] REVERT Endret fasetter for AAPMaxtid og AAPUnntak
    This reverts commit ce4adde6ca39948c6a28c8b3656aab6605b79483.

--- web/src/test/java/no/nav/fo/domene/AAPUnntakUkerIgjenFasettMappingTest.java
+++ web/src/test/java/no/nav/fo/domene/AAPUnntakUkerIgjenFasettMappingTest.java
@@ -11,19 +11,35 @@ class AAPUnntakUkerIgjenFasettMappingTest {
 
     @Test
     void finnUkemapping() {
-        assertEquals(UKE_UNDER2, finnUkeMapping(-1).get());
-        assertEquals(UKE_UNDER2, finnUkeMapping(0).get());
-        assertEquals(UKE_UNDER2, finnUkeMapping(5*2-1).get());
+        assertEquals(UKE_UNDER12, finnUkeMapping(-1).get());
+        assertEquals(UKE_UNDER12, finnUkeMapping(0).get());
+        assertEquals(UKE_UNDER12, finnUkeMapping(5*12-1).get());
 
-        assertEquals(UKE2_6, finnUkeMapping(5*2).get());
-        assertEquals(UKE2_6, finnUkeMapping(5*6-1).get());
+        assertEquals(UKE12_23, finnUkeMapping(5*12).get());
+        assertEquals(UKE12_23, finnUkeMapping(5*23-1).get());
 
-        assertEquals(UKE92_96, finnUkeMapping(5*92).get());
-        assertEquals(UKE92_96, finnUkeMapping(5*96-1).get());
+        assertEquals(UKE24_35, finnUkeMapping(5*24).get());
+        assertEquals(UKE24_35, finnUkeMapping(5*35-1).get());
 
-        assertEquals(UKE102_104, finnUkeMapping(5*102).get());
-        assertEquals(UKE102_104, finnUkeMapping(5*104-1).get());
-        assertEquals(UKE102_104, finnUkeMapping(5*104).get());
+        assertEquals(UKE36_47, finnUkeMapping(5*36).get());
+        assertEquals(UKE36_47, finnUkeMapping(5*47-1).get());
+
+        assertEquals(UKE48_59, finnUkeMapping(5*48).get());
+        assertEquals(UKE48_59, finnUkeMapping(5*59-1).get());
+
+        assertEquals(UKE60_71, finnUkeMapping(5*60).get());
+        assertEquals(UKE60_71, finnUkeMapping(5*71-1).get());
+
+        assertEquals(UKE72_83, finnUkeMapping(5*72).get());
+        assertEquals(UKE72_83, finnUkeMapping(5*83-1).get());
+
+        assertEquals(UKE84_95, finnUkeMapping(5*84).get());
+        assertEquals(UKE84_95, finnUkeMapping(5*95-1).get());
+
+        assertEquals(UKE96_107, finnUkeMapping(5*96).get());
+        assertEquals(UKE96_107, finnUkeMapping(5*104).get());
+        assertEquals(UKE96_107, finnUkeMapping(522).get());
+        assertEquals(UKE96_107, finnUkeMapping(510).get());
     }
 
     @Test
