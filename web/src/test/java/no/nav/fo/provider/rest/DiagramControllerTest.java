6b80b6e3333e31a5776491c298963d7224fd449d
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-03-12 08:20:16 +0100

    [FO-302] V2 av diagram-api
    Legger til V2 av api for diagram-data som baserer seg på samme
    "stepper"-logikk som frontenden gjør.
    
    På denne måten kan vi slippe å holde en Enum i backend i sync med frontend sine forventninger.
    
    Etter migrering til nytt endepunkt må det ryddes opp i backend og indeksen
    for å fjerne ubrukte felter og klasser.

--- web/src/test/java/no/nav/fo/provider/rest/DiagramControllerTest.java
+++ web/src/test/java/no/nav/fo/provider/rest/DiagramControllerTest.java
@@ -4,6 +4,7 @@ import no.nav.brukerdialog.security.context.ThreadLocalSubjectHandler;
 import no.nav.fo.domene.*;
 import no.nav.fo.service.BrukertilgangService;
 import no.nav.fo.service.SolrService;
+import no.nav.fo.util.StepperUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -34,30 +35,30 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 public class DiagramControllerTest {
 
     private static final List<Bruker> BRUKERE = asList(
-            new Bruker().setFnr("1").setUtlopsdatoFasett(MND1).setDagputlopUkeFasett(UKE_UNDER2).setAapmaxtidUkeFasett(UKE_UNDER12),
-            new Bruker().setFnr("2").setUtlopsdatoFasett(MND2).setDagputlopUkeFasett(UKE2_5).setAapmaxtidUkeFasett(UKE12_23),
-            new Bruker().setFnr("3").setUtlopsdatoFasett(MND3).setDagputlopUkeFasett(UKE6_9).setAapmaxtidUkeFasett(UKE24_35),
-            new Bruker().setFnr("4").setUtlopsdatoFasett(MND4).setDagputlopUkeFasett(UKE10_13).setAapmaxtidUkeFasett(UKE36_47),
-            new Bruker().setFnr("5").setUtlopsdatoFasett(MND5).setDagputlopUkeFasett(UKE14_17).setAapmaxtidUkeFasett(UKE48_59),
-            new Bruker().setFnr("6").setUtlopsdatoFasett(MND6).setDagputlopUkeFasett(UKE18_21).setAapmaxtidUkeFasett(UKE60_71),
-            new Bruker().setFnr("7").setUtlopsdatoFasett(MND7).setDagputlopUkeFasett(UKE22_25).setAapmaxtidUkeFasett(UKE72_83),
-            new Bruker().setFnr("8").setUtlopsdatoFasett(MND8).setDagputlopUkeFasett(UKE26_29).setAapmaxtidUkeFasett(UKE84_95),
-            new Bruker().setFnr("9").setUtlopsdatoFasett(MND9).setDagputlopUkeFasett(UKE30_33).setAapmaxtidUkeFasett(UKE96_107),
-            new Bruker().setFnr("10").setUtlopsdatoFasett(MND10).setDagputlopUkeFasett(UKE34_37).setAapmaxtidUkeFasett(UKE108_119),
-            new Bruker().setFnr("11").setUtlopsdatoFasett(MND11).setDagputlopUkeFasett(UKE38_41).setAapmaxtidUkeFasett(UKE120_131),
-            new Bruker().setFnr("12").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE42_45).setAapmaxtidUkeFasett(UKE132_143),
-            new Bruker().setFnr("13").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE46_49).setAapmaxtidUkeFasett(UKE144_155),
-            new Bruker().setFnr("14").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUkeFasett(UKE156_167),
-            new Bruker().setFnr("15").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUkeFasett(UKE168_179),
-            new Bruker().setFnr("16").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUkeFasett(UKE180_191),
-            new Bruker().setFnr("17").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUkeFasett(UKE192_203),
-            new Bruker().setFnr("18").setUtlopsdatoFasett(MND12).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUkeFasett(UKE204_215)
+            new Bruker().setFnr("1").setUtlopsdatoFasett(MND1).setDagputlopUke(0).setDagputlopUkeFasett(UKE_UNDER2).setAapmaxtidUke(0).setAapmaxtidUkeFasett(UKE_UNDER12),
+            new Bruker().setFnr("2").setUtlopsdatoFasett(MND2).setDagputlopUke(2).setDagputlopUkeFasett(UKE2_5).setAapmaxtidUke(13).setAapmaxtidUkeFasett(UKE12_23),
+            new Bruker().setFnr("3").setUtlopsdatoFasett(MND3).setDagputlopUke(8).setDagputlopUkeFasett(UKE6_9).setAapmaxtidUke(30).setAapmaxtidUkeFasett(UKE24_35),
+            new Bruker().setFnr("4").setUtlopsdatoFasett(MND4).setDagputlopUke(13).setDagputlopUkeFasett(UKE10_13).setAapmaxtidUke(40).setAapmaxtidUkeFasett(UKE36_47),
+            new Bruker().setFnr("5").setUtlopsdatoFasett(MND5).setDagputlopUke(14).setDagputlopUkeFasett(UKE14_17).setAapmaxtidUke(50).setAapmaxtidUkeFasett(UKE48_59),
+            new Bruker().setFnr("6").setUtlopsdatoFasett(MND6).setDagputlopUke(19).setDagputlopUkeFasett(UKE18_21).setAapmaxtidUke(70).setAapmaxtidUkeFasett(UKE60_71),
+            new Bruker().setFnr("7").setUtlopsdatoFasett(MND7).setDagputlopUke(22).setDagputlopUkeFasett(UKE22_25).setAapmaxtidUke(80).setAapmaxtidUkeFasett(UKE72_83),
+            new Bruker().setFnr("8").setUtlopsdatoFasett(MND8).setDagputlopUke(27).setDagputlopUkeFasett(UKE26_29).setAapmaxtidUke(90).setAapmaxtidUkeFasett(UKE84_95),
+            new Bruker().setFnr("9").setUtlopsdatoFasett(MND9).setDagputlopUke(30).setDagputlopUkeFasett(UKE30_33).setAapmaxtidUke(100).setAapmaxtidUkeFasett(UKE96_107),
+            new Bruker().setFnr("10").setUtlopsdatoFasett(MND10).setDagputlopUke(37).setDagputlopUkeFasett(UKE34_37).setAapmaxtidUke(110).setAapmaxtidUkeFasett(UKE108_119),
+            new Bruker().setFnr("11").setUtlopsdatoFasett(MND11).setDagputlopUke(38).setDagputlopUkeFasett(UKE38_41).setAapmaxtidUke(131).setAapmaxtidUkeFasett(UKE120_131),
+            new Bruker().setFnr("12").setUtlopsdatoFasett(MND12).setDagputlopUke(42).setDagputlopUkeFasett(UKE42_45).setAapmaxtidUke(140).setAapmaxtidUkeFasett(UKE132_143),
+            new Bruker().setFnr("13").setUtlopsdatoFasett(MND12).setDagputlopUke(46).setDagputlopUkeFasett(UKE46_49).setAapmaxtidUke(150).setAapmaxtidUkeFasett(UKE144_155),
+            new Bruker().setFnr("14").setUtlopsdatoFasett(MND12).setDagputlopUke(52).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUke(156).setAapmaxtidUkeFasett(UKE156_167),
+            new Bruker().setFnr("15").setUtlopsdatoFasett(MND12).setDagputlopUke(51).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUke(168).setAapmaxtidUkeFasett(UKE168_179),
+            new Bruker().setFnr("16").setUtlopsdatoFasett(MND12).setDagputlopUke(50).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUke(180).setAapmaxtidUkeFasett(UKE180_191),
+            new Bruker().setFnr("17").setUtlopsdatoFasett(MND12).setDagputlopUke(51).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUke(195).setAapmaxtidUkeFasett(UKE192_203),
+            new Bruker().setFnr("18").setUtlopsdatoFasett(MND12).setDagputlopUke(52).setDagputlopUkeFasett(UKE50_52).setAapmaxtidUke(210).setAapmaxtidUkeFasett(UKE204_215)
     );
 
     private static final List<Bruker> BRUKERE_AAP_UNNTAK = asList(
-            new Bruker().setFnr("1").setUtlopsdatoFasett(MND1).setDagputlopUkeFasett(UKE_UNDER2).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE24_35),
-            new Bruker().setFnr("2").setUtlopsdatoFasett(MND2).setDagputlopUkeFasett(UKE2_5).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE36_47),
-            new Bruker().setFnr("3").setUtlopsdatoFasett(MND3).setDagputlopUkeFasett(UKE6_9).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE36_47)
+            new Bruker().setFnr("1").setUtlopsdatoFasett(MND1).setDagputlopUkeFasett(UKE_UNDER2).setAapUnntakUkerIgjen(25).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE24_35),
+            new Bruker().setFnr("2").setUtlopsdatoFasett(MND2).setDagputlopUkeFasett(UKE2_5).setAapUnntakUkerIgjen(40).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE36_47),
+            new Bruker().setFnr("3").setUtlopsdatoFasett(MND3).setDagputlopUkeFasett(UKE6_9).setAapUnntakUkerIgjen(40).setAapUnntakUkerIgjenFasett(AAPUnntakUkerIgjenFasettMapping.UKE36_47)
     );
 
     @Mock
@@ -156,5 +157,63 @@ public class DiagramControllerTest {
         assertThat(gruppering).hasSize(9);
         assertThat(storsteGruppe).contains(2L);
         assertThat(gruppering.get(AAPUnntakUkerIgjenFasettMapping.UKE36_47)).isEqualTo(2L);
+    }
+
+    @Test
+    public void skalGrupperePaUtlopsdatoV2() throws Exception {
+        List<Bruker> brukere = new ArrayList<>(BRUKERE);
+        brukere.add(BRUKERE.get(3));
+
+        when(solr.hentBrukere(anyString(), any(), any(), any(), any(Filtervalg.class))).thenReturn(new BrukereMedAntall(brukere.size(),brukere));
+
+        Response response = controller.hentDiagramData2("Z999000", "0100", new Filtervalg().setYtelse(YtelseFilter.DAGPENGER));
+
+        List<StepperUtils.Step> gruppering = (List<StepperUtils.Step>) response.getEntity();
+        Optional<Long> storsteGruppe = gruppering.stream().map(StepperUtils.Step::getVerdi).max(Long::compare);
+        Optional<StepperUtils.Step> uke10_13_verdi = gruppering.stream().filter((step) -> step.getFra() == 10 && step.getTil() == 13).findFirst();
+        Optional<StepperUtils.Step> uke50_52_verdi = gruppering.stream().filter((step) -> step.getFra() == 50 && step.getTil() == 52).findFirst();
+
+        assertThat(gruppering).hasSize(14);
+        assertThat(storsteGruppe).contains(5L);
+        assertThat(uke10_13_verdi.isPresent()).isTrue();
+        assertThat(uke50_52_verdi.isPresent()).isTrue();
+        assertThat(uke10_13_verdi.get().getVerdi()).isEqualTo(2L);
+        assertThat(uke50_52_verdi.get().getVerdi()).isEqualTo(5L);
+    }
+
+    @Test
+    public void skalGrupperePaAApUtlopsdatoV2() throws Exception {
+        List<Bruker> brukere = new ArrayList<>(BRUKERE);
+        brukere.add(BRUKERE.get(3));
+
+        when(solr.hentBrukere(anyString(), any(), any(), any(), any(Filtervalg.class))).thenReturn(new BrukereMedAntall(brukere.size(),brukere));
+
+        Response response = controller.hentDiagramData2("Z999000", "0100", new Filtervalg().setYtelse(YtelseFilter.AAP_MAXTID));
+
+        List<StepperUtils.Step> gruppering = (List<StepperUtils.Step>) response.getEntity();
+        Optional<Long> storsteGruppe = gruppering.stream().map(StepperUtils.Step::getVerdi).max(Long::compare);
+        Optional<StepperUtils.Step> uke38_46_verdi = gruppering.stream().filter((step) -> step.getFra() == 38 && step.getTil() == 46).findFirst();
+
+        assertThat(gruppering).hasSize(25);
+        assertThat(storsteGruppe).contains(2L);
+        assertThat(uke38_46_verdi.isPresent()).isTrue();
+        assertThat(uke38_46_verdi.get().getVerdi()).isEqualTo(2L);
+    }
+
+    @Test
+    public void skalGrupperePaAAPUnntakUtlopsdatoV2() throws Exception {
+        List<Bruker> brukere = new ArrayList<>(BRUKERE_AAP_UNNTAK);
+
+        when(solr.hentBrukere(anyString(), any(), any(), any(), any(Filtervalg.class))).thenReturn(new BrukereMedAntall(brukere.size(), brukere));
+
+        Response response = controller.hentDiagramData2("Z999000", "0100", new Filtervalg().setYtelse(YtelseFilter.AAP_UNNTAK));
+
+        List<StepperUtils.Step> gruppering = (List<StepperUtils.Step>) response.getEntity();
+        Optional<Long> storsteGruppe = gruppering.stream().map(StepperUtils.Step::getVerdi).max(Long::compare);
+        Optional<StepperUtils.Step> uke37_41_verdi = gruppering.stream().filter((step) -> step.getFra() == 37 && step.getTil() == 41).findFirst();
+
+        assertThat(gruppering).hasSize(22);
+        assertThat(storsteGruppe).contains(2L);
+        assertThat(uke37_41_verdi.get().getVerdi()).isEqualTo(2L);
     }
 }
