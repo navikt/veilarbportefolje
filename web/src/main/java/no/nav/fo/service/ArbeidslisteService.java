c1c599c51d172f2c1b9ad75646e9fdaf81a1ae45
Torstein Gjengedal <torstein.gjengedal@nav.no>
2018-04-11 16:27:21 +0200

    [FO-844] Indekserer asynkront slik at rest-operasjonen slipper å vente.

--- web/src/main/java/no/nav/fo/service/ArbeidslisteService.java
+++ web/src/main/java/no/nav/fo/service/ArbeidslisteService.java
@@ -59,7 +59,7 @@ public class ArbeidslisteService {
         data.setAktoerId(aktoerId.get());
         return arbeidslisteRepository
                 .insertArbeidsliste(data)
-                .onSuccess(solrService::indekserBrukerdata);
+                .onSuccess(solrService::indekserAsynkront);
     }
 
     public Try<AktoerId> updateArbeidsliste(ArbeidslisteData data) {
@@ -70,7 +70,7 @@ public class ArbeidslisteService {
 
         return arbeidslisteRepository
                 .updateArbeidsliste(data.setAktoerId(aktoerId.get()))
-                .onSuccess(solrService::indekserBrukerdata);
+                .onSuccess(solrService::indekserAsynkront);
     }
 
     public Try<AktoerId> deleteArbeidsliste(Fnr fnr) {
@@ -80,13 +80,7 @@ public class ArbeidslisteService {
         }
         return arbeidslisteRepository
                 .deleteArbeidsliste(aktoerId.get())
-                .onSuccess(solrService::indekserBrukerdata);
-    }
-
-    public Try<AktoerId> deleteArbeidsliste(AktoerId aktoerId) {
-        return arbeidslisteRepository
-                .deleteArbeidsliste(aktoerId)
-                .onSuccess(solrService::indekserBrukerdata);
+                .onSuccess(solrService::indekserAsynkront);
     }
 
     public Try<String> hentEnhet(Fnr fnr) {
