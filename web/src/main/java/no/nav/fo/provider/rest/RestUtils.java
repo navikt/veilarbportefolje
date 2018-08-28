a4716cadb5dc8117c1a9655a9a1a0fd269eb42e2
Oscar Eckbo <oscar.eckbo@nav.no>
2018-08-28 12:00:11 +0200

    [FO-373] kaster eksisterende WebApplication

--- web/src/main/java/no/nav/fo/provider/rest/RestUtils.java
+++ web/src/main/java/no/nav/fo/provider/rest/RestUtils.java
@@ -3,6 +3,7 @@ package no.nav.fo.provider.rest;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import java.util.function.Supplier;
 
@@ -15,10 +16,13 @@ class RestUtils {
                 .toEither()
                 .fold(
                         (throwable) -> {
+                            if (throwable instanceof WebApplicationException) {
+                                return ((WebApplicationException) throwable).getResponse();
+                            }
                             log.warn("Exception ved restkall", throwable);
-                            return Response.status(Response.Status.INTERNAL_SERVER_ERROR);
+                            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
                         },
-                        (entity) -> Response.status(OK).entity(entity)
-                ).build();
+                        (entity) -> Response.status(OK).entity(entity).build()
+                );
     }
 }
