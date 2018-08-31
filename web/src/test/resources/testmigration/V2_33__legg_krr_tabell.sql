7f6512343ea1d3930941ea9dfae8f3e01c991ae1
alexander-svendsen <alexander.svendsen@nav.no>
2018-08-31 15:54:58 +0200

    FO-270: Run migration unless test migration exist
    The default migrations are first added, then overwritten by the
    test migration if a script exist with the same name and version
    
    Removed duplicate migration files

--- web/src/test/resources/testmigration/V2_33__legg_krr_tabell.sql
+++
@@ -1,9 +0,0 @@
-create table KRR
-(
-  FODSELSNR VARCHAR2(11) not null
-    constraint KRR_FODSELSNR_PK
-    primary key,
-  RESERVASJON VARCHAR2(1),
-  SISTEVERIFISERT TIMESTAMP(6),
-  LAGTTILIDB TIMESTAMP(6)
-)
