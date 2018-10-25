7d9c561341fab1eb16f11c8fa2abd04945c6d065
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-10-25 18:22:03 +0200

    FO-1400: Fix test

--- src/test/java/no/nav/fo/veilarbportefolje/filmottak/ytelser/KopierGR199FraArenaTest.java
+++ src/test/java/no/nav/fo/veilarbportefolje/filmottak/ytelser/KopierGR199FraArenaTest.java
@@ -2,15 +2,25 @@ package no.nav.fo.veilarbportefolje.filmottak.ytelser;
 
 import io.vavr.control.Try;
 import no.nav.melding.virksomhet.loependeytelser.v1.LoependeYtelser;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.InputStream;
 
+import static java.lang.System.setProperty;
+import static no.nav.fo.veilarbportefolje.config.ApplicationConfig.LOEPENDEYTELSER_FILNAVN_PROPERTY;
+import static no.nav.fo.veilarbportefolje.config.ApplicationConfig.LOEPENDEYTELSER_PATH_PROPERTY;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class KopierGR199FraArenaTest {
     private static InputStream vedtakFormat = KopierGR199FraArenaTest.class.getResourceAsStream("/arena-vedtak.xml");
+
+    @BeforeClass
+    public static void setup() {
+        setProperty(LOEPENDEYTELSER_PATH_PROPERTY, "/");
+        setProperty(LOEPENDEYTELSER_FILNAVN_PROPERTY, "test.xml");
+    }
 
     @Test
     public void unmarshalling() {
