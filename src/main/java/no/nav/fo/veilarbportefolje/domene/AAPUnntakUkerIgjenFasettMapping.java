86091ff95cc1eaac9eba54b4fae2540910f2fb82
hakonph <hakon.planke.holm@nav.no>
2018-11-22 13:08:04 +0100

    FO-1673 leger brukere med mere enn MAx dager til max i stedet for exception

--- src/main/java/no/nav/fo/veilarbportefolje/domene/AAPUnntakUkerIgjenFasettMapping.java
+++ src/main/java/no/nav/fo/veilarbportefolje/domene/AAPUnntakUkerIgjenFasettMapping.java
@@ -1,10 +1,11 @@
 package no.nav.fo.veilarbportefolje.domene;
 
-import no.nav.fo.veilarbportefolje.exception.UgyldigAntallDagerIgjenException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
+@Slf4j
 public enum AAPUnntakUkerIgjenFasettMapping implements FasettMapping {
     UKE_UNDER12(0, 11), UKE12_23(12, 23), UKE24_35(24, 35), UKE36_47(36, 47),
     UKE48_59(48, 59), UKE60_71(60, 71), UKE72_83(72, 83), UKE84_95(84, 95),
@@ -22,15 +23,17 @@ public enum AAPUnntakUkerIgjenFasettMapping implements FasettMapping {
         int MAX_ANTALL_DAGER = 522;
 
         if (dager > MAX_ANTALL_DAGER) {
-            throw new UgyldigAntallDagerIgjenException(dager);
+            log.info("bruekr med % dager. max antall dager %", dager, MAX_ANTALL_DAGER);
+            dager = MAX_ANTALL_DAGER;
         } else if (dager < 0) {
+            log.info("bruker emd mindre enn 0 dager, % ", dager);
             dager = 0;
         }
 
         int uker = dager / 5;
 
         return Stream.of(values())
-                .filter((mapping) -> mapping.start <= uker && uker <= mapping.stop)
+                .filter(mapping -> mapping.start <= uker && uker <= mapping.stop)
                 .findAny();
     }
 
