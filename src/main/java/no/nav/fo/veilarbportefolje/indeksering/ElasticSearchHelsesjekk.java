947e3fc7fb6ff96fb9157e6fcfc32ea2e415629c
Peder Korsveien <peder.korsveien@nav.no>
2019-01-30 15:00:53 +0100

    PUS-221 Bruk en instans av ES-client

--- src/main/java/no/nav/fo/veilarbportefolje/indeksering/ElasticSearchHelsesjekk.java
+++ src/main/java/no/nav/fo/veilarbportefolje/indeksering/ElasticSearchHelsesjekk.java
@@ -1,18 +1,34 @@
 package no.nav.fo.veilarbportefolje.indeksering;
 
+import lombok.SneakyThrows;
 import no.nav.apiapp.selftest.Helsesjekk;
 import no.nav.apiapp.selftest.HelsesjekkMetadata;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+import java.io.IOException;
 
 import static org.elasticsearch.cluster.health.ClusterHealthStatus.GREEN;
 
 @Component
 public class ElasticSearchHelsesjekk implements Helsesjekk {
 
+    RestHighLevelClient client;
+
+    @Inject
+    public ElasticSearchHelsesjekk(RestHighLevelClient client) {
+        this.client = client;
+    }
+
     @Override
+    @SneakyThrows
     public void helsesjekk() {
-        ClusterHealthResponse health = ElasticUtils.clusterHealthcheck();
+        ClusterHealthResponse health = client.cluster().health(new ClusterHealthRequest(), RequestOptions.DEFAULT);
         if (health.getStatus() != GREEN) {
             throw new RuntimeException(health.toString());
         }
