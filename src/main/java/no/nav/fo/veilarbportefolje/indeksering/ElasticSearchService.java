ab354390cae99e42e8efe4e776ace3ab93c68ac9
Peder Korsveien <peder.korsveien@nav.no>
2019-02-01 09:12:18 +0100

    PUS-221 Bedre logging ved deltaindeksering

--- src/main/java/no/nav/fo/veilarbportefolje/indeksering/ElasticSearchService.java
+++ src/main/java/no/nav/fo/veilarbportefolje/indeksering/ElasticSearchService.java
@@ -221,13 +221,14 @@ public class ElasticSearchService implements IndekseringService {
     @SneakyThrows
     public void slettBruker(BrukerDTO bruker) {
 
-        log.info("Sletter bruker med aktørId {}", bruker.aktoer_id);
         DeleteByQueryRequest deleteQuery = new DeleteByQueryRequest(getAlias())
                 .setQuery(new TermQueryBuilder("fnr", bruker.fnr));
 
         BulkByScrollResponse response = client.deleteByQuery(deleteQuery, DEFAULT);
-        if (response.getDeleted() != 1) {
-            log.warn("Feil ved sletting av bruker med aktoerId {} i indeks {}", bruker.aktoer_id, response.toString());
+        if (response.getDeleted() == 1) {
+            log.info("Slettet bruker med aktorId {} fra indeks {}", bruker.aktoer_id, getAlias());
+        } else {
+            log.warn("Feil ved sletting av bruker med aktoerId {} i indeks {}: {}", bruker.aktoer_id, getAlias(), response.toString());
         }
     }
 
