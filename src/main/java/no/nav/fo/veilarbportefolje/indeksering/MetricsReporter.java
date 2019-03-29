18080bb33657306378dd98e8b367e9527f9703b3
Peder Korsveien <peder.korsveien@nav.no>
2019-03-29 16:10:39 +0100

    PUS-221 disable leaderelection for feilsøking

--- src/main/java/no/nav/fo/veilarbportefolje/indeksering/MetricsReporter.java
+++ src/main/java/no/nav/fo/veilarbportefolje/indeksering/MetricsReporter.java
@@ -14,7 +14,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static no.nav.common.leaderelection.LeaderElection.isLeader;
 
 @Component
 @Slf4j
@@ -26,11 +25,11 @@ public class MetricsReporter {
     public MetricsReporter(UnleashService unleash) {
         this.unleash = unleash;
 
-        if (isLeader()) {
-            log.info("logger metrikker for antall dokumenter i elastic");
+//        if (isLeader()) {
+//            log.info("logger metrikker for antall dokumenter i elastic");
             ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
             scheduler.scheduleAtFixedRate(new ReportNumberOfDocuments(), 1, 1, MINUTES);
-        }
+//        }
 
     }
 
