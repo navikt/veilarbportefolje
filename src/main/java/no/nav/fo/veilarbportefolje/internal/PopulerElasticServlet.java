f9322b1b5ec7f28d7946f95d89aee173dab5ed47
Peder Korsveien <peder.korsveien@nav.no>
2019-06-04 15:57:24 +0200

    KAIZEN-0 Legg jobId på batch-jobber

--- src/main/java/no/nav/fo/veilarbportefolje/internal/PopulerElasticServlet.java
+++ src/main/java/no/nav/fo/veilarbportefolje/internal/PopulerElasticServlet.java
@@ -1,14 +1,14 @@
 package no.nav.fo.veilarbportefolje.internal;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.batch.BatchJob;
 import no.nav.fo.veilarbportefolje.indeksering.ElasticIndexer;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import static java.util.concurrent.CompletableFuture.runAsync;
+import java.util.Optional;
 
 @Slf4j
 public class PopulerElasticServlet extends HttpServlet {
@@ -22,10 +22,13 @@ public class PopulerElasticServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         if (AuthorizationUtils.isBasicAuthAuthorized(req)) {
-            log.info("Manuell Indeksering: Hovedindeksering i ElasticSearch");
-            runAsync(() -> elasticIndexer.hovedindeksering());
-            resp.getWriter().write("Hovedindeksering i ElasticSearch startet");
-            resp.setStatus(200);
+            Optional<String> maybeJobId = BatchJob.runAsync(() -> elasticIndexer.hovedindeksering());
+
+            if (maybeJobId.isPresent()) {
+                resp.getWriter().write("Hovedindeksering i ElasticSearch startet med jobId: {}");
+                resp.setStatus(200);
+            }
+
         } else {
             AuthorizationUtils.writeUnauthorized(resp);
         }
