41b7b42a7f8664c9fef4d4b7580aa24d044d6b12
Peder Korsveien <peder.korsveien@nav.no>
2020-01-07 14:38:22 +0100

    KZ-0 Revert krr-rest

--- src/main/java/no/nav/fo/veilarbportefolje/config/ClientConfig.java
+++ src/main/java/no/nav/fo/veilarbportefolje/config/ClientConfig.java
@@ -1,8 +1,6 @@
 package no.nav.fo.veilarbportefolje.config;
 
-import net.jodah.failsafe.RetryPolicy;
 import no.nav.brukerdialog.security.oidc.SystemUserTokenProvider;
-import no.nav.fo.veilarbportefolje.krr.KrrDTO;
 import no.nav.sbl.rest.RestUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,31 +9,9 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import java.io.IOException;
-import java.util.Optional;
-
-import static java.time.Duration.ofSeconds;
 
 @Configuration
 public class ClientConfig {
-
-    private static int retryDelayInSeconds = 10;
-    private static int maxRetries = 3;
-
-    public static int getRetryDelayInSeconds() {
-        return retryDelayInSeconds;
-    }
-
-    public static void setRetryDelayInSeconds(int retryDelayInSeconds) {
-        ClientConfig.retryDelayInSeconds = retryDelayInSeconds;
-    }
-
-    public static int getMaxRetries() {
-        return maxRetries;
-    }
-
-    public static void setMaxRetries(int maxRetries) {
-        ClientConfig.maxRetries = maxRetries;
-    }
 
     @Bean
     public Client client() {
