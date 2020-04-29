f3304e51a11616415603967adf7325e99b5feb31
alexandraback <alexandra1.back@gmail.com>
2020-04-29 07:22:51 +0200

    FO-2540: logg vedtakstatus

--- src/main/java/no/nav/pto/veilarbportefolje/vedtakstotte/VedtakService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/vedtakstotte/VedtakService.java
@@ -26,6 +26,7 @@ public class VedtakService implements KafkaConsumerService {
     public void behandleKafkaMelding(String melding) {
         KafkaVedtakStatusEndring vedtakStatusEndring = fromJson(melding, KafkaVedtakStatusEndring.class);
         KafkaVedtakStatusEndring.VedtakStatusEndring vedtakStatus = vedtakStatusEndring.getVedtakStatusEndring();
+        log.info("Behandler vedtakstatus {} " + vedtakStatus.name());
         switch (vedtakStatus) {
             case UTKAST_SLETTET : {
                 slettUtkast(vedtakStatusEndring);
@@ -43,7 +44,7 @@ public class VedtakService implements KafkaConsumerService {
             case GODKJENT_AV_BESLUTTER:
             case KLAR_TIL_BESLUTTER:
             case KLAR_TIL_VEILEDER: {
-                log.info("Behandler vedtakstatus {} " + vedtakStatus.name());
+                log.info("oppdaterUtkast vedtakstatus {} " + vedtakStatus.name());
                 oppdaterUtkast(vedtakStatusEndring);
             }
         }
