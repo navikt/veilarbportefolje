487967d7ed810bbaede860937541b1a5b2ba4cef
Peder Korsveien <peder.korsveien@nav.no>
2020-05-12 13:02:31 +0200

    KZ-0 Logg faktiske subscriptions

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaConsumerRunnable.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaConsumerRunnable.java
@@ -54,7 +54,7 @@ public class KafkaConsumerRunnable implements Runnable {
         try {
             log.info("Subscriber til topic {}", this.topic);
             consumer.subscribe(singletonList(topic));
-            log.info("Har subscribet til topic {}", this.topic);
+            consumer.subscription().forEach(subscription -> log.info("Har subscribet til topic {}", this.topic));
 
             boolean logged = false;
             while (featureErPa() && !shutdown.get()) {
