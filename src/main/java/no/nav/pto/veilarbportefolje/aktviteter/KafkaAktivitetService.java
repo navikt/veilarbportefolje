d73e482f0d5b018487153f77a871be3dbadaf515
alexandraback <alexandra1.back@gmail.com>
2020-05-22 10:33:38 +0200

    KAIZEN-0: Mer logging

--- src/main/java/no/nav/pto/veilarbportefolje/aktviteter/KafkaAktivitetService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/aktviteter/KafkaAktivitetService.java
@@ -31,10 +31,11 @@ public class KafkaAktivitetService implements KafkaConsumerService<String> {
          */
         ObjectMapper objectMapper = JsonProvider.createObjectMapper();
         try {
+            log.info("Inne i aktivitetmelding");
             List<AktivitetDataFraFeed> aktivitetData = objectMapper.readValue(kafkaMelding, objectMapper.getTypeFactory().constructParametricType(List.class, AktivitetDataFraFeed.class));
             aktivitetService.oppdaterAktiviteter(aktivitetData);
         } catch (JsonProcessingException e) {
-            log.error("Kunne ikke desirialisera kafka aktiviitetmelding: {}", kafkaMelding);
+            log.error("Kunde ikke deserialisera kafka aktivitetmelding: {}", kafkaMelding);
         }
     }
 }
