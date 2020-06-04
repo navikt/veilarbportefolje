e1429177cdf026ec7d5f9b4c854407d56a263203
alexandraback <alexandra1.back@gmail.com>
2020-06-04 14:10:30 +0200

    KAIZEN-0: Hent fnr hvis indeksering med aktorId feiler

--- src/main/java/no/nav/pto/veilarbportefolje/registrering/RegistreringService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/registrering/RegistreringService.java
@@ -3,8 +3,10 @@ package no.nav.pto.veilarbportefolje.registrering;
 import no.nav.arbeid.soker.registrering.ArbeidssokerRegistrertEvent;
 import no.nav.pto.veilarbportefolje.domene.AktoerId;
 import no.nav.pto.veilarbportefolje.elastic.ElasticIndexer;
+import no.nav.pto.veilarbportefolje.elastic.domene.OppfolgingsBruker;
 import no.nav.pto.veilarbportefolje.kafka.KafkaConsumerService;
 import no.nav.pto.veilarbportefolje.service.AktoerService;
+import no.nav.pto.veilarbportefolje.util.Result;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -23,8 +25,12 @@ public class RegistreringService implements KafkaConsumerService<ArbeidssokerReg
     public void behandleKafkaMelding(ArbeidssokerRegistrertEvent kafkaRegistreringMelding) {
         AktoerId aktoerId = AktoerId.of(kafkaRegistreringMelding.getAktorid());
         registreringRepository.upsertBrukerRegistrering(kafkaRegistreringMelding);
-        aktoerService.hentFnrFraAktorId(aktoerId)
-                .onSuccess(fnr -> elasticIndexer.indekser(fnr));
+        Result<OppfolgingsBruker> oppfolgingsBrukerResult = elasticIndexer.indekser(aktoerId);
+
+        if(oppfolgingsBrukerResult.isErr()) {
+            aktoerService.hentFnrFraAktorId(aktoerId)
+                    .onSuccess(elasticIndexer::indekser);
+        }
     }
 
     public Optional<ZonedDateTime> hentRegistreringOpprettet(AktoerId aktoerId) {
