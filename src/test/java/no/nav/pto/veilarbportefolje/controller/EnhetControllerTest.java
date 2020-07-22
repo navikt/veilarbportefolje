991996babb9d89696b8c6ac79f6dbe010d9777c1
alexandraback <alexandra1.back@gmail.com>
2020-07-22 10:15:30 +0200

    FO-2946: Fiks EnhetsController test

--- src/test/java/no/nav/pto/veilarbportefolje/controller/EnhetControllerTest.java
+++ src/test/java/no/nav/pto/veilarbportefolje/controller/EnhetControllerTest.java
@@ -1,6 +1,5 @@
 package no.nav.pto.veilarbportefolje.controller;
 
-
 import no.nav.common.abac.Pep;
 import no.nav.common.auth.subject.SsoToken;
 import no.nav.common.auth.subject.Subject;
@@ -8,17 +7,14 @@ import no.nav.common.auth.subject.SubjectHandler;
 import no.nav.common.metrics.MetricsClient;
 import no.nav.pto.veilarbportefolje.arenafiler.gr202.tiltak.TiltakService;
 import no.nav.pto.veilarbportefolje.auth.AuthService;
-import no.nav.pto.veilarbportefolje.domene.BrukereMedAntall;
 import no.nav.pto.veilarbportefolje.domene.Filtervalg;
 import no.nav.pto.veilarbportefolje.elastic.ElasticIndexer;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
-import java.util.Optional;
 
 
 import static no.nav.common.auth.subject.IdentType.InternBruker;
@@ -28,7 +24,6 @@ import static org.mockito.Mockito.*;
 public class EnhetControllerTest {
 
     private ElasticIndexer elasticIndexer;
-    private AuthService authService;
     private EnhetController enhetController;
     private Pep pep;
 
@@ -36,8 +31,9 @@ public class EnhetControllerTest {
     public void initController() {
         elasticIndexer = mock(ElasticIndexer.class);
         pep = mock(Pep.class);
-        authService = new AuthService(pep);
+        AuthService authService = new AuthService(pep);
         enhetController = new EnhetController(elasticIndexer, authService, mock(MetricsClient.class), mock(TiltakService.class));
+
     }
 
     @Test
@@ -53,19 +49,21 @@ public class EnhetControllerTest {
     @Test
     public void skalHenteHelePortefoljeFraIndeksDersomManMangleAntall() throws Exception {
         when(pep.harVeilederTilgangTilEnhet(any(), any())).thenReturn(true);
-        when(pep.harVeilederTilgangTilOppfolging(any())).thenReturn(true);
-
-        enhetController.hentPortefoljeForEnhet("0001", 0, null, "ikke_satt", "ikke_satt", new Filtervalg());
-
-        verify(elasticIndexer, times(1)).hentBrukere(any(), any(), any(), any(), any(), any(), isNull());
+        when(pep.harVeilederTilgangTilModia(any())).thenReturn(true);
+        SubjectHandler.withSubject(
+                new Subject("testident", InternBruker, SsoToken.oidcToken("token", Collections.emptyMap())),
+                () -> enhetController.hentPortefoljeForEnhet("0001", 0, null, "ikke_satt", "ikke_satt", new Filtervalg()));
+        verify(elasticIndexer, times(1)).hentBrukere(any(), any(), any(), any(), any(), any(), any());
     }
 
     @Test
     public void skalHenteHelePortefoljeFraIndeksDersomManMangleFra() throws Exception {
         when(pep.harVeilederTilgangTilEnhet(any(), any())).thenReturn(true);
-        when(pep.harVeilederTilgangTilOppfolging(any())).thenReturn(true);
+        when(pep.harVeilederTilgangTilModia(any())).thenReturn(true);
+        SubjectHandler.withSubject(
+                new Subject("testident", InternBruker, SsoToken.oidcToken("token", Collections.emptyMap())),
+                () -> enhetController.hentPortefoljeForEnhet("0001", null, 20, "ikke_satt", "ikke_satt", new Filtervalg()));
 
-        enhetController.hentPortefoljeForEnhet("0001", null, 20, "ikke_satt", "ikke_satt", new Filtervalg());
 
         verify(elasticIndexer, times(1)).hentBrukere(any(), any(), any(), any(), any(), isNull(), any());
     }
