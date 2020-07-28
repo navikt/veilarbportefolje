8d693081e7690cb4067a6c693b0eff786e0b3df1
alexandraback <alexandra1.back@gmail.com>
2020-07-28 13:56:17 +0200

    FO-2946: Bruk servicebrukertoken før autentisering mot veilarbveileder

--- src/main/java/no/nav/pto/veilarbportefolje/client/VeilarbVeilederClient.java
+++ src/main/java/no/nav/pto/veilarbportefolje/client/VeilarbVeilederClient.java
@@ -11,10 +11,7 @@ import org.springframework.cache.annotation.Cacheable;
 import java.util.List;
 import static java.lang.String.format;
 import static no.nav.common.utils.EnvironmentUtils.requireNamespace;
-import static no.nav.pto.veilarbportefolje.client.RestClientUtils.authHeaderMedInnloggetBruker;
-import static no.nav.pto.veilarbportefolje.client.RestClientUtils.createBearerToken;
 import static no.nav.pto.veilarbportefolje.config.CacheConfig.VEILARBVEILEDER;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 public class VeilarbVeilederClient {
@@ -34,7 +31,6 @@ public class VeilarbVeilederClient {
 
         Request request  = new Request.Builder()
                 .url(url + path)
-                .header(AUTHORIZATION,  createBearerToken(authHeaderMedInnloggetBruker()))
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
