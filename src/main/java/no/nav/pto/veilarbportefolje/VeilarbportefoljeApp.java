7c4ca65c9c61ed3862ec98642c3de148e7587709
alexandraback <alexandra1.back@gmail.com>
2020-08-07 12:19:20 +0200

    KAIZEN-0: Bump common

--- src/main/java/no/nav/pto/veilarbportefolje/VeilarbportefoljeApp.java
+++ src/main/java/no/nav/pto/veilarbportefolje/VeilarbportefoljeApp.java
@@ -1,14 +1,9 @@
 package no.nav.pto.veilarbportefolje;
 
-import no.nav.common.cxf.StsSecurityConstants;
-import no.nav.common.utils.Credentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-import static no.nav.common.utils.EnvironmentUtils.getRequiredProperty;
-import static no.nav.common.utils.NaisUtils.getCredentials;
-import static no.nav.pto.veilarbportefolje.config.ApplicationConfig.SECURITYTOKENSERVICE_URL_PROPERTY_NAME;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -16,12 +11,6 @@ public class VeilarbportefoljeApp {
 
     public static void main(String... args) {
         System.setProperty("oppfolging.feed.brukertilgang", "srvveilarboppfolging");
-
-        //STS
-        System.setProperty(StsSecurityConstants.STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL_PROPERTY_NAME));
-        Credentials serviceUser = getCredentials("service_user");
-        System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
-        System.setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
 
         SpringApplication.run(VeilarbportefoljeApp.class, args);
     }
--- src/main/java/no/nav/pto/veilarbportefolje/config/FilterConfig.java
+++ src/main/java/no/nav/pto/veilarbportefolje/config/FilterConfig.java
@@ -3,31 +3,24 @@ package no.nav.pto.veilarbportefolje.config;
 import no.nav.common.auth.oidc.filter.OidcAuthenticationFilter;
 import no.nav.common.auth.oidc.filter.OidcAuthenticatorConfig;
 import no.nav.common.auth.subject.IdentType;
+import no.nav.common.auth.utils.ServiceUserTokenFinder;
 import no.nav.common.log.LogFilter;
 import no.nav.common.rest.filter.SetStandardHttpHeadersFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static no.nav.common.auth.Constants.*;
-import static no.nav.common.auth.oidc.filter.OidcAuthenticator.fromConfig;
+import static no.nav.common.auth.oidc.filter.OidcAuthenticator.fromConfigs;
 import static no.nav.common.utils.EnvironmentUtils.isDevelopment;
 import static no.nav.common.utils.EnvironmentUtils.requireApplicationName;
 
 @Configuration
 public class FilterConfig {
 
-    public OidcAuthenticatorConfig openAmAuthConfig(EnvironmentProperties environmentProperties) {
-        return new OidcAuthenticatorConfig()
-                .withDiscoveryUrl(environmentProperties.getOpenAmDiscoveryUrl())
-                .withClientId(environmentProperties.getOpenAmClientId())
-                .withIdTokenCookieName(OPEN_AM_ID_TOKEN_COOKIE_NAME)
-                .withRefreshTokenCookieName(REFRESH_TOKEN_COOKIE_NAME)
-                .withRefreshUrl(environmentProperties.getOpenAmRefreshUrl())
-                .withIdentType(IdentType.InternBruker);
-    }
 
     public OidcAuthenticatorConfig azureAdAuthConfig(EnvironmentProperties environmentProperties) {
         return new OidcAuthenticatorConfig()
@@ -37,21 +30,48 @@ public class FilterConfig {
                 .withIdentType(IdentType.InternBruker);
     }
 
+    private OidcAuthenticatorConfig openAmStsAuthConfig(EnvironmentProperties properties) {
+        return new OidcAuthenticatorConfig()
+                .withDiscoveryUrl(properties.getOpenAmDiscoveryUrl())
+                .withClientId(properties.getOpenAmClientId())
+                .withIdTokenFinder(new ServiceUserTokenFinder())
+                .withIdentType(IdentType.Systemressurs);
+    }
+    private OidcAuthenticatorConfig naisStsAuthConfig(EnvironmentProperties properties) {
+        return new OidcAuthenticatorConfig()
+                .withDiscoveryUrl(properties.getStsDiscoveryUrl())
+                .withClientIds(List.of("srvveilarbperson")) // TODO VILKE ANDRA KALLER PORTEFOLJE ???
+                .withIdentType(IdentType.Systemressurs);
+    }
+
+    private OidcAuthenticatorConfig openAmAuthConfig(EnvironmentProperties properties) {
+        return new OidcAuthenticatorConfig()
+                .withDiscoveryUrl(properties.getOpenAmDiscoveryUrl())
+                .withClientId(properties.getOpenAmClientId())
+                .withIdTokenCookieName(OPEN_AM_ID_TOKEN_COOKIE_NAME)
+                .withRefreshTokenCookieName(REFRESH_TOKEN_COOKIE_NAME)
+                .withIdTokenFinder((req) -> Optional.empty()) // This overrides the default finder which checks the Authorization header for tokens
+                .withRefreshUrl(properties.getOpenAmRefreshUrl())
+                .withIdentType(IdentType.InternBruker);
+    }
+
     @Bean
     public FilterRegistrationBean authenticationFilterRegistrationBean(EnvironmentProperties properties) {
-        OidcAuthenticatorConfig openAmConfig = openAmAuthConfig(properties);
-        OidcAuthenticatorConfig azureAdConfig = azureAdAuthConfig(properties);
-
         FilterRegistrationBean<OidcAuthenticationFilter> registration = new FilterRegistrationBean<>();
         OidcAuthenticationFilter authenticationFilter = new OidcAuthenticationFilter(
-                Arrays.asList(fromConfig(openAmConfig), fromConfig(azureAdConfig))
+                fromConfigs(
+                        openAmAuthConfig(properties),
+                        azureAdAuthConfig(properties),
+                        openAmStsAuthConfig(properties),
+                        naisStsAuthConfig(properties)
+                )
         );
-
         registration.setFilter(authenticationFilter);
         registration.setOrder(1);
         registration.addUrlPatterns("/api/*");
         return registration;
     }
+
 
     @Bean
     public FilterRegistrationBean logFilterRegistrationBean() {
