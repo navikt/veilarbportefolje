a8c93c3892775c4c53b63f87c956c5d594e5f4cd
Peder Korsveien <peder.korsveien@nav.no>
2020-08-12 11:46:22 +0200

    Bruk JAXB_CONTEXT_FACTORY

--- src/main/java/no/nav/pto/veilarbportefolje/VeilarbportefoljeApp.java
+++ src/main/java/no/nav/pto/veilarbportefolje/VeilarbportefoljeApp.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import static javax.xml.bind.JAXBContext.JAXB_CONTEXT_FACTORY;
+
 
 @SpringBootApplication
 @ServletComponentScan
@@ -11,7 +13,7 @@ public class VeilarbportefoljeApp {
 
     public static void main(String... args) {
         System.setProperty("oppfolging.feed.brukertilgang", "srvveilarboppfolging");
-        System.setProperty("javax.xml.bind.JAXBContextFactory", "com.sun.xml.bind.v2.JAXBContextFactory");
+        System.setProperty(JAXB_CONTEXT_FACTORY, "com.sun.xml.bind.v2.JAXBContextFactory");
 
         SpringApplication.run(VeilarbportefoljeApp.class, args);
     }
