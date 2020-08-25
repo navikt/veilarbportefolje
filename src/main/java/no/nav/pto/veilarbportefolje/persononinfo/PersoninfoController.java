8d259dd69f9d08a48e2a003a960c63a113dae938
Peder Korsveien <peder.korsveien@nav.no>
2020-08-25 11:00:58 +0200

    KZ-0 Fjern jax-rs fra PersonInfoController

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PersoninfoController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PersoninfoController.java
@@ -9,13 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.*;
-
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
 @RestController
 @RequestMapping("/api/personinfo")
-@Produces(APPLICATION_JSON)
 public class PersoninfoController {
     private final AuthService authService;
     private final PersonRepository personRepository;
@@ -29,7 +24,7 @@ public class PersoninfoController {
     @GetMapping("/{fnr}")
     public Personinfo hentPersoninfo(@PathVariable("fnr") String fnr) {
         authService.tilgangTilBruker(fnr);
-        return personRepository.hentPersoninfoForFnr(Fnr.of(fnr))
-                .orElseThrow(() -> new NotFoundException("Kunne ikke finne personinfo for: " + fnr));
+        return personRepository.hentPersoninfoForFnr(Fnr.of(fnr)).orElseThrow();
     }
+
 }
