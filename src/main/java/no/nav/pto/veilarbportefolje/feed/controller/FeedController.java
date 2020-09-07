9465def8f49e7e29ef3c9b58249e7fe7a1133a3f
Peder Korsveien <peder.korsveien@nav.no>
2020-09-07 13:12:07 +0200

    KZ-0 KZ-0 Fjern enda mer jax-rs

--- src/main/java/no/nav/pto/veilarbportefolje/feed/controller/FeedController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/feed/controller/FeedController.java
@@ -1,18 +1,16 @@
 package no.nav.pto.veilarbportefolje.feed.controller;
 
-import no.nav.pto.veilarbportefolje.feed.common.Authorization;
 import no.nav.pto.veilarbportefolje.feed.consumer.FeedConsumer;
 import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.util.Optional.ofNullable;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
@@ -37,21 +35,18 @@ public class FeedController {
     }
 
     @RequestMapping(path = "{name}", method = HEAD)
-    public Response webhookCallback(@PathParam("name") String feedname) {
-        return ofNullable(feedname)
-                .map((name) -> consumers.get(name))
-                .map((consumer) -> authorizeRequest(consumer, feedname))
-                .map(FeedConsumer::webhookCallback)
-                .map((hadCallback) -> Response.status(hadCallback ? 200 : 404))
-                .orElse(Response.status(404))
-                .build();
-    }
+    public void webhookCallback(@PathVariable("name") String feedname) {
+        final FeedConsumer feedConsumer = consumers.get(feedname);
 
-    private <T extends Authorization> T authorizeRequest(T feed, String name) {
-        if (!feed.getAuthorizationModule().isRequestAuthorized(name)) {
-            throw new WebApplicationException(Response.Status.FORBIDDEN);
+        final boolean authorized = feedConsumer.getAuthorizationModule().isRequestAuthorized(feedname);
+        if (!authorized) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Consumer is not authorized to read feed");
         }
-        return feed;
+
+        final boolean callback = feedConsumer.webhookCallback();
+        if (!callback) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find webhook callback");
+        }
     }
 
 }
