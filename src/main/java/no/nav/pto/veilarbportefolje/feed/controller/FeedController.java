9db4563b3bbb71e949c2ca0a80d366aa16a91118
Peder Korsveien <peder.korsveien@nav.no>
2020-09-10 13:45:57 +0200

    Opprett consumer i konstruktøren til FeedController

--- src/main/java/no/nav/pto/veilarbportefolje/feed/controller/FeedController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/feed/controller/FeedController.java
@@ -3,6 +3,7 @@ package no.nav.pto.veilarbportefolje.feed.controller;
 import no.nav.pto.veilarbportefolje.feed.common.Authorization;
 import no.nav.pto.veilarbportefolje.feed.consumer.FeedConsumer;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.Optional.ofNullable;
+import static no.nav.pto.veilarbportefolje.domene.BrukerOppdatertInformasjon.FEED_NAME;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
@@ -23,17 +25,11 @@ import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 public class FeedController {
 
     private static final Logger LOG = getLogger(FeedController.class);
+    private final Map<String, FeedConsumer> consumers = new HashMap<>();
 
-    private Map<String, FeedConsumer> consumers = new HashMap<>();
-
-
-    public <DOMAINOBJECT extends Comparable<DOMAINOBJECT>> FeedController addFeed(String clientFeedname, FeedConsumer consumer) {
-        LOG.info("ny feed-klient. navn={}", clientFeedname);
-        consumers.put(clientFeedname, consumer);
-        return this;
-    }
-
-    public FeedController() {
+    @Autowired
+    public FeedController(FeedConsumer consumer) {
+        consumers.put(FEED_NAME, consumer);
         LOG.info("starter");
     }
 
