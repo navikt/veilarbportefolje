fc9fee27aed32d5099d7aff8451a57ac53c8606f
stianStensli <stian.horn.stensli@nav.no>
2020-11-04 12:27:30 +0100

    TC-175 Ny 'scheduled' metrikk.

--- src/main/java/no/nav/pto/veilarbportefolje/internal/InternalController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/internal/InternalController.java
@@ -6,10 +6,14 @@ import no.nav.common.health.selftest.SelfTestChecks;
 import no.nav.common.health.selftest.SelfTestUtils;
 import no.nav.common.health.selftest.SelftTestCheckResult;
 import no.nav.common.health.selftest.SelftestHtmlGenerator;
+import no.nav.common.metrics.InfluxClient;
+import no.nav.pto.veilarbportefolje.metrikker.MetricsUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +31,12 @@ public class InternalController {
     private final SelfTestChecks selfTestChecks;
 
     @Autowired
-    public InternalController(SelfTestChecks selfTestChecks) {
+    private final JdbcTemplate db;
+
+    @Autowired
+    public InternalController(SelfTestChecks selfTestChecks, JdbcTemplate db) {
         this.selfTestChecks = selfTestChecks;
+        this.db = db;
     }
 
     @GetMapping("/isReady")
@@ -55,5 +63,16 @@ public class InternalController {
                 .status(status)
                 .contentType(MediaType.TEXT_HTML)
                 .body(html);
+    }
+
+    // kl 1:00 hver dag
+    @Scheduled(cron = "0 * 1 * * *")
+    private void metrikkOppdatering() {
+        String sql =  "SELECT count(*) FROM AKTOERID_TO_PERSONID "
+                    + "WHERE PERSONID IN "
+                    + "(SELECT PERSONID FROM AKTOERID_TO_PERSONID GROUP BY PERSONID HAVING max(GJELDENE) = 0)";
+        MetricsUtils.timed("portefolje.metrikker.usermapping", () -> {
+            db.queryForObject(sql, Integer.class);
+        }, new InfluxClient());
     }
 }
