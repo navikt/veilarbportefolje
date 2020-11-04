1cfc59eaeb38c333ab2c9a1c9405fd3c834f4598
Peder Korsveien <peder.korsveien@nav.no>
2020-11-04 13:45:47 +0100

    Fjern test for mappingjobb

--- src/main/java/no/nav/pto/veilarbportefolje/util/JobUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/JobUtils.java
@@ -1,17 +1,13 @@
 package no.nav.pto.veilarbportefolje.util;
 
-import no.nav.common.leaderelection.LeaderElectionClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.lang.invoke.MethodHandles;
 import java.net.UnknownHostException;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static java.net.InetAddress.getLocalHost;
 import static no.nav.common.utils.IdUtils.generateId;
@@ -21,14 +17,6 @@ public class JobUtils {
     private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static String MDC_JOB_ID = "jobId";
-
-    public static Optional<RunningJob> runAsyncJobOnLeader(Runnable runnable, LeaderElectionClient leaderElectionClient) {
-        if (!leaderElectionClient.isLeader()) {
-            return Optional.empty();
-        }
-        RunningJob runningJob = runAsyncJob(runnable);
-        return Optional.of(runningJob);
-    }
 
     public static RunningJob runAsyncJob(Runnable runnable) {
         String jobId = generateId();
