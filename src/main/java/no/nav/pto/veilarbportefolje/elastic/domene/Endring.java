89df8efd9c4de9f27c4332af6b82233cb0a216ea
stianStensli <stian.horn.stensli@nav.no>
2021-01-15 11:02:09 +0100

    Tc-220 new endrings format i elastic

--- src/main/java/no/nav/pto/veilarbportefolje/elastic/domene/Endring.java
+++ src/main/java/no/nav/pto/veilarbportefolje/elastic/domene/Endring.java
@@ -1,6 +1,5 @@
 package no.nav.pto.veilarbportefolje.elastic.domene;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,13 +8,4 @@ import lombok.experimental.Accessors;
 public class Endring {
     private String aktivtetId;
     private String tidspunkt;
-
-    public Endring(){
-
-    }
-
-    @JsonCreator
-    public Endring(String tidspunkt){
-        this.tidspunkt = tidspunkt;
-    }
 }
