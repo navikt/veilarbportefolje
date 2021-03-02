c1f4696d549d249e755aaa0fcd5ec3e667d6bd80
stianStensli <stian.horn.stensli@nav.no>
2021-03-02 10:56:50 +0100

    debug

--- src/main/java/no/nav/pto/veilarbportefolje/sistelest/SistLestService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/sistelest/SistLestService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.AktorId;
 import no.nav.pto.veilarbportefolje.domene.value.VeilederId;
+import no.nav.pto.veilarbportefolje.elastic.ElasticServiceV2;
 import no.nav.pto.veilarbportefolje.kafka.KafkaConsumerService;
 import no.nav.pto.veilarbportefolje.service.BrukerService;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import static no.nav.common.json.JsonUtils.fromJson;
 public class SistLestService implements KafkaConsumerService<String> {
     private final SistLestRepository sistLestRepository;
     private final BrukerService brukerService;
+    private final ElasticServiceV2 elasticServiceV2;
     private final AtomicBoolean rewind = new AtomicBoolean(false);
 
     @Override
@@ -26,12 +28,13 @@ public class SistLestService implements KafkaConsumerService<String> {
         SistLestKafkaMelding melding = fromJson(kafkaMelding, SistLestKafkaMelding.class);
         log.info("Aktivitetsplanen for {} ble lest av {}, lest: {}", melding.getAktorId(), melding.getVeilederId(), melding.getHarLestTidspunkt());
         Optional<VeilederId> veilederId = brukerService.hentVeilederForBruker(melding.aktorId);
-        veilederId.ifPresent(currentVeileder -> {
-            if (currentVeileder.equals(melding.getVeilederId())) {
-                sistLestRepository.upsert(melding);
-            }
-            // TODO: update elastic
-        });
+        if(veilederId.isEmpty()){
+            return;
+        }
+        if (veilederId.get().equals(melding.getVeilederId())) {
+            sistLestRepository.upsert(melding);
+            //elasticServiceV2.updateSistLest(melding);
+        }
     }
 
     public void slettSistLest(AktorId aktorId) {
