1ab527d3d72d9579840132054121c54cccb6f706
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-03-19 20:33:25 +0100

    Fix metrics reporter

--- src/main/java/no/nav/pto/veilarbportefolje/elastic/MetricsReporter.java
+++ src/main/java/no/nav/pto/veilarbportefolje/elastic/MetricsReporter.java
@@ -24,8 +24,8 @@ public class MetricsReporter {
     public MetricsReporter(ElasticIndexer elasticIndexer) {
         this.elasticIndexer = elasticIndexer;
 
+        Gauge.builder("veilarbelastic_number_of_docs", ()->(double)ElasticUtils.getCount()).register(getMeterRegistry());
         Gauge.builder("portefolje_indeks_sist_opprettet", this::sjekkIndeksSistOpprettet).register(getMeterRegistry());
-        Gauge.builder("veilarbelastic_number_of_docs", ElasticUtils::getCount).register(getMeterRegistry());
     }
 
     private Number sjekkIndeksSistOpprettet() {
