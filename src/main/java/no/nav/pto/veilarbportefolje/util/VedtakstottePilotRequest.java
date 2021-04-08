9021a783c17986ce3684f930626cabd6d8a25f5d
stianStensli <stian.horn.stensli@nav.no>
2021-04-08 12:53:51 +0200

    Tc-304 La til enhetId i kallet

--- src/main/java/no/nav/pto/veilarbportefolje/util/VedtakstottePilotRequest.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/VedtakstottePilotRequest.java
@@ -1,6 +1,7 @@
 package no.nav.pto.veilarbportefolje.util;
 
 import no.nav.common.rest.client.RestUtils;
+import no.nav.common.types.identer.EnhetId;
 import no.nav.common.utils.UrlUtils;
 import no.nav.pto.veilarbportefolje.auth.AuthUtils;
 import okhttp3.OkHttpClient;
@@ -24,17 +25,20 @@ public class VedtakstottePilotRequest {
         this.baseURL = (isProduction().orElse(false)) ? "https://app.adeo.no/" : "https://app-q1.dev.adeo.no/";
     }
 
-    public boolean erVedtakstottePilotPa(){
+    public boolean erVedtakstottePilotPa(EnhetId enhetId) {
+        if (enhetId == null) {
+            return false;
+        }
         Request request = new Request.Builder()
-                .url(UrlUtils.joinPaths(baseURL, "/veilarbvedtaksstotte/api/utrulling/tilhorerVeilederUtrulletKontor"))
+                .url(UrlUtils.joinPaths(baseURL, "/veilarbvedtaksstotte/api/utrulling/" + enhetId.get()))
                 .header(HttpHeaders.ACCEPT, MEDIA_TYPE_JSON.toString())
-                .header("Authorization","Bearer " + AuthUtils.getInnloggetBrukerToken())
+                .header("Authorization", "Bearer " + AuthUtils.getInnloggetBrukerToken())
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
             throwIfNotSuccessful(response);
             return RestUtils.parseJsonResponseOrThrow(response, Boolean.class);
-        }catch ( Exception exception){
+        } catch (Exception exception) {
             return false;
         }
     }
