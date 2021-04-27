0a2d77ef763871928ad063ba48130ce9a8d6e6c8
stianStensli <stian.horn.stensli@nav.no>
2021-04-27 10:14:42 +0200

    api url

--- src/main/java/no/nav/pto/veilarbportefolje/config/DatabaseConfig.java
+++ src/main/java/no/nav/pto/veilarbportefolje/config/DatabaseConfig.java
@@ -1,11 +1,13 @@
 package no.nav.pto.veilarbportefolje.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
+@Configuration
 public interface DatabaseConfig {
 
     DataSource dataSource();
