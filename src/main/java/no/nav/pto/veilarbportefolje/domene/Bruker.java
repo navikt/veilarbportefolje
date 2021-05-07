d07db3f0da7ab7bb36dc1664184d46759d6261c2
emildohlenhansen <emil.dohlen.hansen@bekk.no>
2021-05-07 15:45:55 +0200

    Oppdater tester og legg til safeBool

--- src/main/java/no/nav/pto/veilarbportefolje/domene/Bruker.java
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/Bruker.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.arbeidsliste.Arbeidsliste;
 import no.nav.pto.veilarbportefolje.elastic.domene.Endring;
 import no.nav.pto.veilarbportefolje.elastic.domene.OppfolgingsBruker;
+import no.nav.pto.veilarbportefolje.postgres.PostgresUtils;
 import no.nav.pto.veilarbportefolje.util.FodselsnummerUtils;
 import no.nav.pto.veilarbportefolje.util.OppfolgingUtils;
 
@@ -234,27 +235,26 @@ public class Bruker {
         boolean erSykmeldtMedArbeidsgiver = OppfolgingUtils.erSykmeldtMedArbeidsgiver(formidlingsgruppekode, kvalifiseringsgruppekode);
         return
                 setFnr((String) row.get(fodselsnummer))
-                        .setNyForVeileder((boolean) row.get(NY_FOR_VEILEDER))
+                        .setNyForVeileder(PostgresUtils.safeBool((boolean) row.get(NY_FOR_VEILEDER)))
                         .setTrengerVurdering(trengerVurdering)
                         .setErSykmeldtMedArbeidsgiver(erSykmeldtMedArbeidsgiver) // Etiketten sykemeldt ska vises oavsett om brukeren har ett påbegynnt vedtak eller ej;
                         .setFornavn((String) row.get(FORNAVN))
                         .setEtternavn((String) row.get(ETTERNAVN))
                         .setVeilederId((String) row.get(VEILEDERID))
                         .setDiskresjonskode(("7".equals(diskresjonskode) || "6".equals(diskresjonskode)) ? diskresjonskode : null)
-                        .setEgenAnsatt((boolean) row.get(SPERRET_ANSATT))
-                        .setErDoed((boolean) row.get(ER_DOED))
+                        .setEgenAnsatt(PostgresUtils.safeBool((boolean) row.get(SPERRET_ANSATT)))
+                        .setErDoed(PostgresUtils.safeBool((boolean) row.get(ER_DOED)))
                         .setSikkerhetstiltak(sikkerhetstiltak == null ? new ArrayList<>() : Collections.singletonList(sikkerhetstiltak))
                         .setFodselsdato(toLocalDateTimeOrNull(FodselsnummerUtils.lagFodselsdato(fodselsnummer)))
                         .setKjonn(FodselsnummerUtils.lagKjonn(fodselsnummer))
                         .setVenterPaSvarFraNAV(toLocalDateTimeOrNull((Timestamp) row.get(VENTER_PA_NAV)))
                         .setVenterPaSvarFraBruker(toLocalDateTimeOrNull((Timestamp) row.get(VENTER_PA_BRUKER)))
                         .setVedtakStatus(VEDTAKSTATUS)
-                        .setVedtakStatusEndret(toLocalDateTimeOrNull(VEDTAKSTATUS_ENDRET_TIDSPUNKT))
+                        .setVedtakStatusEndret(toLocalDateTimeOrNull((Timestamp) row.get(VEDTAKSTATUS_ENDRET_TIDSPUNKT)))
                         .setOppfolgingStartdato(toLocalDateTimeOrNull((Timestamp) row.get(STARTDATO)))
                         .setAnsvarligVeilederForVedtak(VEDTAKSTATUS_ANSVARLIG_VEILDERNAVN)
                         .setOppfolgingStartdato(toLocalDateTimeOrNull((Timestamp) row.get(STARTDATO)))
                         .setTrengerRevurdering(trengerRevurdering);
-
         //TODO: utledd manuell
     }
     // TODO: sjekk om disse feltene er i bruk, de kan være nødvendige for statuser eller filtere
@@ -268,4 +268,5 @@ public class Bruker {
         public static final String SIKKERHETSTILTAK_TYPE_KODE = "SIKKERHETSTILTAK_TYPE_KODE";
         public static final String HAR_OPPFOLGINGSSAK = "HAR_OPPFOLGINGSSAK";
      */
+
 }
