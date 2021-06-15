308681a3c68d9e5b878fd9d0005a1e144d227ed2
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-06-15 16:40:37 +0200

    Bugfix

--- src/main/java/no/nav/pto/veilarbportefolje/service/UnleashService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/service/UnleashService.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import no.nav.common.featuretoggle.UnleashClient;
 import no.nav.common.health.HealthCheck;
 import no.nav.common.health.HealthCheckResult;
-import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
 public class UnleashService implements HealthCheck {
     private final UnleashClient unleashClient;
