68eb6fff48ae2ccc4c0b697b91441a5eea63c06d
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-06-15 16:26:20 +0200

    Bugfix

--- src/main/java/no/nav/pto/veilarbportefolje/service/UnleashService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/service/UnleashService.java
@@ -1,21 +1,15 @@
 package no.nav.pto.veilarbportefolje.service;
 
+import lombok.RequiredArgsConstructor;
 import no.nav.common.featuretoggle.UnleashClient;
 import no.nav.common.health.HealthCheck;
 import no.nav.common.health.HealthCheckResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UnleashService implements HealthCheck {
-
     private final UnleashClient unleashClient;
-
-    @Autowired
-    public UnleashService(UnleashClient unleashClient) {
-        this.unleashClient = unleashClient;
-    }
-
 
     public boolean isEnabled(String featureToggle) {
         return this.unleashClient.isEnabled(featureToggle);
