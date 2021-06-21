794b30f116477f732605d5f8197b14c727748707
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-06-21 10:32:10 +0200

    Add import

--- src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.elastic.domene.OppfolgingsBruker;
 import no.nav.pto.veilarbportefolje.vedtakstotte.KafkaVedtakStatusEndring;
+import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
 import org.apache.commons.lang3.text.WordUtils;
 
 import javax.sql.DataSource;
