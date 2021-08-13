3510259b414703a24c000e46aef67f28828e730a
stianStensli <stian.horn.stensli@nav.no>
2021-08-13 12:02:47 +0200

    Tc-100 kommentar clean up

--- src/main/java/no/nav/pto/veilarbportefolje/arenaaktiviteter/arenaDTO/TiltakStatuser.java
+++ src/main/java/no/nav/pto/veilarbportefolje/arenaaktiviteter/arenaDTO/TiltakStatuser.java
@@ -3,9 +3,7 @@ package no.nav.pto.veilarbportefolje.arenaaktiviteter.arenaDTO;
 import java.util.List;
 
 /**
- GodkjenteStatuser: Aktuell (AKTUELL), Gjennomføres (GJENN), Informasjonsmøte (INFOMOETE), Takket ja til tilbud (JATAKK), Godkjent tiltaksplass (TILBUD), Venteliste (VENTELISTE)
- *  AKTUELL gjelder kun for Arbeidsmarkedopplæring (AMO) og er da unntatt dersom det gjelder Individuelt tiltak (IND) eller Institusjonelt tiltak (INST)
- *
+ * Tiltaks topicen hjelper ogsa til med aa filtere ut de tiltakene som ikke skal i oversikten
  * @link https://confluence.adeo.no/pages/viewpage.action?pageId=409961201
  */
 public class TiltakStatuser {
