919ea0458315f492f8a1408fb608130478639d1f
stianStensli <stian.horn.stensli@nav.no>
2021-08-27 16:55:18 +0200

    Tc-100 Oppdatert til SAK_ID

--- src/main/java/no/nav/pto/veilarbportefolje/arenapakafka/arenaDTO/YtelseAAPDTO.java
+++
@@ -1,9 +0,0 @@
-package no.nav.pto.veilarbportefolje.arenapakafka.arenaDTO;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public class YtelseAAPDTO extends YtelsesInnhold{
-    @JsonAlias("ANTALLUKERIGJEN") Integer antallUkerIgjen;
-    @JsonAlias("ANTALLDAGERIGJEN") Integer antallDagerIgjen;
-    @JsonAlias("ANTALLDAGERIGJENUNNTAK") Integer antallDagerIgjenUnntak;
-}
