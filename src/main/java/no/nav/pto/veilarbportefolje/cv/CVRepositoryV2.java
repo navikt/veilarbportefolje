2def79baa6d2200536f9436932501b29386445a4
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-08-27 14:27:21 +0200

    Fix cv service

--- src/main/java/no/nav/pto/veilarbportefolje/cv/CVRepositoryV2.java
+++ src/main/java/no/nav/pto/veilarbportefolje/cv/CVRepositoryV2.java
@@ -1,8 +1,9 @@
 package no.nav.pto.veilarbportefolje.cv;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.AktorId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,18 +14,14 @@ import java.util.Optional;
 import static java.time.Instant.now;
 import static no.nav.pto.veilarbportefolje.database.PostgresTable.BRUKER_CV.*;
 import static no.nav.pto.veilarbportefolje.postgres.PostgresUtils.queryForObjectOrNull;
-import static no.nav.pto.veilarbportefolje.util.DbUtils.boolToJaNei;
 
 @Slf4j
 @Repository
+@RequiredArgsConstructor
 public class CVRepositoryV2 {
-
+    @NonNull
+    @Qualifier("PostgresJdbc")
     private final JdbcTemplate db;
-
-    @Autowired
-    public CVRepositoryV2(@Qualifier("PostgresJdbc") JdbcTemplate db) {
-        this.db = db;
-    }
 
     public int upsertHarDeltCv(AktorId aktoerId, boolean harDeltCv) {
         log.info("Oppdater delt CV for bruker: {}, harDeltCV: {}", aktoerId.get(), harDeltCv);
@@ -34,9 +31,9 @@ public class CVRepositoryV2 {
                         "ON CONFLICT (" + AKTOERID + ") " +
                         "DO UPDATE SET (" + HAR_DELT_CV + ", " + SISTE_MELDING_MOTTATT + ") = (?, ?)",
                 aktoerId.get(),
-                boolToJaNei(harDeltCv),
+                harDeltCv,
                 Timestamp.from(now()),
-                boolToJaNei(harDeltCv),
+                harDeltCv,
                 Timestamp.from(now()));
     }
 
@@ -48,9 +45,9 @@ public class CVRepositoryV2 {
                         "ON CONFLICT (" + AKTOERID + ") " +
                         "DO UPDATE SET (" + CV_EKSISTERER + ", " + SISTE_MELDING_MOTTATT + ") = (?, ?)",
                 aktoerId.get(),
-                boolToJaNei(cvEksisterer),
+                cvEksisterer,
                 Timestamp.from(now()),
-                boolToJaNei(cvEksisterer),
+                cvEksisterer,
                 Timestamp.from(now()));
     }
 
@@ -76,7 +73,7 @@ public class CVRepositoryV2 {
                 "UPDATE %s SET %s = ? WHERE %s = ?",
                 TABLE_NAME, HAR_DELT_CV, AKTOERID
         );
-        return db.update(updateSql, boolToJaNei(false), aktoerId.get());
+        return db.update(updateSql, false, aktoerId.get());
     }
 
     public int slettCVData(AktorId aktoerId) {
