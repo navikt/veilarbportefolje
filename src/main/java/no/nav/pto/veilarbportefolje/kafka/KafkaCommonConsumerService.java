ea14066dc26ed9a7dd0ae06f07d046055e349656
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-09-01 14:32:33 +0200

    Changes based on code review

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonConsumerService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonConsumerService.java
@@ -1,32 +1,22 @@
 package no.nav.pto.veilarbportefolje.kafka;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.service.UnleashService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.util.concurrent.TimeUnit;
-
-import static no.nav.common.client.utils.CacheUtils.tryCacheFirst;
 import static no.nav.pto.veilarbportefolje.config.FeatureToggle.NY_KAFKA_COMMON_LIB;
 
 @Slf4j
 public abstract class KafkaCommonConsumerService<T> {
-    private final Cache<String, Boolean> unleashServiceCache = Caffeine.newBuilder()
-            .expireAfterWrite(30, TimeUnit.SECONDS)
-            .maximumSize(10)
-            .build();
-
     protected boolean isNyKafkaLibraryEnabled() {
-        return tryCacheFirst(unleashServiceCache, NY_KAFKA_COMMON_LIB, () -> getUnleashService().isEnabled(NY_KAFKA_COMMON_LIB));
+        return getUnleashService().isEnabled(NY_KAFKA_COMMON_LIB);
     }
 
     public void behandleKafkaRecord(ConsumerRecord<String, T> kafkaMelding) {
         if (!isNyKafkaLibraryEnabled()) {
             return;
         }
-        
+
         log.info(
                 "Behandler kafka-melding med key: {} og offset: {}, og partition: {} på topic {}",
                 kafkaMelding.key(),
