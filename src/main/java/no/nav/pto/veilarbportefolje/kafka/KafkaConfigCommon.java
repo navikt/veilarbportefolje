e49ca2bee12c793aa51046a86da3c327bf8a8fbc
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-09-08 12:16:58 +0200

    Fix aiven avro deserialization

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaConfigCommon.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaConfigCommon.java
@@ -165,7 +165,7 @@ public class KafkaConfigCommon {
                                 .withConsumerConfig(
                                         Topic.AIVEN_REGISTRERING_TOPIC.topicName,
                                         Deserializers.stringDeserializer(),
-                                        KafkaAivenAvroDeserializer.deserializer(),
+                                        Deserializers.aivenAvroDeserializer(),
                                         registreringService::behandleKafkaRecord
                                 ),
                         new KafkaConsumerClientBuilder.TopicConfig<String, ArbeidssokerProfilertEvent>()
@@ -175,7 +175,7 @@ public class KafkaConfigCommon {
                                 .withConsumerConfig(
                                         Topic.AIVEN_PROFILERING_TOPIC.topicName,
                                         Deserializers.stringDeserializer(),
-                                        KafkaAivenAvroDeserializer.deserializer(),
+                                        Deserializers.aivenAvroDeserializer(),
                                         profileringService::behandleKafkaRecord
                                 )
                 );
