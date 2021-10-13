6a1d9fca3d3e472e55a88db38dfe1135f1631a73
stianStensli <stian.horn.stensli@nav.no>
2021-10-13 11:56:10 +0200

    java 17 casting fix

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/AivenAvroDeserializer.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/AivenAvroDeserializer.java
@@ -1,17 +1,17 @@
 package no.nav.pto.veilarbportefolje.kafka.deserializers;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
-import no.nav.common.kafka.consumer.util.deserializer.AvroDeserializer;
 import no.nav.common.kafka.consumer.util.deserializer.Deserializers;
 import no.nav.common.utils.EnvironmentUtils;
+import org.apache.kafka.common.serialization.Deserializer;
 
 import java.util.Map;
 
 public class AivenAvroDeserializer<T> {
     private static final String KAFKA_SCHEMAS_URL = EnvironmentUtils.getRequiredProperty("KAFKA_SCHEMAS_URL");
 
-    public AvroDeserializer<T> getDeserializer() {
-        AvroDeserializer<T> avroDeserializer = (AvroDeserializer<T>) Deserializers.aivenAvroDeserializer();
+    public Deserializer<T> getDeserializer() {
+        Deserializer<T> avroDeserializer = Deserializers.aivenAvroDeserializer();
         avroDeserializer.configure(Map.of(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, KAFKA_SCHEMAS_URL,
                 KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true), false);
         return avroDeserializer;
