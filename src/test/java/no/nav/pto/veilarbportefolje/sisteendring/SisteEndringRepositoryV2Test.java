83ff60875e0d905478e22f0410bb0e4c5399acac
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-10-15 16:24:58 +0200

    Uniform datetime precision

--- src/test/java/no/nav/pto/veilarbportefolje/sisteendring/SisteEndringRepositoryV2Test.java
+++ src/test/java/no/nav/pto/veilarbportefolje/sisteendring/SisteEndringRepositoryV2Test.java
@@ -3,6 +3,7 @@ package no.nav.pto.veilarbportefolje.sisteendring;
 import no.nav.common.types.identer.AktorId;
 import no.nav.pto.veilarbportefolje.elastic.domene.Endring;
 import no.nav.pto.veilarbportefolje.elastic.domene.OppfolgingsBruker;
+import no.nav.pto.veilarbportefolje.util.DateUtils;
 import no.nav.pto.veilarbportefolje.util.SingletonPostgresContainer;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,7 +11,6 @@ import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Timestamp;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
@@ -35,7 +35,7 @@ public class SisteEndringRepositoryV2Test {
 
     @Test
     public void mapDbTilOppfolgingsbruker() {
-        ZonedDateTime zonedDateTime_1 = ZonedDateTime.now(ZoneId.of("Europe/Oslo"));
+        ZonedDateTime zonedDateTime_1 = DateUtils.now();
         String tidspunkt_1 = zonedDateTime_1.toOffsetDateTime().toString();
         SisteEndringDTO dto_1 = new SisteEndringDTO()
                 .setTidspunkt(ZonedDateTime.parse(tidspunkt_1))
@@ -43,7 +43,7 @@ public class SisteEndringRepositoryV2Test {
                 .setAktoerId(AKTORID)
                 .setAktivtetId("1");
 
-        ZonedDateTime zonedDateTime_2 = ZonedDateTime.now(ZoneId.of("Europe/Oslo")).minusDays(3);
+        ZonedDateTime zonedDateTime_2 = DateUtils.now().minusDays(3);
         String tidspunkt_2 = zonedDateTime_2.toOffsetDateTime().toString();
         SisteEndringDTO dto_2 = new SisteEndringDTO()
                 .setTidspunkt(ZonedDateTime.parse(tidspunkt_2))
@@ -69,14 +69,14 @@ public class SisteEndringRepositoryV2Test {
 
     @Test
     public void slettSisteEndringer() {
-        ZonedDateTime zonedDateTime_1 = ZonedDateTime.now(ZoneId.of("Europe/Oslo"));
+        ZonedDateTime zonedDateTime_1 = DateUtils.now();
         String tidspunkt_1 = zonedDateTime_1.toOffsetDateTime().toString();
         SisteEndringDTO dto_1 = new SisteEndringDTO()
                 .setTidspunkt(ZonedDateTime.parse(tidspunkt_1))
                 .setKategori(SisteEndringsKategori.FULLFORT_STILLING)
                 .setAktoerId(AKTORID);
 
-        ZonedDateTime zonedDateTime_2 = ZonedDateTime.now(ZoneId.of("Europe/Oslo")).minusDays(3);
+        ZonedDateTime zonedDateTime_2 = DateUtils.now().minusDays(3);
         String tidspunkt_2 = zonedDateTime_2.toOffsetDateTime().toString();
         SisteEndringDTO dto_2 = new SisteEndringDTO()
                 .setTidspunkt(ZonedDateTime.parse(tidspunkt_2))
