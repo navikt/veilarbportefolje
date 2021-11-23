f9c41fff04e18f81122fa840ea55d402134c2cbc
stianStensli <stian.horn.stensli@nav.no>
2021-11-23 08:37:36 +0100

    Fjernet cronjob migrering

--- src/main/java/no/nav/pto/veilarbportefolje/config/SchedulConfig.java
+++ src/main/java/no/nav/pto/veilarbportefolje/config/SchedulConfig.java
@@ -3,7 +3,6 @@ package no.nav.pto.veilarbportefolje.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.job.leader_election.LeaderElectionClient;
-import no.nav.pto.veilarbportefolje.arbeidsliste.ArbeidslisteService;
 import no.nav.pto.veilarbportefolje.arenapakafka.ytelser.YtelsesService;
 import no.nav.pto.veilarbportefolje.database.BrukerAktiviteterService;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +20,6 @@ public class SchedulConfig implements SchedulingConfigurer{
     private final BrukerAktiviteterService brukerAktiviteterService;
     private final YtelsesService ytelsesService;
     private final LeaderElectionClient leaderElectionClient;
-    private final ArbeidslisteService arbeidslisteService;
 
     // Denne jobben må kjøre etter midnatt
     @Scheduled(cron = "0 1 0 * * ?")
@@ -40,16 +38,6 @@ public class SchedulConfig implements SchedulingConfigurer{
             ytelsesService.oppdaterBrukereMedYtelserSomStarterIDagOracle();
         } else {
             log.info("Starter ikke jobb: oppdaterYtelser");
-        }
-    }
-
-    @Scheduled(cron = "0 0 3 * * ?")
-    public void migrerArbeidslistaTilPostgresCron() {
-        if (leaderElectionClient.isLeader()) {
-            log.info("Starter jobb: migrerArbeidslistaTilPostgres");
-            arbeidslisteService.migrerArbeidslistaTilPostgres();
-        } else {
-            log.info("Starter ikke jobb: migrerArbeidslistaTilPostgres");
         }
     }
 
