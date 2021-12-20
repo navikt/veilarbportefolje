9e38461f9cb8595446a450ec7fea66e297818f52
Dragutin Vujovic <dragutin.vujovic@nav.no>
2021-12-20 17:14:24 +0100

    Add admin controll for ES index

--- src/main/java/no/nav/pto/veilarbportefolje/elastic/ElasticUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/elastic/ElasticUtils.java
@@ -1,6 +1,9 @@
 package no.nav.pto.veilarbportefolje.elastic;
 
-import lombok.SneakyThrows;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.elastic.domene.ElasticClientConfig;
 import org.apache.http.HttpHost;
@@ -9,30 +12,24 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
-import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
-import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.xcontent.XContentType;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static no.nav.pto.veilarbportefolje.elastic.ElasticConfig.BRUKERINDEKS_ALIAS;
-import static org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest.AliasActions.Type.ADD;
-import static org.elasticsearch.client.RequestOptions.DEFAULT;
 
 @Slf4j
 public class ElasticUtils {
 
     private static final int SOCKET_TIMEOUT = 120_000;
     private static final int CONNECT_TIMEOUT = 60_000;
+
+    public static ElasticsearchClient createESClient(ElasticClientConfig config) {
+        RestClient restClient = RestClient.builder(
+                new HttpHost(config.getHostname(), config.getPort())).build();
+
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
 
     public static RestHighLevelClient createClient(ElasticClientConfig config) {
         HttpHost httpHost = new HttpHost(
@@ -75,49 +72,6 @@ public class ElasticUtils {
                 return provider;
             }
         };
-    }
-
-
-    @SneakyThrows
-    public static void opprettNyIndeks(RestHighLevelClient restHighLevelClient) {
-        Path elasticSettingsPath = Paths.get("src", "test", "resources", "elastic_settings.json");
-        String json = Files.readString(elasticSettingsPath).trim();
-        String navn = createIndexName();
-
-        CreateIndexRequest request = new CreateIndexRequest(navn)
-                .source(json, XContentType.JSON);
-
-        CreateIndexResponse response = restHighLevelClient.indices().create(request, DEFAULT);
-
-        if (!response.isAcknowledged()) {
-            log.error("Kunne ikke opprette ny indeks {}", navn);
-            throw new RuntimeException();
-        }
-
-        //do: hovedindeksering
-
-        opprettAliasForIndeks(navn, restHighLevelClient);
-    }
-
-    public static String createIndexName() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmm");
-        String timestamp = LocalDateTime.now().format(formatter);
-        return String.format("%s_%s", BRUKERINDEKS_ALIAS, timestamp);
-    }
-
-    @SneakyThrows
-    private static void opprettAliasForIndeks(String indeks, RestHighLevelClient restHighLevelClient) {
-        IndicesAliasesRequest.AliasActions addAliasAction = new IndicesAliasesRequest.AliasActions(ADD)
-                .index(indeks)
-                .alias(BRUKERINDEKS_ALIAS);
-
-        IndicesAliasesRequest request = new IndicesAliasesRequest().addAliasAction(addAliasAction);
-        AcknowledgedResponse response = restHighLevelClient.indices().updateAliases(request, DEFAULT);
-
-        if (!response.isAcknowledged()) {
-            log.error("Kunne ikke legge til alias {}", BRUKERINDEKS_ALIAS);
-            throw new RuntimeException();
-        }
     }
 
 }
