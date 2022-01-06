2bcc3150de3a6fd3ab9254510d7db876f12d195c
stianStensli <stian.horn.stensli@nav.no>
2022-01-06 11:37:56 +0100

    Bruk av opensearch GenericContainer

--- src/test/java/no/nav/pto/veilarbportefolje/config/ApplicationConfigTest.java
+++ src/test/java/no/nav/pto/veilarbportefolje/config/ApplicationConfigTest.java
@@ -6,7 +6,6 @@ import no.nav.common.job.leader_election.LeaderElectionClient;
 import no.nav.common.metrics.MetricsClient;
 import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.common.utils.Credentials;
-import no.nav.common.utils.IdUtils;
 import no.nav.pto.veilarbportefolje.aktiviteter.AktivitetDAO;
 import no.nav.pto.veilarbportefolje.aktiviteter.AktivitetService;
 import no.nav.pto.veilarbportefolje.aktiviteter.AktivitetStatusRepositoryV2;
@@ -79,7 +78,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
 
@@ -146,17 +146,22 @@ import static org.mockito.Mockito.when;
 })
 public class ApplicationConfigTest {
 
-    private static final ElasticsearchContainer ELASTICSEARCH_CONTAINER;
-    private static final String ELASTICSEARCH_VERSION = "7.16.1";
+    private static final GenericContainer<?> OPENSEARCH_CONTAINER;
+    private static final String OPENSEARCH_VERSION = "1.2.3";
     private static final String ELASTICSEARCH_TEST_PASSWORD = "test";
     private static final String ELASTICSEARCH_TEST_USERNAME = "elastic";
 
     static {
-        ELASTICSEARCH_CONTAINER = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:" + ELASTICSEARCH_VERSION);
-        ELASTICSEARCH_CONTAINER.withPassword(ELASTICSEARCH_TEST_PASSWORD);
-        ELASTICSEARCH_CONTAINER.start();
-        System.setProperty("elastic.indexname", IdUtils.generateId());
-        System.setProperty("elastic.httphostaddress", ELASTICSEARCH_CONTAINER.getHttpHostAddress());
+        OPENSEARCH_CONTAINER = new GenericContainer<>(
+                DockerImageName.parse("opensearchproject/opensearch")
+                        .withTag(OPENSEARCH_VERSION)
+        );
+
+        OPENSEARCH_CONTAINER.withExposedPorts(9200);
+        OPENSEARCH_CONTAINER.withEnv("discovery.type", "single-node");
+        OPENSEARCH_CONTAINER.withEnv("DISABLE_INSTALL_DEMO_CONFIG", "true");
+        OPENSEARCH_CONTAINER.withEnv("DISABLE_SECURITY_PLUGIN", "true");
+        OPENSEARCH_CONTAINER.start();
     }
 
 
@@ -239,8 +244,8 @@ public class ApplicationConfigTest {
         ElasticClientConfig elasticTestConfig = ElasticClientConfig.builder()
                 .username(ELASTICSEARCH_TEST_USERNAME)
                 .password(ELASTICSEARCH_TEST_PASSWORD)
-                .hostname(ELASTICSEARCH_CONTAINER.getHost())
-                .port(ELASTICSEARCH_CONTAINER.getFirstMappedPort())
+                .hostname(OPENSEARCH_CONTAINER.getHost())
+                .port(OPENSEARCH_CONTAINER.getFirstMappedPort())
                 .scheme("http")
                 .build();
 
