9096be10be24bc85c2861446b1343bd8a9813bc9
stianStensli <stian.horn.stensli@nav.no>
2022-02-28 15:44:51 +0100

    fjerner bruk av spring versjon av db-schedule

--- src/main/java/no/nav/pto/veilarbportefolje/config/SchedulConfig.java
+++ src/main/java/no/nav/pto/veilarbportefolje/config/SchedulConfig.java
@@ -1,50 +1,69 @@
 package no.nav.pto.veilarbportefolje.config;
 
+import com.github.kagkarlsson.scheduler.Scheduler;
 import com.github.kagkarlsson.scheduler.task.Task;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.Schedules;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.arenapakafka.ytelser.YtelsesService;
 import no.nav.pto.veilarbportefolje.arenapakafka.ytelser.YtelsesServicePostgres;
 import no.nav.pto.veilarbportefolje.database.BrukerAktiviteterService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 import java.time.LocalTime;
 
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
 public class SchedulConfig {
     private final BrukerAktiviteterService brukerAktiviteterService;
     private final YtelsesService ytelsesService;
     private final YtelsesServicePostgres ytelsesServicePostgres;
+    private final Scheduler scheduler;
+
+    @Autowired
+    public SchedulConfig(@Qualifier("Postgres") DataSource dataSource,
+                         BrukerAktiviteterService brukerAktiviteterService,
+                         YtelsesServicePostgres ytelsesServicePostgres,
+                         YtelsesService ytelsesService) {
+        this.brukerAktiviteterService = brukerAktiviteterService;
+        this.ytelsesServicePostgres = ytelsesServicePostgres;
+        this.ytelsesService = ytelsesService;
+
+        scheduler = Scheduler.create(dataSource,
+                        oppdaterBrukerAktiviteter(), oppdaterNyeYtelser(),
+                        oppdaterNyeYtelserPostgres(), test())
+                .build();
+    }
 
     // Denne jobben må kjøre etter midnatt
-    @Bean
-    public Task<Void> oppdaterBrukerAktiviteter() {
+    private Task<Void> oppdaterBrukerAktiviteter() {
         return Tasks.recurring("indekserer_aktivitet_endringer", Schedules.daily(LocalTime.of(0, 1)))
                 .execute((instance, ctx) -> brukerAktiviteterService.syncAktivitetOgBrukerData());
     }
 
     // Denne jobben må kjøre etter midnatt
-    @Bean
-    public Task<Void> oppdaterNyeYtelser() {
+    private Task<Void> oppdaterNyeYtelser() {
         return Tasks.recurring("indekserer_ytelse_endringer", Schedules.daily(LocalTime.of(1, 0)))
                 .execute((instance, ctx) -> ytelsesService.oppdaterBrukereMedYtelserSomStarterIDagOracle());
     }
 
     // Denne jobben må kjøre etter midnatt
-    @Bean
-    public Task<Void> oppdaterNyeYtelserPostgres() {
+    private Task<Void> oppdaterNyeYtelserPostgres() {
         return Tasks.recurring("indekserer_ytelse_endringer_postgres", Schedules.daily(LocalTime.of(1, 1)))
                 .execute((instance, ctx) -> ytelsesServicePostgres.oppdaterBrukereMedYtelserSomStarterIDagPostgres());
     }
 
-    @Bean
-    public Task<Void> test() {
-        return Tasks.recurring("test", Schedules.daily(LocalTime.of(15, 0)))
+    private Task<Void> test() {
+        return Tasks.recurring("test", Schedules.daily(LocalTime.of(15, 50)))
                 .execute((instance, ctx) -> log.info("hello world: db-schedule"));
+    }
+
+    @PostConstruct
+    public void start() {
+        scheduler.start();
     }
 }
