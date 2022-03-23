078b0f16b0ef1f9b829d10d7839dcc348113d6a4
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-03-23 16:00:06 +0100

    Code refactoring

--- src/main/java/no/nav/pto/veilarbportefolje/oppfolging/SkjermingPersonerService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/oppfolging/SkjermingPersonerService.java
@@ -5,12 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.pto.veilarbportefolje.kafka.KafkaCommonConsumerService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SkjermingPersonerService extends KafkaCommonConsumerService<SkjermingDTO> {
     @Override
     protected void behandleKafkaMeldingLogikk(SkjermingDTO skjermingDTO) {
-        log.info("Skjerming personer, fra: " + skjermingDTO.getSkjermetFra() + ", til: " + skjermingDTO.getSkjermetTil());
+        LocalDateTime skjermetFra = null;
+        LocalDateTime skjermetTil = null;
+        if (skjermingDTO.getSkjermetFra() != null && skjermingDTO.getSkjermetFra().length == 6) {
+            skjermetFra = LocalDateTime.of(skjermingDTO.getSkjermetFra()[0], skjermingDTO.getSkjermetFra()[1], skjermingDTO.getSkjermetFra()[2], skjermingDTO.getSkjermetFra()[3], skjermingDTO.getSkjermetFra()[4], skjermingDTO.getSkjermetFra()[5]);
+        }
+        if (skjermingDTO.getSkjermetTil() != null && skjermingDTO.getSkjermetTil().length == 6) {
+            skjermetTil = LocalDateTime.of(skjermingDTO.getSkjermetTil()[0], skjermingDTO.getSkjermetTil()[1], skjermingDTO.getSkjermetTil()[2], skjermingDTO.getSkjermetTil()[3], skjermingDTO.getSkjermetTil()[4], skjermingDTO.getSkjermetTil()[5]);
+        }
+
+        log.info("Skjerming personer, fra: " + skjermetFra + ", til: " + skjermetTil);
     }
 }
