9e6d317165d078aee84d881b6a2ac648b3fdb9a3
stianStensli <stian.horn.stensli@nav.no>
2022-03-25 11:13:31 +0100

    toggler ved bruk av ident hvis mulig

--- src/main/java/no/nav/pto/veilarbportefolje/config/FeatureToggle.java
+++ src/main/java/no/nav/pto/veilarbportefolje/config/FeatureToggle.java
@@ -1,7 +1,12 @@
 package no.nav.pto.veilarbportefolje.config;
 
 
+import no.finn.unleash.UnleashContext;
+import no.nav.pto.veilarbportefolje.auth.AuthUtils;
 import no.nav.pto.veilarbportefolje.service.UnleashService;
+import org.springframework.web.server.ResponseStatusException;
+
+import static no.nav.common.featuretoggle.UnleashUtils.resolveUnleashContextFromSubject;
 
 public class FeatureToggle {
     private FeatureToggle() {
@@ -23,6 +28,12 @@ public class FeatureToggle {
     }
 
     public static boolean brukIkkeAvtalteMoter(UnleashService unleashService) {
-        return unleashService.isEnabled(FeatureToggle.IKKE_AVTALTE_MOTER);
+        try{
+            String userId = AuthUtils.getInnloggetVeilederIdent().toString();
+            return unleashService.isEnabled(FeatureToggle.IKKE_AVTALTE_MOTER, new UnleashContext(userId, null, null, resolveUnleashContextFromSubject().getProperties()));
+        } catch (ResponseStatusException exception){
+            // Dette er ok, ved feks. systemkall og under test
+            return unleashService.isEnabled(FeatureToggle.IKKE_AVTALTE_MOTER);
+        }
     }
 }
