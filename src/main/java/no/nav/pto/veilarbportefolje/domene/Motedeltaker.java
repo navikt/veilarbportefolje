c64f000dcd2ff0cffd7827d5995325f079ef3f2c
stianStensli <stian.horn.stensli@nav.no>
2022-03-30 11:49:01 +0200

    fix: stackoverflow json

--- src/main/java/no/nav/pto/veilarbportefolje/domene/Motedeltaker.java
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/Motedeltaker.java
@@ -1,5 +1,4 @@
 package no.nav.pto.veilarbportefolje.domene;
 
 public record Motedeltaker(String fornavn, String etternavn, String fnr){
-    public static Motedeltaker skjermetDeltaker = new Motedeltaker("", "", null);
 }
--- src/main/java/no/nav/pto/veilarbportefolje/domene/Moteplan.java
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/Moteplan.java
@@ -1,9 +1,7 @@
 package no.nav.pto.veilarbportefolje.domene;
 
-import static no.nav.pto.veilarbportefolje.domene.Motedeltaker.skjermetDeltaker;
-
 public record Moteplan (Motedeltaker deltaker, String dato, boolean avtaltMedNav) {
     public Moteplan getSkjermetMoteplan(){
-        return new Moteplan(skjermetDeltaker, this.dato, this.avtaltMedNav);
+        return new Moteplan(new Motedeltaker("", "", null), this.dato, this.avtaltMedNav);
     }
 }
