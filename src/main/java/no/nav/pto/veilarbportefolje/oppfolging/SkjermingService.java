8b5992b7d09260b914b7849fb5613762a97b05f8
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-04-20 11:01:00 +0200

    Bugfix for skjerming service

--- src/main/java/no/nav/pto/veilarbportefolje/oppfolging/SkjermingService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/oppfolging/SkjermingService.java
@@ -1,6 +1,7 @@
 package no.nav.pto.veilarbportefolje.oppfolging;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.Fnr;
 import no.nav.pto.veilarbportefolje.util.DateUtils;
@@ -19,39 +20,32 @@ public class SkjermingService {
     private final SkjermingRepository skjermingRepository;
 
 
+    @SneakyThrows
     public void behandleSkjermedePersoner(ConsumerRecord<String, SkjermingDTO> kafkaMelding) {
-        try {
-            SkjermingDTO skjermingDTO = kafkaMelding.value();
-            LocalDateTime skjermetFra = null;
-            LocalDateTime skjermetTil = null;
-            if (skjermingDTO.getSkjermetFra() != null && skjermingDTO.getSkjermetFra().length == 6) {
-                skjermetFra = LocalDateTime.of(skjermingDTO.getSkjermetFra()[0], skjermingDTO.getSkjermetFra()[1], skjermingDTO.getSkjermetFra()[2], skjermingDTO.getSkjermetFra()[3], skjermingDTO.getSkjermetFra()[4], skjermingDTO.getSkjermetFra()[5]);
-            }
-            if (skjermingDTO.getSkjermetTil() != null && skjermingDTO.getSkjermetTil().length == 6) {
-                skjermetTil = LocalDateTime.of(skjermingDTO.getSkjermetTil()[0], skjermingDTO.getSkjermetTil()[1], skjermingDTO.getSkjermetTil()[2], skjermingDTO.getSkjermetTil()[3], skjermingDTO.getSkjermetTil()[4], skjermingDTO.getSkjermetTil()[5]);
-            }
-
-            if (skjermetFra == null && skjermetTil == null) {
-                throw new Exception("Possible illegal data about skjerming period, kafka message: " + kafkaMelding.value());
-            }
-
-            skjermingRepository.settSkjermingPeriode(Fnr.of(kafkaMelding.key()), DateUtils.toTimestamp(skjermetFra), DateUtils.toTimestamp(skjermetTil));
-        } catch (Exception e) {
-            log.error("Can't save skjerming periode " + e, e);
+        SkjermingDTO skjermingDTO = kafkaMelding.value();
+        LocalDateTime skjermetFra = null;
+        LocalDateTime skjermetTil = null;
+        if (skjermingDTO.getSkjermetFra() != null && skjermingDTO.getSkjermetFra().length >= 6) {
+            skjermetFra = LocalDateTime.of(skjermingDTO.getSkjermetFra()[0], skjermingDTO.getSkjermetFra()[1], skjermingDTO.getSkjermetFra()[2], skjermingDTO.getSkjermetFra()[3], skjermingDTO.getSkjermetFra()[4], skjermingDTO.getSkjermetFra()[5]);
         }
+        if (skjermingDTO.getSkjermetTil() != null && skjermingDTO.getSkjermetTil().length >= 6) {
+            skjermetTil = LocalDateTime.of(skjermingDTO.getSkjermetTil()[0], skjermingDTO.getSkjermetTil()[1], skjermingDTO.getSkjermetTil()[2], skjermingDTO.getSkjermetTil()[3], skjermingDTO.getSkjermetTil()[4], skjermingDTO.getSkjermetTil()[5]);
+        }
+
+        if (skjermetFra == null && skjermetTil == null) {
+            throw new Exception("Possible illegal data about skjerming period, kafka message: " + kafkaMelding.value());
+        }
+
+        skjermingRepository.settSkjermingPeriode(Fnr.of(kafkaMelding.key()), DateUtils.toTimestamp(skjermetFra), DateUtils.toTimestamp(skjermetTil));
     }
 
     public void behandleSkjermingStatus(ConsumerRecord<String, String> kafkaMelding) {
-        try {
-            Boolean erSkjermet = Boolean.valueOf(kafkaMelding.value());
+        Boolean erSkjermet = Boolean.valueOf(kafkaMelding.value());
 
-            if (erSkjermet) {
-                skjermingRepository.settSkjerming(Fnr.of(kafkaMelding.key()), erSkjermet);
-            } else {
-                skjermingRepository.deleteSkjermingData(Fnr.of(kafkaMelding.key()));
-            }
-        } catch (Exception e) {
-            log.error("Can't save skjerming status " + e, e);
+        if (erSkjermet) {
+            skjermingRepository.settSkjerming(Fnr.of(kafkaMelding.key()), erSkjermet);
+        } else {
+            skjermingRepository.deleteSkjermingData(Fnr.of(kafkaMelding.key()));
         }
     }
 
