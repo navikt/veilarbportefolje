8da389b51bfce677d091a9c27b7023a6538e5144
stianStensli <stian.horn.stensli@nav.no>
2022-04-21 10:13:29 +0200

    bruk av junit 5

--- src/test/java/no/nav/pto/veilarbportefolje/persononinfo/PdlServiceTest.java
+++ src/test/java/no/nav/pto/veilarbportefolje/persononinfo/PdlServiceTest.java
@@ -1,16 +1,16 @@
 package no.nav.pto.veilarbportefolje.persononinfo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import lombok.SneakyThrows;
 import no.nav.common.client.pdl.PdlClientImpl;
 import no.nav.pto.veilarbportefolje.config.ApplicationConfigTest;
 import no.nav.pto.veilarbportefolje.persononinfo.PdlResponses.PDLIdent;
 import no.nav.pto.veilarbportefolje.persononinfo.PdlResponses.PdlIdentRespons;
-import no.nav.pto.veilarbportefolje.util.SingletonPostgresContainer;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -19,45 +19,45 @@ import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
-import static com.github.tomakehurst.wiremock.client.WireMock.givenThat;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static no.nav.pto.veilarbportefolje.util.TestDataUtils.randomAktorId;
 import static no.nav.pto.veilarbportefolje.util.TestUtil.readFileAsJsonString;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ApplicationConfigTest.class)
 public class PdlServiceTest {
     private final ObjectMapper mapper = new ObjectMapper();
+    private final String pdlResponsFraFil = readFileAsJsonString("/identer_pdl.json", getClass());
+    private final JdbcTemplate db;
     private PdlService pdlService;
-    private JdbcTemplate db;
 
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
+    @Autowired
+    public PdlServiceTest(@Qualifier("PostgresJdbc") JdbcTemplate db){
+        this.db = db;
+    }
 
-    @Before
+    @BeforeEach
     public void setup() {
-        this.db = SingletonPostgresContainer.init().createJdbcTemplate();
+        WireMockServer server = new WireMockServer();
         db.update("truncate pdl_identer");
 
-        String apiUrl = "http://localhost:" + wireMockRule.port();
+        server.stubFor(post(anyUrl())
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(pdlResponsFraFil))
+        );
+        server.start();
+
         this.pdlService = new PdlService(
                 new PdlRepository(db),
-                new PdlClientImpl(apiUrl, () -> "SYSTEM", () -> "SYSTEM")
+                new PdlClientImpl("http://localhost:" + server.port(), () -> "SYSTEM", () -> "SYSTEM")
         );
     }
 
     @Test
     @SneakyThrows
     public void lagreIdenterFraPdl() {
-        String pdlResponsJson = readFileAsJsonString("/identer_pdl.json", getClass());
-        givenThat(post(anyUrl())
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBody(pdlResponsJson))
-        );
-
-        var identerFrafil = mapper.readValue(pdlResponsJson, PdlIdentRespons.class)
+        var identerFraFil = mapper.readValue(pdlResponsFraFil, PdlIdentRespons.class)
                 .getData()
                 .getHentIdenter()
                 .getIdenter();
@@ -68,8 +68,8 @@ public class PdlServiceTest {
                 .map(PdlServiceTest::mapTilident)
                 .toList();
 
-        identerFrafil.forEach(filIdent ->
-                assertTrue(identerFraPostgres.contains(filIdent))
+        identerFraFil.forEach(filIdent ->
+                assertThat(identerFraPostgres.contains(filIdent)).isTrue()
         );
     }
 
