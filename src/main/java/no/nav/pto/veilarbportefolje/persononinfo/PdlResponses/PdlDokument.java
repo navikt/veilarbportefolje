22ecc6023997353ccc81bf01be6bb6505d2a2b56
stianStensli <stian.horn.stensli@nav.no>
2022-05-18 11:35:32 +0200

    tags

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PdlResponses/PdlDokument.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PdlResponses/PdlDokument.java
@@ -3,9 +3,12 @@ package no.nav.pto.veilarbportefolje.persononinfo.PdlResponses;
 import lombok.Builder;
 import lombok.Value;
 
+import java.util.Set;
+
 @Value
 @Builder
 public class PdlDokument {
+    Set<String> tags;
     PdlPersonResponse.PdlPersonResponseData.HentPersonResponsData hentPerson;
     PdlIdentResponse.HentIdenterResponseData.HentIdenterResponsData hentIdenter;
 }
