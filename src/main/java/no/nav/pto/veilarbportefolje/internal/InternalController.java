7cc08ae36f0278741aaff61c75631a6dde9a0a9c
stianStensli <stian.horn.stensli@nav.no>
2022-05-31 12:48:03 +0200

    hovedindekserings fix

--- src/main/java/no/nav/pto/veilarbportefolje/internal/InternalController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/internal/InternalController.java
@@ -1,15 +1,12 @@
 package no.nav.pto.veilarbportefolje.internal;
 
 import lombok.RequiredArgsConstructor;
-import no.nav.common.health.HealthCheck;
-import no.nav.common.health.HealthCheckUtils;
 import no.nav.common.health.selftest.SelfTestChecks;
 import no.nav.common.health.selftest.SelfTestUtils;
 import no.nav.common.health.selftest.SelftTestCheckResult;
 import no.nav.common.health.selftest.SelftestHtmlGenerator;
 import no.nav.common.metrics.Event;
 import no.nav.common.metrics.MetricsClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,9 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.List;
 
 import static no.nav.common.health.selftest.SelfTestUtils.checkAllParallel;
@@ -35,12 +30,6 @@ public class InternalController {
 
     @GetMapping("/isReady")
     public void isReady() {
-        List<HealthCheck> healthChecks = Collections.emptyList();
-
-        HealthCheckUtils.findFirstFailingCheck(healthChecks)
-                .ifPresent((failedCheck) -> {
-                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-                });
     }
 
     @GetMapping("/isAlive")
