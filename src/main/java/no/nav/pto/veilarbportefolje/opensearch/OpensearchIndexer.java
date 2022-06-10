72a1278266c3112e2add21bed807b2407f7b97ce
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-06-10 12:19:15 +0200

    Small fix

--- src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchIndexer.java
+++ src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchIndexer.java
@@ -111,6 +111,7 @@ public class OpensearchIndexer {
         List<OppfolgingsBruker> brukere = brukerRepositoryV2.hentOppfolgingsBrukere(aktorIds);
         postgresOpensearchMapper.flettInnAktivitetsData(brukere);
         postgresOpensearchMapper.flettInnSisteEndringerData(brukere);
+        postgresOpensearchMapper.flettInnStatsborgerskapData(brukere);
         if(brukere.isEmpty()){
             log.warn("Skriver ikke til index da alle brukere i batchen er ugyldige");
             return;
