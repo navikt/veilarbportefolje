0d16c68b781c8bd8b8066ef8630a16ec61930693
stianStensli <stian.horn.stensli@nav.no>
2022-06-16 11:09:29 +0200

    contextAwareUserTokenSupplier for obo og openAM

--- src/main/java/no/nav/pto/veilarbportefolje/util/VedtakstottePilotRequest.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/VedtakstottePilotRequest.java
@@ -7,16 +7,13 @@ import no.nav.common.types.identer.EnhetId;
 import no.nav.common.utils.EnvironmentUtils;
 import no.nav.common.utils.UrlUtils;
 import no.nav.pto.veilarbportefolje.auth.AuthService;
-import no.nav.pto.veilarbportefolje.auth.AuthUtils;
 import no.nav.pto.veilarbportefolje.auth.DownstreamApi;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import javax.ws.rs.core.HttpHeaders;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import static no.nav.common.client.utils.CacheUtils.tryCacheFirst;
 import static no.nav.common.rest.client.RestUtils.MEDIA_TYPE_JSON;
@@ -24,15 +21,15 @@ import static no.nav.common.rest.client.RestUtils.throwIfNotSuccessful;
 
 public class VedtakstottePilotRequest {
     private final DownstreamApi veilarbVedtakstotteApi;
-    private final Function<DownstreamApi, Optional<String>> aadOboTokenProvider;
+    private final AuthService authService;
     private final String baseURL;
     private final OkHttpClient client;
     private final Cache<EnhetId, Boolean> hentVedtakstotteCache;
 
     public VedtakstottePilotRequest(AuthService authService) {
+        this.authService = authService;
         this.veilarbVedtakstotteApi = new DownstreamApi(EnvironmentUtils.requireClusterName(), "pto", "veilarbvedtaksstotte");
         this.baseURL = UrlUtils.createServiceUrl("veilarbvedtaksstotte", "pto", true);
-        this.aadOboTokenProvider =  authService::getAadOboTokenForTjeneste;
         this.client = no.nav.common.rest.client.RestClient.baseClient();
 
         this.hentVedtakstotteCache = Caffeine.newBuilder()
@@ -52,8 +49,7 @@ public class VedtakstottePilotRequest {
         Request request = new Request.Builder()
                 .url(UrlUtils.joinPaths(baseURL, "/api/utrulling/erUtrullet?enhetId=" + enhetId.get()))
                 .header(HttpHeaders.ACCEPT, MEDIA_TYPE_JSON.toString())
-                .header("Authorization", "Bearer " + aadOboTokenProvider.apply(veilarbVedtakstotteApi)
-                        .orElseGet(AuthUtils::getInnloggetBrukerToken))
+                .header("Authorization", "Bearer " + authService.getOboOrOpenAmToken(veilarbVedtakstotteApi))
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
