3ab42a53b014ac278f64ca81448028736b7fe177
stianStensli <stian.horn.stensli@nav.no>
2022-07-06 09:52:00 +0200

    debugger med logging

--- src/main/java/no/nav/pto/veilarbportefolje/controller/Frontendlogger.java
+++ src/main/java/no/nav/pto/veilarbportefolje/controller/Frontendlogger.java
@@ -3,6 +3,7 @@ package no.nav.pto.veilarbportefolje.controller;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.common.metrics.Event;
 import no.nav.common.metrics.MetricsClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import static no.nav.common.utils.EnvironmentUtils.isProduction;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/logger")
@@ -19,13 +23,17 @@ public class Frontendlogger {
 
     @PostMapping("/event")
     public void skrivEventTilInflux(FrontendEvent event) {
-        Event toInflux = new Event(event.name);
+        Event toInflux = new Event(event.name + ".event");
         if (event.getTags() != null) {
-            event.tags.forEach(toInflux::addTagToReport);
+            event.getTags().forEach(toInflux::addTagToReport);
         }
         if (event.getFields() != null) {
-            event.fields.forEach(toInflux::addFieldToReport);
+            event.getFields().forEach(toInflux::addFieldToReport);
         }
+        toInflux.getTags().put("environment", isProduction().orElse(false) ? "p" : "q1");
+
+        log.info("debug input: " + event);
+        log.info("debug influx: " + toInflux.getFields());
         metricsClient.report(toInflux);
     }
 
@@ -35,5 +43,16 @@ public class Frontendlogger {
         private final String name;
         private final Map<String, Object> fields;
         private final Map<String, String> tags;
+
+        @Override
+        public String toString() {
+            return "Name: " + name + ", fields: " + (fields == null ? null : fields.entrySet())
+                    + ", tags: " + (tags == null ? null : tags.entrySet());
+        }
+    }
+
+    public static String toString(Event event) {
+        return "fields: " + (event.getFields() == null ? null : event.getFields().entrySet())
+                + ", tags: " + (event.getTags() == null ? null : event.getTags().entrySet());
     }
 }
---
+++ src/test/java/no/nav/pto/veilarbportefolje/controller/EndringsloggTest.java
@@ -0,0 +1,13 @@
+package no.nav.pto.veilarbportefolje.controller;
+
+import no.nav.pto.veilarbportefolje.mock.MetricsClientMock;
+import org.junit.Test;
+
+public class EndringsloggTest {
+    Frontendlogger frontendlogger = new Frontendlogger(new MetricsClientMock());
+
+    @Test
+    public void testLogger() {
+        frontendlogger.skrivEventTilInflux(new Frontendlogger.FrontendEvent("test", null, null));
+    }
+}
