fa0f2645da46afcc9a9310367137b134302b7a09
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-01-31 10:25:37 +0100

    Fix tests

--- src/test/java/no/nav/pto/veilarbportefolje/controller/EnhetControllerTest.java
+++ src/test/java/no/nav/pto/veilarbportefolje/controller/EnhetControllerTest.java
@@ -18,14 +18,15 @@ import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient;
 import no.nav.common.types.identer.EnhetId;
 import no.nav.common.types.identer.NavIdent;
 import no.nav.poao_tilgang.client.Decision;
-import no.nav.poao_tilgang.client.PoaoTilgangClient;
 import no.nav.pto.veilarbportefolje.arenapakafka.aktiviteter.TiltakService;
 import no.nav.pto.veilarbportefolje.auth.AuthService;
+import no.nav.pto.veilarbportefolje.auth.PoaoTilgangWrapper;
 import no.nav.pto.veilarbportefolje.domene.BrukereMedAntall;
 import no.nav.pto.veilarbportefolje.domene.Filtervalg;
 import no.nav.pto.veilarbportefolje.opensearch.OpensearchService;
 import no.nav.pto.veilarbportefolje.persononinfo.bosted.BostedService;
 import no.nav.pto.veilarbportefolje.persononinfo.personopprinelse.PersonOpprinnelseService;
+import no.nav.pto.veilarbportefolje.service.UnleashService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -45,24 +46,27 @@ public class EnhetControllerTest {
     private OpensearchService opensearchService;
     private EnhetController enhetController;
     private Pep pep;
-    private PoaoTilgangClient tilgangClient;
+    private PoaoTilgangWrapper poaoTilgangWrapper;
     private AuthContextHolder authContextHolder;
+
+    private UnleashService unleashService;
 
     @Before
     public void initController() {
         opensearchService = mock(OpensearchService.class);
         pep = mock(Pep.class);
-        tilgangClient = mock(PoaoTilgangClient.class);
+        poaoTilgangWrapper = mock(PoaoTilgangWrapper.class);
         authContextHolder = AuthContextHolderThreadLocal.instance();
+        unleashService = mock(UnleashService.class);
 
-        AuthService authService = new AuthService(pep, tilgangClient, mock(AzureAdOnBehalfOfTokenClient.class));
+        AuthService authService = new AuthService(pep, poaoTilgangWrapper, mock(AzureAdOnBehalfOfTokenClient.class), unleashService);
         enhetController = new EnhetController(opensearchService, authService, mock(TiltakService.class), mock(PersonOpprinnelseService.class), mock(BostedService.class));
     }
 
     @Test
     @SneakyThrows
     public void skal_hent_portefolje_fra_indeks_dersom_tilgang() {
-        when(tilgangClient.harVeilederTilgangTilModia(anyString())).thenReturn(Decision.Permit.INSTANCE);
+        when(poaoTilgangWrapper.harVeilederTilgangTilModia()).thenReturn(Decision.Permit.INSTANCE);
         when(pep.harVeilederTilgangTilEnhet(any(NavIdent.class), any(EnhetId.class))).thenReturn(true);
         when(opensearchService.hentBrukere(any(), any(), any(), any(), any(), any(), any())).thenReturn(new BrukereMedAntall(0, Collections.emptyList()));
 
@@ -76,7 +80,7 @@ public class EnhetControllerTest {
     @Test
     public void skal_hente_hele_portefolje_fra_indeks_dersom_man_mangle_antall() {
         when(pep.harVeilederTilgangTilEnhet(any(), any())).thenReturn(true);
-        when(tilgangClient.harVeilederTilgangTilModia(anyString())).thenReturn(Decision.Permit.INSTANCE);
+        when(poaoTilgangWrapper.harVeilederTilgangTilModia()).thenReturn(Decision.Permit.INSTANCE);
         when(opensearchService.hentBrukere(any(), any(), any(), any(), any(), any(), any())).thenReturn(new BrukereMedAntall(0, Collections.emptyList()));
 
         authContextHolder.withContext(
@@ -89,7 +93,7 @@ public class EnhetControllerTest {
     @Test
     public void skal_hente_hele_portefolje_fra_indeks_dersom_man_mangle_fra() {
         when(pep.harVeilederTilgangTilEnhet(any(), any())).thenReturn(true);
-        when(tilgangClient.harVeilederTilgangTilModia(anyString())).thenReturn(Decision.Permit.INSTANCE);
+        when(poaoTilgangWrapper.harVeilederTilgangTilModia()).thenReturn(Decision.Permit.INSTANCE);
         when(opensearchService.hentBrukere(any(), any(), any(), any(), any(), any(), any())).thenReturn(new BrukereMedAntall(0, Collections.emptyList()));
         authContextHolder
                 .withContext(
@@ -102,7 +106,7 @@ public class EnhetControllerTest {
 
     @Test(expected = ResponseStatusException.class)
     public void skal_ikke_hente_noe_hvis_mangler_tilgang() {
-        when(tilgangClient.harVeilederTilgangTilModia(anyString())).thenReturn(new Decision.Deny("", ""));
+        when(poaoTilgangWrapper.harVeilederTilgangTilModia()).thenReturn(new Decision.Deny("", ""));
         authContextHolder.withContext(
                 new AuthContext(UserRole.INTERN, generateMockJWT()),
                 () -> enhetController.hentPortefoljeForEnhet("0001", null, 20, "ikke_satt", "ikke_satt", new Filtervalg())
