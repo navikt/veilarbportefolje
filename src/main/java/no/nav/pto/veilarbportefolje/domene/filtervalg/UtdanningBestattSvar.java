4980f74582ca826cb36f790664e63d45b9eb6585
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-03-17 16:36:19 +0100

    Small bug fix

--- src/main/java/no/nav/pto/veilarbportefolje/domene/filtervalg/UtdanningBestattSvar.java
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/filtervalg/UtdanningBestattSvar.java
@@ -3,6 +3,7 @@ package no.nav.pto.veilarbportefolje.domene.filtervalg;
 public enum UtdanningBestattSvar {
     JA,
     NEI,
+    VET_IKKE,
     INGEN_SVAR,
 
     INGEN_DATA
--- src/main/java/no/nav/pto/veilarbportefolje/domene/filtervalg/UtdanningGodkjentSvar.java
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/filtervalg/UtdanningGodkjentSvar.java
@@ -3,7 +3,8 @@ package no.nav.pto.veilarbportefolje.domene.filtervalg;
 public enum UtdanningGodkjentSvar {
     JA,
     NEI,
-    INGEN_SVAR,
+    VET_IKKE,
 
+    INGEN_SVAR,
     INGEN_DATA
 }
