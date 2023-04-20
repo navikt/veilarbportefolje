430da8cad2d11e4fa6850097098dc90d6275776c
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-04-20 10:55:59 +0200

    Add new table for barn for brukere

---
+++ src/main/resources/db/postgres/V1_63__barn_under_18.sql
@@ -0,0 +1,9 @@
+CREATE TABLE BRUKER_DATA_BARN
+(
+    foresatt_ident      VARCHAR(30) NOT NULL,
+    bor_med_foresatt    boolean,
+    barn_foedselsdato   DATE        NOT NULL,
+    barn_diskresjonkode VARCHAR(3)
+);
+
+
