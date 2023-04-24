e5a4356cff453fec18fa09614e02790fb07a8e62
Oda Dahlen <oda.dahlen@nav.no>
2023-04-24 21:17:28 +0200

    Lagt til deler av det som trengs for å ha det nye filteret for personer med barn under 18 år

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarService.java
@@ -3,16 +3,10 @@ package no.nav.pto.veilarbportefolje.persononinfo.barnUnder18Aar;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.Fnr;
-import no.nav.pto.veilarbportefolje.persononinfo.domene.PDLPersonBarn;
+import no.nav.pto.veilarbportefolje.opensearch.domene.BarnUnder18AarData;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static no.nav.pto.veilarbportefolje.util.DateUtils.erUnder18Aar;
-import static no.nav.pto.veilarbportefolje.util.SecureLog.secureLog;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -21,93 +15,19 @@ public class BarnUnder18AarService {
 
     private final BarnUnder18AarRepository barnUnder18AarRepository;
 
-    public Map<Fnr, List<BarnUnder18AarData>> hentBarnUnder18Aar(List<Fnr> fnrForeldre) {
+    List<BarnUnder18AarData> data = new ArrayList<>();
+    //BarnUnder18AarData data2 = (1L, true, "");
+
+
+    public Map<Fnr, List<BarnUnder18AarData>> hentBarnUnder18AarAlle(List<Fnr> fnrs) {
         Map<Fnr, List<BarnUnder18AarData>> result = new HashMap<>();
+        Map<Fnr, List<BarnUnder18AarData>> result2 = new HashMap<>();
 
-        fnrForeldre.forEach(fnrPerson -> {
-                    List<BarnUnder18AarData> barnListe = new ArrayList<>();
-                    barnUnder18AarRepository.hentForeldreansvarForPerson(fnrPerson).forEach(fnrBarn -> {
-                                BarnUnder18AarData barnUnder18AarData = barnUnder18AarRepository.hentInfoOmBarn(fnrBarn);
-                                if (barnUnder18AarData != null) {
-                                    barnListe.add(barnUnder18AarData);
-                                }
-                            }
-                    );
-                    result.put(fnrPerson, barnListe);
-                }
+        fnrs.forEach( fnr ->
+                        result.put(fnr,barnUnder18AarRepository.hentBarnUnder18Aar(fnr.toString()))
         );
+
+        //data.add(new BarnUnder18AarData(1L, true, ""));
         return result;
-    }
-
-    public List<Fnr> hentBarnFnrsForForeldre(List<Fnr> fnrForeldre) {
-        List<Fnr> result = new ArrayList<>();
-
-        fnrForeldre.forEach(fnrForelder -> {
-                    result.addAll(barnUnder18AarRepository.hentForeldreansvarForPerson(fnrForelder));
-                }
-        );
-        return result;
-    }
-
-    public void lagreBarnOgForeldreansvar(Fnr foresattIdent, List<BarnUnder18Aar> barnFraPdl) {
-        List<Fnr> lagredeBarn = barnUnder18AarRepository.hentForeldreansvarForPerson(foresattIdent);
-
-        List<Fnr> barnFnrFraPdl = barnFraPdl.stream().map(BarnUnder18Aar::getFnr).toList();
-
-        lagredeBarn.forEach(barnFnr -> {
-                    if (!barnFnrFraPdl.contains(barnFnr)) {
-                        barnUnder18AarRepository.slettForeldreansvar(foresattIdent, barnFnr);
-                        slettBarnDataHvisIngenForeldreErUnderOppfolging(barnFnr);
-                        secureLog.warn(String.format("Barn fjernet fra PDL for foreldre %s og barn %s", foresattIdent, barnFnr));
-                    }
-                }
-        );
-
-        barnFraPdl.forEach(barnUnder18Aar -> {
-            if (erUnder18Aar(barnUnder18Aar.getFodselsdato())) {
-                barnUnder18AarRepository.lagreBarnData(barnUnder18Aar.getFnr(), barnUnder18Aar.getFodselsdato(), barnUnder18Aar.getDiskresjonskode());
-                barnUnder18AarRepository.lagreForeldreansvar(foresattIdent, barnUnder18Aar.getFnr());
-            }
-        });
-    }
-
-
-    public void oppdaterEndringPaBarn(Fnr fnrBarn, PDLPersonBarn pdlPersonBarn) {
-        barnUnder18AarRepository.lagreBarnData(fnrBarn, pdlPersonBarn.getFodselsdato(), pdlPersonBarn.getDiskresjonskode());
-    }
-
-    public void slettBarnDataHvisIngenForeldreErUnderOppfolging(List<Fnr> barnIdenter) {
-        barnIdenter.forEach(this::slettBarnDataHvisIngenForeldreErUnderOppfolging);
-    }
-
-    private void slettBarnDataHvisIngenForeldreErUnderOppfolging(Fnr barnFnr) {
-        if (!barnUnder18AarRepository.finnesBarnIForeldreansvar(barnFnr)) {
-            barnUnder18AarRepository.slettBarnData(barnFnr);
-        }
-    }
-
-    public boolean erFnrBarnAvForelderUnderOppfolging(List<Fnr> fnrBarn) {
-        return barnUnder18AarRepository.finnesBarnIForeldreansvar(fnrBarn);
-    }
-
-    public List<Fnr> finnForeldreTilBarn(Fnr fnrBarn) {
-        return barnUnder18AarRepository.hentForeldreTilBarn(fnrBarn);
-    }
-
-    public void slettDataForBarnSomErOver18() {
-        List<Fnr> fnrBarnOver18 = barnUnder18AarRepository.hentAlleBarnOver18();
-
-        fnrBarnOver18.forEach(fnrBarn -> {
-            barnUnder18AarRepository.slettForeldreansvar(fnrBarn);
-            barnUnder18AarRepository.slettBarnData(fnrBarn);
-        });
-
-    }
-
-    public void handterBarnIdentEndring(Fnr aktivtFnrBarn, List<Fnr> inaktiveFnrs) {
-        if (!inaktiveFnrs.isEmpty()) {
-            barnUnder18AarRepository.oppdatereBarnIdent(aktivtFnrBarn, inaktiveFnrs);
-        }
-    }
-
+}
 }
