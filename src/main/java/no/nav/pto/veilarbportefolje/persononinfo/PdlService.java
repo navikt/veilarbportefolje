19a77e1304dc929a419f68edf2412c5c420ea618
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-04-27 11:57:45 +0200

    Implement getting data from pdl for bruker barn

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PdlService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/PdlService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
 import no.nav.pto.veilarbportefolje.domene.Statsborgerskap;
+import no.nav.pto.veilarbportefolje.persononinfo.domene.Familiemedlem;
 import no.nav.pto.veilarbportefolje.persononinfo.domene.PDLIdent;
 import no.nav.pto.veilarbportefolje.persononinfo.domene.PDLPerson;
+import no.nav.pto.veilarbportefolje.persononinfo.domene.PDLPersonBarn;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,10 +35,19 @@ public class PdlService {
     public void hentOgLagreBrukerData(Fnr fnr) {
         PDLPerson personData = pdlClient.hentBrukerDataFraPdl(fnr);
         pdlPersonRepository.upsertPerson(fnr, personData);
-    }
 
-    public void hentOgLagrePdlDataForBrukerBarn(Fnr fnr) {
-        PDLPerson personData = pdlClient.hentBrukerDataFraPdl(fnr);
+        List<Familiemedlem> brukerBarn = personData.getBarnUtenFnr();
+
+        personData.getBarnMedFnr().forEach(barnFnr -> {
+            PDLPersonBarn personBarnData = pdlClient.hentBrukerBarnDataFraPdl(fnr);
+            Familiemedlem familiemedlem = Familiemedlem.of(barnFnr, personBarnData, personData.getBostedsadresse());
+            if (familiemedlem != null) {
+                brukerBarn.add(familiemedlem);
+            }
+
+        });
+        pdlPersonRepository.upsertPersonBarn(fnr, brukerBarn);
+
     }
 
     private List<PDLIdent> hentOgLagreIdenter(AktorId aktorId) {
