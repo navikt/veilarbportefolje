ce42ddba2d427c63bf58a4e1f009ce23736bf7c4
Sondre Ovrid <111113539+slovrid@users.noreply.github.com>
2023-04-27 10:15:51 +0200

    Prodsette: Spring Boot 3 (#1181)
    * TC-329 Bump spring-boot-starter-parent til 3.0.2
    
    * TC-329 Utfør "Java EE to Jakarta EE"-refaktorering i IntelliJ
    
    Ref: https://www.jetbrains.com/help/idea/migrate.html
    
    * TC-329 Bump nav-common og hent de fra GitHub Packages
    
    * TC-329 Fiks kompileringsfeil: implementer manglende metode i PepMock
    
    * TC-329 Fiks deprecated warning for okhttp3.RequestBody
    
    * TC-329 Fiks feilende test: la Spring Boot styre versjon av slf4j
    
    Ref: https://github.com/spring-projects/spring-boot/issues/33854
    
    * TC-329 Fiks feilende test: bruke wiremock-jre8-standalone
    
    Ref: https://stackoverflow.com/a/74673967
    
    * TC-329 Fjern eksplisitt versjon for avhengigheter som er Spring-managed
    
    Ref: https://docs.spring.io/spring-boot/docs/3.0.x/reference/html/dependency-versions.html#appendix.dependency-versions
    
    * Fikse kompileringsfeil i ApplicationConfigTest.java
    
    * Fiks feil der applikasjonen ikke logget riktig
    
    * TC-329 Bump til siste versjon av Spring Boot 3 og common
    
    * TC-329 Bump til siste versjon av common
    
    * TC-329 Fjern eksplisitt versjonering av deps som er spring managed

--- src/main/java/no/nav/pto/veilarbportefolje/util/AuthorizationUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/AuthorizationUtils.java
@@ -3,8 +3,8 @@ package no.nav.pto.veilarbportefolje.util;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.utils.Credentials;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.Objects;
