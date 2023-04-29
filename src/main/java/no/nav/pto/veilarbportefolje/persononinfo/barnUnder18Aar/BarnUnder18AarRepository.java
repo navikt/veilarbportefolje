af8a70e00b69243d6bf870da290a3af7b96524f3
Dragutin Vujovic <dragutin.vujovic@nav.no>
2023-04-29 11:17:53 +0200

    Connect getting of data from pdl with rest of code

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarRepository.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarRepository.java
@@ -12,7 +12,10 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static no.nav.pto.veilarbportefolje.postgres.PostgresUtils.queryForObjectOrNull;
 import static no.nav.pto.veilarbportefolje.util.DateUtils.toLocalDateOrNull;
 
 @Slf4j
@@ -37,21 +40,30 @@ public class BarnUnder18AarRepository {
 
     //TODO Legg til fnr_barn i tabellen. Slett "id".
     //TODO Ta inn PDLPerson her og hent ut data derfra
-    public void upsert(Integer fnrBarn, Fnr fnrForesatt, Boolean borMedForesatt, LocalDate barnFoedselsdato, String diskresjonskode) {
+    public void upsert(Fnr fnrBarn, Fnr fnrForesatt, Boolean borMedForesatt, LocalDate barnFoedselsdato, String diskresjonskode) {
         db.update("""
                         INSERT INTO bruker_data_barn (id, foresatt_ident, bor_med_foresatt, barn_foedselsdato, barn_diskresjonkode)
                         VALUES(?,?,?,?,?) ON CONFLICT (id, foresatt_ident) DO UPDATE SET
                          (bor_med_foresatt, barn_foedselsdato, barn_diskresjonkode) =
                          (excluded.bor_med_foresatt, excluded.barn_foedselsdato, excluded.barn_diskresjonkode)
                          """,
-                fnrBarn, fnrForesatt.get(), borMedForesatt, barnFoedselsdato, diskresjonskode);
+                fnrBarn.get(), fnrForesatt.get(), borMedForesatt, barnFoedselsdato, diskresjonskode);
+    }
+
+    public boolean erEndringForBarnAvBrukerUnderOppfolging(List<Fnr> fnrs) {
+        String identerParam = fnrs.stream().map(Fnr::get).collect(Collectors.joining(",", "{", "}"));
+        return Optional.ofNullable(
+                queryForObjectOrNull(() -> db.queryForObject("""
+                        SELECT COUNT(*) as barnAntall FROM BRUKER_DATA_BARN WHERE id = any (?::varchar[])
+                        """, (rs, row) -> rs.getInt("barnAntall") > 0, identerParam))
+        ).orElse(false);
     }
 
 
     private BarnUnder18AarData mapTilBarnUnder18(Map<String, Object> rs) {
         return new BarnUnder18AarData(
-                (Long) alderFraFodselsdato(toLocalDateOrNull((java.sql.Date) rs.get("BARN_FOEDSELSDATO")),
-                LocalDate.now()), (boolean) rs.get("BOR_MED_FORESATT"),
+                alderFraFodselsdato(toLocalDateOrNull((java.sql.Date) rs.get("BARN_FOEDSELSDATO")),
+                        LocalDate.now()), (boolean) rs.get("BOR_MED_FORESATT"),
                 (String) rs.get("BARN_DISKRESJONKODE"));
     }
 
@@ -59,7 +71,6 @@ public class BarnUnder18AarRepository {
         Integer age = Period.between(date, now).getYears();
         return age.longValue();
     }
-
 }
 
 
