cae3b0b9f25699320ac53d32ece84dae7fc4fe20
Oda Dahlen <oda.dahlen@nav.no>
2023-05-12 00:04:15 +0200

    Fikset tester ++

--- src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/persononinfo/barnUnder18Aar/BarnUnder18AarService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.types.identer.Fnr;
 import no.nav.pto.veilarbportefolje.opensearch.domene.BarnUnder18AarData;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -15,32 +16,40 @@ public class BarnUnder18AarService {
 
     private final BarnUnder18AarRepository barnUnder18AarRepository;
 
-    public Map<Fnr, List<BarnUnder18AarData>> hentBarnUnder18AarAlle(List<Fnr> fnrs) {
+    public Map<Fnr, List<BarnUnder18AarData>> hentBarnUnder18AarAlle(List<Fnr> fnrPersoner) {
         Map<Fnr, List<BarnUnder18AarData>> result = new HashMap<>();
+        List<BarnUnder18AarData> barnListe = new ArrayList();
 
-        fnrs.forEach( fnr ->
-                        result.put(fnr,barnUnder18AarRepository.hentBarnUnder18Aar(fnr.toString()))
+        fnrPersoner.forEach(fnrPerson -> {
+                    barnUnder18AarRepository.hentForeldreansvarForPerson(fnrPerson).forEach(fnrBarn ->
+                            {
+                                barnUnder18AarRepository.hentInfoOmBarn(fnrBarn);
+                                barnListe.add(barnUnder18AarRepository.hentInfoOmBarn(fnrBarn));
+                            }
+                    );
+                    result.put(fnrPerson, barnListe);
+                    barnListe.clear();
+                }
         );
-
         return result;
-}
+    }
 
-    public void lagreBarnOgForeldreansvar(Fnr foresattIdent, List<BarnUnder18Aar> barn){
+    public void lagreBarnOgForeldreansvar(Fnr foresattIdent, List<BarnUnder18Aar> barnFraPdl) {
         List<Fnr> lagredeBarn = barnUnder18AarRepository.hentForeldreansvarForPerson(foresattIdent);
 
-        List<Fnr> barnFnrFraPdl = barn.stream().map(BarnUnder18Aar::getFnr).toList();
+        List<Fnr> barnFnrFraPdl = barnFraPdl.stream().map(BarnUnder18Aar::getFnr).toList();
 
-        lagredeBarn.forEach( barnFnr -> {
-                    if (!barnFnrFraPdl.contains(barnFnr)){
+        lagredeBarn.forEach(barnFnr -> {
+                    if (!barnFnrFraPdl.contains(barnFnr)) {
                         barnUnder18AarRepository.slettForeldreansvar(foresattIdent, barnFnr);
-                        if (!barnUnder18AarRepository.finnesBarnIForeldreansvar(barnFnr)){
+                        if (!barnUnder18AarRepository.finnesBarnIForeldreansvar(barnFnr)) {
                             barnUnder18AarRepository.slettBarnData(barnFnr);
                         }
                     }
                 }
         );
 
-        barn.forEach(barnUnder18Aar -> {
+        barnFraPdl.forEach(barnUnder18Aar -> {
             barnUnder18AarRepository.lagreBarnData(barnUnder18Aar.getFnr(), barnUnder18Aar.getFodselsdato(), barnUnder18Aar.getDiskresjonskode());
             barnUnder18AarRepository.lagreForeldreansvar(foresattIdent, barnUnder18Aar.getFnr());
         });
