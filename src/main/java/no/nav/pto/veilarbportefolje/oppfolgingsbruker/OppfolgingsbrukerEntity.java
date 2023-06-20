d24ca8cd4b350f5b802ffc317b15661f5e20a607
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-06-20 11:57:50 +0200

    TC-250 Fjern data som nå hentes fra pdl.

--- src/main/java/no/nav/pto/veilarbportefolje/oppfolgingsbruker/OppfolgingsbrukerEntity.java
+++ src/main/java/no/nav/pto/veilarbportefolje/oppfolgingsbruker/OppfolgingsbrukerEntity.java
@@ -9,16 +9,11 @@ public record OppfolgingsbrukerEntity(
         String fodselsnr,
         String formidlingsgruppekode,
         ZonedDateTime iserv_fra_dato,
-        String etternavn,
-        String fornavn,
         String nav_kontor,
         String kvalifiseringsgruppekode,
         String rettighetsgruppekode,
         String hovedmaalkode,
-        String sikkerhetstiltak_type_kode,
-        String fr_kode,
         boolean sperret_ansatt,
-        boolean er_doed,
         ZonedDateTime endret_dato
 ) {
 }
