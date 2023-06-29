54f280acd2573bb14f68647ba955ece26d3acc6b
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-06-29 12:39:41 +0200

    TC-250 feilretting en test

--- src/test/java/no/nav/pto/veilarbportefolje/arenapakafka/DtoParsingTest.java
+++ src/test/java/no/nav/pto/veilarbportefolje/arenapakafka/DtoParsingTest.java
@@ -2,6 +2,7 @@ package no.nav.pto.veilarbportefolje.arenapakafka;
 
 import no.nav.pto.veilarbportefolje.arenapakafka.arenaDTO.*;
 import no.nav.pto.veilarbportefolje.config.ApplicationConfigTest;
+import no.nav.pto.veilarbportefolje.util.EndToEndTest;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +11,7 @@ import static no.nav.pto.veilarbportefolje.util.TestUtil.readFileAsJsonString;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ApplicationConfigTest.class)
-public class DtoParsingTest {
+public class DtoParsingTest extends EndToEndTest {
     @Test
     public void skal_bygge_korrekt_UtdanningsAktivitetInnhold_json() {
         String goldenGateDtoString = readFileAsJsonString("/goldenGateUtdanningsAktivitet.json", getClass());
@@ -18,7 +19,7 @@ public class DtoParsingTest {
         UtdanningsAktivitetDTO goldenGateDTO = fromJson(goldenGateDtoString, UtdanningsAktivitetDTO.class);
         assertThat(goldenGateDTO.getCurrentTimestamp()).isEqualTo("2021-06-23T09:03:55.677014");
         assertThat(goldenGateDTO.getAfter().getClass()).isEqualTo(UtdanningsAktivitetInnhold.class);
-        assertThat(goldenGateDTO.getAfter().getEndretDato().getDato().toString().substring(0,10)).isEqualTo("2021-06-18");
+        assertThat(goldenGateDTO.getAfter().getEndretDato().getDato().toString()).isEqualTo("2021-06-18T00:00+02:00[Europe/Oslo]");
         assertThat(goldenGateDTO.getAfter()).isNotNull();
         assertThat(goldenGateDTO.getBefore()).isNull();
     }
