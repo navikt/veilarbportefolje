6c0a769effaaf22eae73465474a742b4f6d89e9d
Oda Dahlen <oda.dahlen@nav.no>
2023-12-19 23:12:36 +0100

    Vi hadde ikke en unik henting av huskelapp, så ved indeksering ville huskelapp blitt feil for de som hadde > 1 rad med huskelapp

--- src/test/java/no/nav/pto/veilarbportefolje/postgres/BrukerRepositoryV2Test.java
+++ src/test/java/no/nav/pto/veilarbportefolje/postgres/BrukerRepositoryV2Test.java
@@ -2,7 +2,12 @@ package no.nav.pto.veilarbportefolje.postgres;
 
 import io.getunleash.DefaultUnleash;
 import no.nav.common.types.identer.AktorId;
+import no.nav.common.types.identer.EnhetId;
 import no.nav.common.types.identer.Fnr;
+import no.nav.pto.veilarbportefolje.domene.value.VeilederId;
+import no.nav.pto.veilarbportefolje.huskelapp.HuskelappRepository;
+import no.nav.pto.veilarbportefolje.huskelapp.controller.dto.HuskelappOpprettRequest;
+import no.nav.pto.veilarbportefolje.huskelapp.controller.dto.HuskelappRedigerRequest;
 import no.nav.pto.veilarbportefolje.kodeverk.KodeverkService;
 import no.nav.pto.veilarbportefolje.opensearch.domene.OppfolgingsBruker;
 import no.nav.pto.veilarbportefolje.oppfolging.OppfolgingRepositoryV2;
@@ -21,6 +26,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.UUID;
 
 import static no.nav.pto.veilarbportefolje.domene.Kjonn.K;
 import static no.nav.pto.veilarbportefolje.persononinfo.domene.PDLIdent.Gruppe.AKTORID;
@@ -37,6 +43,7 @@ public class BrukerRepositoryV2Test {
     private OppfolgingsbrukerRepositoryV3 oppfolgingsbrukerRepositoryV3;
     private OppfolgingRepositoryV2 oppfolgingRepositoryV2;
 
+    private HuskelappRepository huskelappRepository;
     @BeforeEach
     public void setUp() {
         JdbcTemplate db = SingletonPostgresContainer.init().createJdbcTemplate();
@@ -48,6 +55,38 @@ public class BrukerRepositoryV2Test {
         this.oppfolgingRepositoryV2 = new OppfolgingRepositoryV2(db);
         this.pdlPersonRepository = new PdlPersonRepository(db, db);
         this.oppfolgingsbrukerRepositoryV3 = new OppfolgingsbrukerRepositoryV3(db, null);
+        this.huskelappRepository = new HuskelappRepository(db, null);
+    }
+
+    @Test
+    public void skalKunHenteAktivHuskelapp() {
+        Fnr fnr_1 = Fnr.of("3");
+        AktorId aktorId = randomAktorId();
+        List<PDLIdent> identer = List.of(
+                new PDLIdent(fnr_1.get(), false, FOLKEREGISTERIDENT),
+                new PDLIdent(aktorId.get(), false, AKTORID)
+        );
+
+        oppfolgingRepositoryV2.settUnderOppfolging(aktorId, ZonedDateTime.now());
+        pdlPersonRepository.upsertPerson(fnr_1, new PDLPerson().setKjonn(K).setFoedsel(LocalDate.now()));
+        pdlIdentRepository.upsertIdenter(identer);
+        HuskelappOpprettRequest huskelapp1 = new HuskelappOpprettRequest(fnr_1, LocalDate.of(2024, 2,10), "Husk nr 1", EnhetId.of("0000"));
+        UUID huskelappUUID = huskelappRepository.opprettHuskelapp(huskelapp1, VeilederId.of("Z123456"));
+        HuskelappRedigerRequest huskelapp2 = new HuskelappRedigerRequest(huskelappUUID, fnr_1, LocalDate.of(2026, 1,1), "Husk nr 2", EnhetId.of("0000"));
+        huskelappRepository.redigerHuskelapp(huskelapp2, VeilederId.of("Z123456"));
+
+
+        oppfolgingsbrukerRepositoryV3.leggTilEllerEndreOppfolgingsbruker(
+                new OppfolgingsbrukerEntity(fnr_1.get(), null, null,
+                        "0000", null, null,
+                        null, ZonedDateTime.now()));
+        List<OppfolgingsBruker> oppfolgingsBrukerMedHuskelapp = brukerRepositoryV2.hentOppfolgingsBrukere(List.of(aktorId));
+
+
+        Assertions.assertThat(oppfolgingsBrukerMedHuskelapp.size()).isEqualTo(1);
+        Assertions.assertThat(oppfolgingsBrukerMedHuskelapp.get(0).getFnr()).isEqualTo(fnr_1.get());
+        Assertions.assertThat(oppfolgingsBrukerMedHuskelapp.get(0).getEnhet_id()).isEqualTo("0000");
+        Assertions.assertThat(oppfolgingsBrukerMedHuskelapp.get(0).getHuskelapp().frist()).isEqualTo(LocalDate.of(2026, 1,1));
     }
 
     @Test
@@ -68,11 +107,11 @@ public class BrukerRepositoryV2Test {
 
         oppfolgingsbrukerRepositoryV3.leggTilEllerEndreOppfolgingsbruker(
                 new OppfolgingsbrukerEntity(fnr_1.get(), null, null,
-                         "0000", null, null,
+                        "0000", null, null,
                         null, ZonedDateTime.now().minusDays(1)));
         oppfolgingsbrukerRepositoryV3.leggTilEllerEndreOppfolgingsbruker(
                 new OppfolgingsbrukerEntity(fnr_2.get(), null, null,
-                         "0000", null, null,
+                        "0000", null, null,
                         null, ZonedDateTime.now()));
         List<OppfolgingsBruker> oppfolgingsBrukers_pre_nyFnrIArena = brukerRepositoryV2.hentOppfolgingsBrukere(List.of(aktorId));
 
