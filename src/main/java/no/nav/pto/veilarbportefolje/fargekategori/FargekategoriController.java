44fc3f10208f4198d4202b64e5355ce147cb676d
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-01-23 20:33:47 +0100

    Legg til autoriseringssjekker

--- src/main/java/no/nav/pto/veilarbportefolje/fargekategori/FargekategoriController.java
+++ src/main/java/no/nav/pto/veilarbportefolje/fargekategori/FargekategoriController.java
@@ -1,10 +1,14 @@
 package no.nav.pto.veilarbportefolje.fargekategori;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.common.types.identer.EnhetId;
 import no.nav.common.types.identer.Fnr;
+import no.nav.pto.veilarbportefolje.auth.AuthService;
 import no.nav.pto.veilarbportefolje.auth.AuthUtils;
 import no.nav.pto.veilarbportefolje.domene.RestResponse;
+import no.nav.pto.veilarbportefolje.domene.value.NavKontor;
 import no.nav.pto.veilarbportefolje.domene.value.VeilederId;
+import no.nav.pto.veilarbportefolje.service.BrukerServiceV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -22,17 +27,23 @@ import java.util.UUID;
 public class FargekategoriController {
 
     private final FargekategoriService fargekategoriService;
+    private final AuthService authService;
+    private final BrukerServiceV2 brukerServiceV2;
 
     @PutMapping("/fargekategori")
     public ResponseEntity<FargekategoriResponse> oppdaterFargekategoriForBruker(@RequestBody OppdaterFargekategoriRequest request) {
         VeilederId innloggetVeileder = AuthUtils.getInnloggetVeilederIdent();
         validerRequest(request);
 
-        // TODO: Autorisering
-        // Sjekk at veileder har tilgang til oppfølging
-        // Sjekk at veileder har les/skriv tilgang til bruker
-        // Sjekk at veileder har tilgang til enhet (?), muligens dekket av den over
-        // Sjekk at veileder er tilordnet brukeren
+        Optional<NavKontor> brukerEnhet = brukerServiceV2.hentNavKontor(request.fnr);
+
+        if(brukerEnhet.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Bruker med oppgitt fnr er ikke under oppfølging");
+        }
+
+        authService.tilgangTilOppfolging();
+        authService.tilgangTilBruker(request.fnr.get());
+        authService.tilgangTilEnhet(brukerEnhet.get().toString());
 
         return ResponseEntity.ok(new FargekategoriResponse(fargekategoriService.oppdaterFargekategoriForBruker(request, innloggetVeileder)));
     }
