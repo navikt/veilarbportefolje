4f6a8038e16f40f4f95b8605f55e9073e8c3ea28
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-04-23 12:58:14 +0200

    TC-587 Legg til klient og request/response-typer for nytt arbeidssøkerregister

---
+++ src/main/java/no/nav/pto/veilarbportefolje/arbeidssoker/v2/OppslagArbeidssoekerregisteretClient.kt
@@ -0,0 +1,187 @@
+package no.nav.pto.veilarbportefolje.arbeidssoker.v2
+
+
+import jakarta.ws.rs.core.HttpHeaders
+import no.nav.common.rest.client.RestUtils
+import no.nav.common.types.identer.Fnr
+import no.nav.common.utils.UrlUtils
+import no.nav.pto.veilarbportefolje.util.deserializeJsonOrThrow
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+import java.util.*
+import java.util.function.Supplier
+
+class OppslagArbeidssoekerregisteretClient(
+    private val url: String,
+    private val tokenSupplier: Supplier<String>,
+    private val client: OkHttpClient,
+    private val consumerId: String
+) {
+    // TODO: Map til intern type som gir mening for oss
+    fun hentArbeidssokerPerioder(fnr: Fnr): Optional<List<ArbeidssokerperiodeResponse>> {
+        val request: Request = Request.Builder()
+            .url(UrlUtils.joinPaths(url, "/api/v1/veileder/arbeidssoekerperioder"))
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ${tokenSupplier.get()}")
+            .header("Nav-Consumer-Id", consumerId)
+            .post(RestUtils.toJsonRequestBody(ArbeidssoekerperiodeRequest(fnr.get())))
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.code == HttpStatus.NOT_FOUND.value()) {
+                return Optional.empty()
+            }
+
+            RestUtils.throwIfNotSuccessful(response)
+
+            return Optional.ofNullable(
+                response.deserializeJsonOrThrow()
+            )
+        }
+    }
+
+    // TODO: Map til intern type som gir mening for oss
+    fun hentOpplysningerOmArbeidssoeker(identitetsnummer: String, periodeId: UUID): Optional<OpplysningerOmArbeidssoekerResponse> {
+        val request: Request = Request.Builder()
+            .url(UrlUtils.joinPaths(url, "/api/v1/veileder/opplysninger-om-arbeidssoeker"))
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ${tokenSupplier.get()}")
+            .header("Nav-Consumer-Id", consumerId)
+            .post(RestUtils.toJsonRequestBody(OpplysningerOmArbeidssoekerRequest(identitetsnummer, periodeId)))
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.code == HttpStatus.NOT_FOUND.value()) {
+                return Optional.empty()
+            }
+
+            RestUtils.throwIfNotSuccessful(response)
+
+            return Optional.ofNullable(
+                response.deserializeJsonOrThrow()
+            )
+        }
+    }
+
+    // TODO: Map til intern type som gir mening for oss
+    fun hentProfilering(identitetsnummer: String, periodeId: UUID): Optional<ProfileringResponse> {
+        val request: Request = Request.Builder()
+            .url(UrlUtils.joinPaths(url, "/api/v1/veileder/profilering"))
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ${tokenSupplier.get()}")
+            .header("Nav-Consumer-Id", consumerId)
+            .post(RestUtils.toJsonRequestBody(ProfileringRequest(identitetsnummer, periodeId)))
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.code == HttpStatus.NOT_FOUND.value()) {
+                return Optional.empty()
+            }
+
+            RestUtils.throwIfNotSuccessful(response)
+
+            return Optional.ofNullable(
+                response.deserializeJsonOrThrow()
+            )
+        }
+    }
+}
+
+// Arbeidssøkerperioder typer
+data class ArbeidssoekerperiodeRequest(val identitetsnummer: String)
+
+data class ArbeidssokerperiodeResponse(
+    val periodeId: UUID,
+    val metadata: MetadataResponse,
+    val avsluttet: MetadataResponse
+)
+
+// Opplysninger om arbeidssøker typer
+data class OpplysningerOmArbeidssoekerRequest(val identitetsnummer: String, val periodeId: UUID)
+
+data class OpplysningerOmArbeidssoekerResponse(
+    val oppysningerOmArbeidssoekerId: UUID,
+    val periodeId: UUID,
+    val sendtInnAv: MetadataResponse,
+    val utdanning: UtdanningResponse,
+    val helse: HelseResponse,
+    val annet: AnnetResponse,
+    val jobbsituasjon: List<BeskrivelseMedDetaljerResponse>
+)
+
+data class BeskrivelseMedDetaljerResponse(
+    val beskrivelse: JobbSituasjonBeskrivelse,
+    val detaljer: Map<String, String>   // TODO: Litt usikker på om typene er riktig for key/value
+)
+
+enum class JobbSituasjonBeskrivelse {
+    UKJENT_VERDI,
+    UDEFINERT,
+    HAR_SAGT_OPP,
+    HAR_BLITT_SAGT_OPP,
+    ER_PERMITTERT,
+    ALDRI_HATT_JOBB,
+    IKKE_VAERT_I_JOBB_SISTE_2_AAR,
+    AKKURAT_FULLFORT_UTDANNING,
+    VIL_BYTTE_JOBB,
+    USIKKER_JOBBSITUASJON,
+    MIDLERTIDIG_JOBB,
+    DELTIDSJOBB_VIL_MER,
+    NY_JOBB,
+    KONKURS,
+    ANNET
+}
+
+data class AnnetResponse(
+    val andreForholdHindrerArbeid: JaNeiVetIkke
+)
+
+data class HelseResponse(
+    val helsetilstandHindrerArbeid: JaNeiVetIkke
+)
+
+data class UtdanningResponse(
+    val nus: String,    // TODO: Legg til kommentar som beskriver hva nus er
+    val bestaatt: JaNeiVetIkke,
+    val godkjent: JaNeiVetIkke
+)
+
+// Profilering typer
+data class ProfileringRequest(val identitetsnummer: String, val periodeId: UUID)
+
+data class ProfileringResponse(
+    val profileringId: UUID,
+    val periodeId: UUID,
+    val opplysningerOmArbeidssoekerId: UUID,
+    val sendtInnAv: MetadataResponse,
+    val profilertTil: ProfileringsResultat,
+    val jobbetSammenhengendeSeksAvTolvSisteManeder: Boolean,
+    val alder: Int
+)
+
+enum class ProfileringsResultat {
+    UKJENT_VERDI,
+    UDEFINERT,
+    ANTATT_GODE_MULIGHETER,
+    ANTATT_BEHOV_FOR_VEILEDNING,
+    OPPGITT_HINDRINGER
+}
+
+// Felles typer
+enum class JaNeiVetIkke {
+    JA, NEI, VET_IKKE
+}
+
+data class MetadataResponse(
+    val tidspunkt: LocalDateTime,
+    val brukerResponse: BrukerResponse,
+    val kilde: String,
+    val aarsak: String
+)
+
+data class BrukerResponse(
+    val type: BrukerType
+)
+
+enum class BrukerType {
+    UKJENT_VERDI, UDEFINERT, VEILEDER, SYSTEM, SLUTTBRUKER
+}
