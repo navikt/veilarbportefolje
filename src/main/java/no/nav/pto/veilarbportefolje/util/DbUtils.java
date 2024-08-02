84c6881a614d999b33b4791fc46371e4833dab48
Sneha Desai <sneha.desai@nav.no>
2024-08-02 12:59:55 +0200

    Connect til ny db urlen på gcp Co-authored-by: Julie Hill Roa <julie.hill.roa@nav.no> Co-authored-by: Dragutin Vujovic <dragutin.vujovic@nav.no> Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
@@ -1,30 +1,16 @@
 package no.nav.pto.veilarbportefolje.util;
 
 import com.zaxxer.hikari.HikariConfig;
-import lombok.SneakyThrows;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
-
 import javax.sql.DataSource;
-
-import static no.nav.common.utils.EnvironmentUtils.isProduction;
-import static org.postgresql.PGProperty.SOCKET_TIMEOUT;
 
 @Slf4j
 public class DbUtils {
-    private enum DbRole {
-        ADMIN,
-        READONLY,
-    }
 
-    public static DataSource createDataSource(String dbUrl, boolean admin) {
-        if (admin) {
-            HikariConfig config = createDataSourceConfig(dbUrl, 2);
-            return createVaultRefreshDataSource(config, DbRole.ADMIN);
-        }
-        HikariConfig config = createDataSourceConfig(dbUrl, 3);
-        config.addDataSourceProperty(SOCKET_TIMEOUT.getName(), "600"); // 10min
-        return createVaultRefreshDataSource(config, DbRole.READONLY);
+    public static DataSource createDataSource(String dbUrl) {
+        HikariConfig config = createDataSourceConfig(dbUrl, 2);
+        return new HikariDataSource(config);
     }
 
     public static HikariConfig createDataSourceConfig(String dbUrl, int maximumPoolSize) {
@@ -34,29 +20,6 @@ public class DbUtils {
         config.setConnectionTimeout(600000); // 10min
         config.setMinimumIdle(1);
         return config;
-    }
-
-    public static String getSqlAdminRole() {
-        boolean isProd = isProduction().orElse(false);
-        return (isProd ? "veilarbportefolje-prod-admin" : "veilarbportefolje-dev-admin");
-    }
-
-    public static String getSqlReadOnlyRole() {
-        boolean isProd = isProduction().orElse(false);
-        return (isProd ? "veilarbportefolje-prod-readonly" : "veilarbportefolje-dev-readonly");
-    }
-
-    @SneakyThrows
-    private static DataSource createVaultRefreshDataSource(HikariConfig config, DbRole role) {
-        if (role.equals(DbRole.READONLY)) {
-            return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, getMountPath(), getSqlReadOnlyRole());
-        }
-        return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, getMountPath(), getSqlAdminRole());
-    }
-
-    private static String getMountPath() {
-        boolean isProd = isProduction().orElse(false);
-        return "postgresql/" + (isProd ? "prod-fss" : "preprod-fss");
     }
 
     public static String boolToJaNei(boolean bool) {
