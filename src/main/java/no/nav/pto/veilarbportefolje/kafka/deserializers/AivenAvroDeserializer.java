07c5bc84673eefbae1bd6522ed6c56a218af09d1
Dragutin Vujovic <dragutin.vujovic@nav.no>
2024-08-05 12:34:21 +0200

    Update kafka schema registry url

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/AivenAvroDeserializer.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/AivenAvroDeserializer.java
@@ -2,14 +2,18 @@ package no.nav.pto.veilarbportefolje.kafka.deserializers;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import no.nav.common.kafka.consumer.util.deserializer.Deserializers;
+import no.nav.common.utils.EnvironmentUtils;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import java.util.Map;
 
 public class AivenAvroDeserializer<T> {
+    private static final String KAFKA_SCHEMAS_URL = EnvironmentUtils.getRequiredProperty("KAFKA_SCHEMA_REGISTRY");
+
     public Deserializer<T> getDeserializer() {
         Deserializer<T> avroDeserializer = Deserializers.aivenAvroDeserializer();
-        avroDeserializer.configure(Map.of(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true), false);
+        avroDeserializer.configure(Map.of(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, KAFKA_SCHEMAS_URL,
+                KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true), false);
         return avroDeserializer;
     }
 }
