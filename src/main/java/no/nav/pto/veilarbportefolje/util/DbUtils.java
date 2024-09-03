5e0444d3804d21cea4a65a0cf139a119ec8b5074
Dragutin Vujovic <dragutin.vujovic@nav.no>
2024-09-03 09:07:13 +0200

    Increse pool size

--- src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
+++ src/main/java/no/nav/pto/veilarbportefolje/util/DbUtils.java
@@ -11,7 +11,7 @@ public class DbUtils {
 
     public static DataSource createDataSource(String dbUrl) {
         try {
-            HikariConfig config = createDataSourceConfig(dbUrl, 7);
+            HikariConfig config = createDataSourceConfig(dbUrl, 15);
             return new HikariDataSource(config);
         } catch (Exception e) {
             log.info("Can't connect to db, error: " + e, e);
