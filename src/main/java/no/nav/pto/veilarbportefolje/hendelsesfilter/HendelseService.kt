a8fd9f09e9d6d4178a7cc0bcc52d57be39437bf8
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-26 21:24:38 +0100

    Bruk "HendelseService" i consumer-configen og legg til logglinjer for testing

--- src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseService.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseService.kt
@@ -1,28 +1,33 @@
 package no.nav.pto.veilarbportefolje.hendelsesfilter
 
 import no.nav.pto.veilarbportefolje.kafka.KafkaCommonKeyedConsumerService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
 
+@Service
 class HendelseService : KafkaCommonKeyedConsumerService<HendelseRecordValue>() {
+    val log = LoggerFactory.getLogger(HendelseService::class.java)
+
     override fun behandleKafkaMeldingLogikk(kafkaMelding: HendelseRecordValue, nokkel: String) {
         val hendelse = toHendelse(kafkaMelding, nokkel)
         // TODO: Handtere eventuelle feil i mapping
 
-        when(kafkaMelding.operasjon) {
-            Operasjon.START -> { startHendelse(hendelse) }
-            Operasjon.ENDRING -> { oppdaterHendelse(hendelse)}
-            Operasjon.STOPP -> { stoppHendelse(hendelse) }
+        when (kafkaMelding.operasjon) {
+            Operasjon.START -> startHendelse(hendelse)
+            Operasjon.ENDRING -> oppdaterHendelse(hendelse)
+            Operasjon.STOPP -> stoppHendelse(hendelse)
         }
     }
 
     private fun startHendelse(hendelse: Hendelse) {
-        TODO("Not implemented yet")
+        log.info("Starter hendelse med id ${hendelse.id}.")
     }
 
     private fun oppdaterHendelse(hendelse: Hendelse) {
-        TODO("Not yet implemented")
+        log.info("Oppdaterer hendelse med id ${hendelse.id}.")
     }
 
     private fun stoppHendelse(hendelse: Hendelse) {
-        TODO("Not yet implemented")
+        log.info("Sletter hendelse med id ${hendelse.id}.")
     }
 }
