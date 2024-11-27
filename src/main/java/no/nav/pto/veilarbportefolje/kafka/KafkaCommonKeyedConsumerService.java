09b494dc2fa9ec8c3ebc6940c89f7905ddb6b3eb
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-27 12:41:15 +0100

    Fjern unødvendig import

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonKeyedConsumerService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonKeyedConsumerService.java
@@ -2,8 +2,6 @@ package no.nav.pto.veilarbportefolje.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import static no.nav.pto.veilarbportefolje.util.SecureLog.secureLog;
-
 public abstract class KafkaCommonKeyedConsumerService<T> extends KafkaCommonConsumerService<T> {
 
     @Override
