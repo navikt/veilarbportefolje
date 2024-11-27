f4302d51bc6f1833f73f01b7ac5dc20f495f153b
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-27 12:31:59 +0100

    Rename klasse for å signalisere at den ikkje brukar Kafka-key ifm. behandling av melding

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonNonKeyedConsumerService.java
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/KafkaCommonNonKeyedConsumerService.java
@@ -6,7 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import static no.nav.pto.veilarbportefolje.util.SecureLog.secureLog;
 
 @Slf4j
-public abstract class KafkaCommonConsumerService<T> implements IKafkaCommonConsumerService<T> {
+public abstract class KafkaCommonNonKeyedConsumerService<T> implements IKafkaCommonConsumerService<T> {
     public void behandleKafkaRecord(ConsumerRecord<String, T> kafkaMelding) {
         secureLog.info(
                 "Behandler kafka-melding med key: {} og offset: {}, og partition: {} på topic {}",
