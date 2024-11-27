25d00cef8b929f13e4ecd5a635cda9f0bf359dfa
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-27 14:10:44 +0100

    Legg på sjekk for om brukar er under oppfølging

--- src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseService.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseService.kt
@@ -1,6 +1,8 @@
 package no.nav.pto.veilarbportefolje.hendelsesfilter
 
 import no.nav.pto.veilarbportefolje.kafka.KafkaCommonKeyedConsumerService
+import no.nav.pto.veilarbportefolje.persononinfo.PdlIdentRepository
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -8,14 +10,23 @@ import org.springframework.stereotype.Service
 @Service
 class HendelseService(
     @Autowired private val hendelseRepository: HendelseRepository,
+    private val pdlIdentRepository: PdlIdentRepository,
 ) : KafkaCommonKeyedConsumerService<HendelseRecordValue>() {
-    val log = LoggerFactory.getLogger(HendelseService::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(HendelseService::class.java)
 
     override fun behandleKafkaRecordLogikk(hendelseRecordValue: HendelseRecordValue, nokkel: String) {
+        val operasjon = hendelseRecordValue.operasjon
         val hendelse = toHendelse(hendelseRecordValue, nokkel)
         // TODO: Handtere eventuelle feil i mapping
 
-        when (hendelseRecordValue.operasjon) {
+        val isUnderArbeidsrettetOppfolging = pdlIdentRepository.erBrukerUnderOppfolging(hendelse.personIdent.get())
+
+        if (!isUnderArbeidsrettetOppfolging) {
+            logger.info("Fikk melding på bruker som ikke er under oppfølging. Ignorerer melding.")
+            return
+        }
+
+        when (operasjon) {
             Operasjon.START -> startHendelse(hendelse)
             Operasjon.OPPDATER -> oppdaterHendelse(hendelse)
             Operasjon.STOPP -> stoppHendelse(hendelse)
@@ -24,16 +35,16 @@ class HendelseService(
 
     private fun startHendelse(hendelse: Hendelse) {
         hendelseRepository.upsert(hendelse)
-        log.info("Hendelse med id ${hendelse.id} ble startet")
+        logger.info("Hendelse med id ${hendelse.id} ble startet")
     }
 
     private fun oppdaterHendelse(hendelse: Hendelse) {
         hendelseRepository.upsert(hendelse)
-        log.info("Hendelse med id ${hendelse.id} ble oppdatert")
+        logger.info("Hendelse med id ${hendelse.id} ble oppdatert")
     }
 
     private fun stoppHendelse(hendelse: Hendelse) {
         hendelseRepository.delete(hendelse.id)
-        log.info("Hendelse med id ${hendelse.id} ble stoppet")
+        logger.info("Hendelse med id ${hendelse.id} ble stoppet")
     }
 }
