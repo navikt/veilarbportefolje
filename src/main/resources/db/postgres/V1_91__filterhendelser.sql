4a48fb1a3ce6d5ec0e5ce963aa311f1cd146eaa0
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-11-27 13:26:24 +0100

    Rename til "personIdent" då "ident" synest å vere navnekonvensjon for ID-ar som representerer ein person

--- src/main/resources/db/postgres/V1_91__filterhendelser.sql
+++ src/main/resources/db/postgres/V1_91__filterhendelser.sql
@@ -1,7 +1,7 @@
 CREATE TABLE FILTERHENDELSER
 (
     id                uuid         not null primary key,
-    personID          varchar(11)  not null,
+    person_ident      varchar(11)  not null,
 
     hendelse_navn     varchar(100) not null,
     hendelse_dato     timestamp    not null,
@@ -14,4 +14,4 @@ CREATE TABLE FILTERHENDELSER
     sist_endret       timestamp    not null  --  Tidspunktet rada vart endra, lik som oppdateringstidspunkt som standard
 );
 
-CREATE INDEX IDX_FILTERHENDELSER_IDENT on FILTERHENDELSER (personID);
+CREATE INDEX IDX_FILTERHENDELSER_PERSONIDENT on FILTERHENDELSER (person_ident);
