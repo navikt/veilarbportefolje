e8fa466cc2ce468e28c670de66b3cb61d663e2ff
Sondre Larsen Ovrid <sondre.ovrid@bekk.no>
2025-01-22 11:46:17 +0100

    Bytt payload-typen for nokre endpunkt som ikkje klarte å deserialisere JSON string-nodes riktig

---
+++ src/main/java/no/nav/pto/veilarbportefolje/admin/v1/SlettOppfolgingsbrukerRequest.java
@@ -0,0 +1,8 @@
+package no.nav.pto.veilarbportefolje.admin.v1;
+
+import no.nav.common.types.identer.AktorId;
+
+public record SlettOppfolgingsbrukerRequest(
+        AktorId aktorId
+) {
+}
--- src/main/java/no/nav/pto/veilarbportefolje/admin/v2/AdminV2Controller.java
+++ src/main/java/no/nav/pto/veilarbportefolje/admin/v2/AdminV2Controller.java
@@ -12,7 +12,10 @@ import no.nav.pto.veilarbportefolje.auth.DownstreamApi;
 import no.nav.pto.veilarbportefolje.domene.AktorClient;
 import no.nav.pto.veilarbportefolje.opensearch.OpensearchIndexer;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import static no.nav.pto.veilarbportefolje.auth.AuthUtils.erSystemkallFraAzureAd;
