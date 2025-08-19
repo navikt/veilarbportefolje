fe20a998146a74af5e1e483047d5de657686c023
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2025-08-19 10:28:24 +0200

    Fjern logging for ingen hendelse (#1964)
    * Fjern logging for ingen hendelse

--- src/main/java/no/nav/pto/veilarbportefolje/postgres/PostgresOpensearchMapper.java
+++ src/main/java/no/nav/pto/veilarbportefolje/postgres/PostgresOpensearchMapper.java
@@ -261,7 +261,7 @@ public class PostgresOpensearchMapper {
                 Hendelse eldsteHendelsePaPerson = hendelseRepository.getEldste(NorskIdent.of(bruker.getFnr()), Kategori.UTGATT_VARSEL);
                 bruker.setUtgatt_varsel(eldsteHendelsePaPerson.getHendelse());
             } catch (IngenHendelseForPersonException ex) {
-                log.info("Fant ingen hendelse/utgått varsel for person, så ingen data å flette inn.");
+                // Ingen hendelse funnet for personen, men unødvendig med logging
             }
         });
     }
