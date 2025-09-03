bdf2a47d3eac1ef03030e5009f5a85b3267c970e
Kristina Skatun <kristinaskatun@gmail.com>
2025-09-03 11:02:20 +0200

    Legg til sjekk på om aap periode er i oppfølgingsperiode
    (cherry picked from commit 0be2f40fede158b53939acf33655f645a14a49c6)

--- src/main/java/no/nav/pto/veilarbportefolje/aap/AapController.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/aap/AapController.kt
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*
     description = "Aap"
 )
 class AapController(
-    val aapClient: AapClient
+    val aapClient: AapClient,
+    private val aapService: AapService
 ) {
 
     @PostMapping("/hent-aap-sak")
@@ -22,13 +23,21 @@ class AapController(
     }
 
     @PostMapping("/hent-aap-vedtak")
-    fun hentAapMaximum(@RequestBody request: AapVedtakRequest): AapVedtakResponseDto {
+    fun hentAapVedtak(@RequestBody request: AapVedtakRequest): AapVedtakResponseDto {
         return aapClient.hentAapVedtak(
             personnr = request.personidentifikator,
             fom = request.fraOgMedDato,
             tom = request.tilOgMedDato
         )
     }
+
+    @PostMapping("/hent-aap-vedtak-for-oppfolging-periode")
+    fun hentAapVedtakForOppfolgingPeriode(@RequestBody request: AapVedtakRequest): AapVedtakResponseDto {
+        return aapService.hentAapVedtakForOppfolgingPeriode(
+            personIdent = request.personidentifikator
+        )
+    }
+
 
 }
 
