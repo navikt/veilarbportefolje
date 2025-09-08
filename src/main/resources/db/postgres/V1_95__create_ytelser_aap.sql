76b270808de08947981e31ddc3edd428690dc7cc
Kristina Skatun <kristinaskatun@gmail.com>
2025-09-08 17:18:29 +0200

    Drop table for deploy å fungere

--- src/main/resources/db/postgres/V1_95__create_ytelser_aap.sql
+++ src/main/resources/db/postgres/V1_95__create_ytelser_aap.sql
@@ -1,13 +1,15 @@
-CREATE TABLE YTELSER_AAP
-(
-    norsk_ident     varchar(11)  not null,
-    status          varchar(100) not null,
-    saksid          varchar(100) not null,
-    nyeste_periode_fom     date,
-    nyeste_periode_tom     date,
-    rettighetstype  varchar(100),
-    opphorsaarsak   varchar(100),
-    rad_sist_endret timestamp    not null,
+-- CREATE TABLE YTELSER_AAP
+-- (
+--     norsk_ident     varchar(11)  not null,
+--     status          varchar(100) not null,
+--     saksid          varchar(100) not null,
+--     nyeste_periode_fom     date,
+--     nyeste_periode_tom     date,
+--     rettighetstype  varchar(100),
+--     opphorsaarsak   varchar(100),
+--     rad_sist_endret timestamp    not null,
+--
+--     PRIMARY KEY (norsk_ident)
+-- );
 
-    PRIMARY KEY (norsk_ident)
-);
+DROP TABLE YTELSER_AAP;
