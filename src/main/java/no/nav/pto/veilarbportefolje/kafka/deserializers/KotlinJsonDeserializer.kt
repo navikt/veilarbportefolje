46532cac42de0e5dfc8bd258637f00026ab4cb7d
Kristina Skatun <kristinaskatun@gmail.com>
2025-10-06 13:54:48 +0200

    Rett opp navn

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/KotlinJsonDeserializer.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/KotlinJsonDeserializer.kt
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.apache.kafka.common.serialization.Deserializer
 
-class KotlinJsonDeserializer<T>(private val clazz: Class<T>) : Deserializer<T> {
+class KotlinJsonDeserializer<T>(private val klasse: Class<T>) : Deserializer<T> {
     private val objectMapper = ObjectMapper().registerKotlinModule()
 
     override fun deserialize(topic: String?, data: ByteArray?): T? {
         if (data == null) return null
-        return objectMapper.readValue(data, clazz)
+        return objectMapper.readValue(data, klasse)
     }
 }
