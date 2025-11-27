13630624196b7b59a7a0d6fd487554b8abea8b75
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2025-11-27 15:25:30 +0100

    Oppdater dialognavn til å ha med "venterPa" i variablene (#2115)

--- src/main/java/no/nav/pto/veilarbportefolje/domene/frontendmodell/MeldingerVenterPaSvar.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/frontendmodell/MeldingerVenterPaSvar.kt
@@ -3,6 +3,6 @@ package no.nav.pto.veilarbportefolje.domene.frontendmodell
 import java.time.LocalDate
 
 data class MeldingerVenterPaSvar(
-    val datoMeldingFraNav: LocalDate?,
-    val datoMeldingFraBruker: LocalDate?,
+    val datoMeldingVenterPaNav: LocalDate?,
+    val datoMeldingVenterPaBruker: LocalDate?,
 )
