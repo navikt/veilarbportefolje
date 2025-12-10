a316068a7920dc87a37dd68eb662c1328db5be78
Kristina Skatun <kristinaskatun@gmail.com>
2025-12-10 14:44:51 +0100

    Gjør to variabler til not nullable

--- src/main/java/no/nav/pto/veilarbportefolje/domene/frontendmodell/SisteEndringAvBruker.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/domene/frontendmodell/SisteEndringAvBruker.kt
@@ -3,7 +3,7 @@ package no.nav.pto.veilarbportefolje.domene.frontendmodell
 import java.time.LocalDate
 
 data class SisteEndringAvBruker(
-    var kategori: String?,
-    var tidspunkt: LocalDate?,
+    var kategori: String,
+    var tidspunkt: LocalDate,
     var aktivitetId: String?,
 )
