1794dc9f254f8bc9be33b1a9d2547e3b636e663d
Ingrid Fosså <ingrid.fossa@nav.no>
2026-01-05 16:12:03 +0100

    Trekk ut undertestar til eigne testfunksjonar for betre lesbarheit

--- src/test/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchServiceIntTolkesprakTest.kt
+++ src/test/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchServiceIntTolkesprakTest.kt
@@ -1,6 +1,7 @@
 package no.nav.pto.veilarbportefolje.opensearch
 
-import no.nav.pto.veilarbportefolje.domene.*
+import no.nav.pto.veilarbportefolje.domene.Sorteringsfelt
+import no.nav.pto.veilarbportefolje.domene.Sorteringsrekkefolge
 import no.nav.pto.veilarbportefolje.domene.filtervalg.Filtervalg
 import no.nav.pto.veilarbportefolje.domene.frontendmodell.PortefoljebrukerFrontendModell
 import no.nav.pto.veilarbportefolje.opensearch.domene.PortefoljebrukerOpensearchModell
@@ -25,6 +26,10 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
     private lateinit var TESTENHET: String
     private lateinit var TESTVEILEDER: String
 
+    private val tolkesprakJapansk = "JPN"
+    private val tolkesprakSvensk = "SWE"
+    private val tolkesprakNorsk = "NB"
+
     @BeforeEach
     fun setup() {
         TESTENHET = randomNavKontor().value
@@ -32,11 +37,124 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
     }
 
     @Test
-    fun skal_hente_alle_brukere_som_har_tolkbehov() {
-        val tolkesprakJapansk = "JPN"
-        val tolkesprakSvensk = "SWE"
-        val tolkesprakNorsk = "NB"
+    fun `Skal hente alle brukere med talespråktolk`() {
+        // Given
+        val trengerTalespraktolkSistOppdatert = "2022-02-22"
+        val trengerTalespraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakJapansk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTalespraktolkSistOppdatert))
 
+        val trengerTaleOgTegnspraktolkSistOppdatert = "2021-03-23"
+        val trengerTaleOgTegnspraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakSvensk)
+            .setTegnspraaktolk(tolkesprakSvensk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTaleOgTegnspraktolkSistOppdatert))
+
+        val brukerMedAndreTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTegnspraaktolk(tolkesprakNorsk)
+
+        val brukerUtenTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(null)
+            .setTegnspraaktolk(null)
+
+        val brukere = listOf(
+            trengerTalespraktolk,
+            trengerTaleOgTegnspraktolk,
+            brukerMedAndreTolkebehov,
+            brukerUtenTolkebehov,
+        )
+
+        skrivBrukereTilTestindeks(brukere)
+        OpensearchTestClient.pollOpensearchUntil { opensearchTestClient.countDocuments() == brukere.size }
+
+        // When
+        val filtrerPaTalespraktolk = Filtervalg()
+            .setFerdigfilterListe(listOf())
+            .setTolkebehov(listOf("TALESPRAAKTOLK"))
+
+        val response = opensearchService.hentBrukere(
+            TESTENHET,
+            Optional.empty(),
+            Sorteringsrekkefolge.STIGENDE,
+            Sorteringsfelt.IKKE_SATT,
+            filtrerPaTalespraktolk,
+            null,
+            null
+        )
+
+        // Then
+        assertThat(response.antall).isEqualTo(2)
+        assertTrue(
+            response.brukere.stream()
+                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakJapansk }
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkSistOppdatert })
+        assertTrue(
+            response.brukere.stream()
+                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakSvensk }
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTaleOgTegnspraktolkSistOppdatert })
+    }
+
+    @Test
+    fun `Skal hente alle brukere med tegnspråktolk`() {
+        // Given
+        val trengerTegnspraktolkBehovSistOppdatert = "2023-03-24"
+        val trengerTegnspraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTegnspraaktolk(tolkesprakNorsk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTegnspraktolkBehovSistOppdatert))
+
+        val trengerTaleOgTegnspraktolkBehovSistOppdatert = "2021-03-23"
+        val trengerTaleOgTegnspraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakSvensk)
+            .setTegnspraaktolk(tolkesprakSvensk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTaleOgTegnspraktolkBehovSistOppdatert))
+
+        val brukerUtenTegnspraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakJapansk)
+
+        val brukerUtenTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(null)
+            .setTegnspraaktolk(null)
+
+        val brukere = listOf(
+            trengerTegnspraktolk,
+            trengerTaleOgTegnspraktolk,
+            brukerUtenTegnspraktolk,
+            brukerUtenTolkebehov,
+        )
+
+        skrivBrukereTilTestindeks(brukere)
+        OpensearchTestClient.pollOpensearchUntil { opensearchTestClient.countDocuments() == brukere.size }
+
+        // When
+        val filtrerPaTegnspraktolk = Filtervalg()
+            .setFerdigfilterListe(listOf())
+            .setTolkebehov(listOf("TEGNSPRAAKTOLK"))
+
+        val responsBrukereMedTegnspraktolk = opensearchService.hentBrukere(
+            TESTENHET,
+            Optional.empty(),
+            Sorteringsrekkefolge.STIGENDE,
+            Sorteringsfelt.IKKE_SATT,
+            filtrerPaTegnspraktolk,
+            null,
+            null
+        )
+
+        // Then
+        assertThat(responsBrukereMedTegnspraktolk.antall).isEqualTo(2)
+        assertTrue(
+            responsBrukereMedTegnspraktolk.brukere.stream()
+                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.tegnspraaktolk == tolkesprakSvensk }
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTaleOgTegnspraktolkBehovSistOppdatert })
+        assertTrue(
+            responsBrukereMedTegnspraktolk.brukere.stream()
+                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.tegnspraaktolk == tolkesprakNorsk }
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTegnspraktolkBehovSistOppdatert })
+    }
+
+    @Test
+    fun `Skal hente alle brukere med tale- eller tegnspråktolk`() {
+        // Given
         val trengerTalespraktolkBehovSistOppdatert = "2022-02-22"
         val trengerTalespraktolk = genererRandomBruker(TESTENHET, TESTVEILEDER)
             .setTalespraaktolk(tolkesprakJapansk)
@@ -53,78 +171,21 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
             .setTegnspraaktolk(tolkesprakNorsk)
             .setTolkBehovSistOppdatert(LocalDate.parse(trengerTegnspraktolkBehovSistOppdatert))
 
-        val brukerUtenTolkebehov1 = genererRandomBruker(TESTENHET, TESTVEILEDER)
+        val brukerUtenTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
             .setTalespraaktolk(null)
             .setTegnspraaktolk(null)
-
-        val brukerUtenTolkebehov2 = genererRandomBruker(TESTENHET, TESTVEILEDER)
-            .setTalespraaktolk("")
-            .setTegnspraaktolk("")
 
         val brukere = listOf(
             trengerTalespraktolk,
             trengerTaleOgTegnspraktolk,
             trengerTegnspraktolk,
-            brukerUtenTolkebehov1,
-            brukerUtenTolkebehov2
+            brukerUtenTolkebehov,
         )
 
         skrivBrukereTilTestindeks(brukere)
         OpensearchTestClient.pollOpensearchUntil { opensearchTestClient.countDocuments() == brukere.size }
 
-
-        /* Skal hente alle med talespråktolk */
-        val filtrerPaTalespraktolk = Filtervalg()
-            .setFerdigfilterListe(listOf())
-            .setTolkebehov(listOf("TALESPRAAKTOLK"))
-
-        val responsBrukereMedTalespraktolk = opensearchService.hentBrukere(
-            TESTENHET,
-            Optional.empty(),
-            Sorteringsrekkefolge.STIGENDE,
-            Sorteringsfelt.IKKE_SATT,
-            filtrerPaTalespraktolk,
-            null,
-            null
-        )
-
-        assertThat(responsBrukereMedTalespraktolk.antall).isEqualTo(2)
-        assertTrue(
-            responsBrukereMedTalespraktolk.brukere.stream()
-                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakJapansk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkBehovSistOppdatert })
-        assertTrue(
-            responsBrukereMedTalespraktolk.brukere.stream()
-                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakSvensk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTaleOgTegnspraktolkBehovSistOppdatert })
-
-
-        /* Skal hente alle med tegnspråktolk */
-        val filtrerPaTegnspraktolk = Filtervalg()
-            .setFerdigfilterListe(listOf())
-            .setTolkebehov(listOf("TEGNSPRAAKTOLK"))
-
-        val responsBrukereMedTegnspraktolk = opensearchService.hentBrukere(
-            TESTENHET,
-            Optional.empty(),
-            Sorteringsrekkefolge.STIGENDE,
-            Sorteringsfelt.IKKE_SATT,
-            filtrerPaTegnspraktolk,
-            null,
-            null
-        )
-        assertThat(responsBrukereMedTegnspraktolk.antall).isEqualTo(2)
-        assertTrue(
-            responsBrukereMedTegnspraktolk.brukere.stream()
-                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.tegnspraaktolk == tolkesprakSvensk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTaleOgTegnspraktolkBehovSistOppdatert })
-        assertTrue(
-            responsBrukereMedTegnspraktolk.brukere.stream()
-                .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.tegnspraaktolk == tolkesprakNorsk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTegnspraktolkBehovSistOppdatert })
-
-
-        /* Skal hente alle med tegn- eller talespråktolk */
+        // When
         val filtrePaBadeTegnOgTalespraktolk = Filtervalg()
             .setFerdigfilterListe(listOf())
             .setTolkebehov(listOf("TEGNSPRAAKTOLK", "TALESPRAAKTOLK"))
@@ -139,6 +200,7 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
             null
         )
 
+        // Then
         assertThat(responsBrukereMedTegnEllerTalespraktolk.antall).isEqualTo(3)
         assertTrue(
             responsBrukereMedTegnEllerTalespraktolk.brukere.stream()
@@ -150,8 +212,41 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
             responsBrukereMedTegnEllerTalespraktolk.brukere.stream()
                 .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTegnspraktolkBehovSistOppdatert })
 
+    }
 
-        /* Skal hente alle med japansk som tegn- eller talespråk */
+    @Test
+    fun `Skal hente alle brukere med tegn- eller talespråktolkebehov for japansk`() {
+        // Given
+        val trengerTalespraktolkJapanskBehovSistOppdatert = "2022-02-22"
+        val trengerTalespraktolkJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakJapansk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTalespraktolkJapanskBehovSistOppdatert))
+
+        val trengerTaleOgTegnspraktolkMenIkkeJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakSvensk)
+            .setTegnspraaktolk(tolkesprakSvensk)
+            .setTolkBehovSistOppdatert(LocalDate.parse("2021-03-23"))
+
+        val trengerTegnspraktolkMenIkkeJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTegnspraaktolk(tolkesprakNorsk)
+            .setTolkBehovSistOppdatert(LocalDate.parse("2023-03-24"))
+
+        val brukerUtenTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(null)
+            .setTegnspraaktolk(null)
+
+        val brukere = listOf(
+            trengerTalespraktolkJapansk,
+            trengerTaleOgTegnspraktolkMenIkkeJapansk,
+            trengerTegnspraktolkMenIkkeJapansk,
+            brukerUtenTolkebehov,
+        )
+
+
+        skrivBrukereTilTestindeks(brukere)
+        OpensearchTestClient.pollOpensearchUntil { opensearchTestClient.countDocuments() == brukere.size }
+
+        // When
         val filtrerPaTolkebehovJapansk = Filtervalg()
             .setFerdigfilterListe(listOf())
             .setTolkebehov(listOf("TEGNSPRAAKTOLK", "TALESPRAAKTOLK"))
@@ -166,14 +261,48 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
             null,
             null
         )
+
+        // Then
         assertThat(responsBrukereMedTolkebehovJapansk.antall).isEqualTo(1)
         assertTrue(
             responsBrukereMedTolkebehovJapansk.brukere.stream()
                 .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakJapansk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkBehovSistOppdatert })
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkJapanskBehovSistOppdatert })
+    }
 
 
-        /* Skal hente alle med japansk som tegn- eller talespråk, også når ingen tolkebehov er valgt */
+    @Test
+    fun `Skal hente alle brukere med tolkespråk japansk når det bare er filtret på tolkespråk`() {
+        // Given
+        val trengerTalespraktolkJapanskBehovSistOppdatert = "2022-02-22"
+        val trengerTalespraktolkJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakJapansk)
+            .setTolkBehovSistOppdatert(LocalDate.parse(trengerTalespraktolkJapanskBehovSistOppdatert))
+
+        val trengerTaleOgTegnspraktolkMenIkkeJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(tolkesprakSvensk)
+            .setTegnspraaktolk(tolkesprakSvensk)
+            .setTolkBehovSistOppdatert(LocalDate.parse("2021-03-23"))
+
+        val trengerTegnspraktolkMenIkkeJapansk = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTegnspraaktolk(tolkesprakNorsk)
+            .setTolkBehovSistOppdatert(LocalDate.parse("2023-03-24"))
+
+        val brukerUtenTolkebehov = genererRandomBruker(TESTENHET, TESTVEILEDER)
+            .setTalespraaktolk(null)
+            .setTegnspraaktolk(null)
+
+        val brukere = listOf(
+            trengerTalespraktolkJapansk,
+            trengerTaleOgTegnspraktolkMenIkkeJapansk,
+            trengerTegnspraktolkMenIkkeJapansk,
+            brukerUtenTolkebehov,
+        )
+
+        skrivBrukereTilTestindeks(brukere)
+        OpensearchTestClient.pollOpensearchUntil { opensearchTestClient.countDocuments() == brukere.size }
+
+        // When
         val filtrerPaKunTolkesprakJapansk = Filtervalg()
             .setFerdigfilterListe(listOf())
             .setTolkebehov(listOf())
@@ -188,11 +317,13 @@ class OpensearchServiceIntTolkesprakTest @Autowired constructor(
             null,
             null
         )
+
+        // Then
         assertThat(responsBrukereMedTolkesprakJapansk.antall).isEqualTo(1)
         assertTrue(
             responsBrukereMedTolkesprakJapansk.brukere.stream()
                 .filter { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.talespraaktolk == tolkesprakJapansk }
-                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkBehovSistOppdatert })
+                .anyMatch { x: PortefoljebrukerFrontendModell -> x.tolkebehov?.sistOppdatert.toString() == trengerTalespraktolkJapanskBehovSistOppdatert })
     }
 
     private fun genererRandomBruker(
