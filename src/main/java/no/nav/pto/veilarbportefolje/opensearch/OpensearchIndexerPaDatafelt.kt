cc293ae06a23131f3cf199a6d443aa279554d88d
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2026-01-06 22:12:43 +0100

    Utled feltnavn direkte frå properties i "PortefoljebrukerOpensearchModell"

--- src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchIndexerPaDatafelt.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchIndexerPaDatafelt.kt
@@ -6,11 +6,15 @@ import no.nav.pto.veilarbportefolje.arbeidssoeker.v2.OpplysningerOmArbeidssoeker
 import no.nav.pto.veilarbportefolje.arbeidssoeker.v2.ProfileringEntity
 import no.nav.pto.veilarbportefolje.arbeidssoeker.v2.mapTilUtdanning
 import no.nav.pto.veilarbportefolje.dialog.DialogdataDto
+import no.nav.pto.veilarbportefolje.domene.EnsligeForsorgereOvergangsstonad
 import no.nav.pto.veilarbportefolje.domene.HuskelappForBruker
 import no.nav.pto.veilarbportefolje.domene.VeilederId
 import no.nav.pto.veilarbportefolje.ensligforsorger.dto.output.EnsligeForsorgerOvergangsstønadTiltakDto
 import no.nav.pto.veilarbportefolje.hendelsesfilter.Hendelse
+import no.nav.pto.veilarbportefolje.hendelsesfilter.Hendelse.HendelseInnhold
 import no.nav.pto.veilarbportefolje.hendelsesfilter.Kategori
+import no.nav.pto.veilarbportefolje.opensearch.domene.Endring
+import no.nav.pto.veilarbportefolje.opensearch.domene.PortefoljebrukerOpensearchModell
 import no.nav.pto.veilarbportefolje.oppfolging.OppfolgingRepositoryV2
 import no.nav.pto.veilarbportefolje.oppfolgingsvedtak14a.gjeldende14aVedtak.GjeldendeVedtak14a
 import no.nav.pto.veilarbportefolje.sisteendring.SisteEndringDTO
@@ -453,71 +457,76 @@ class OpensearchIndexerPaDatafelt(
 }
 
 object FeltNavn {
-    const val ARBEIDSSOEKER_PROFILERING_RESULTAT = "profilering_resultat"
-    const val ARBEIDSSOEKER_BRUKERS_SITUASJONER = "brukers_situasjoner"
-    const val ARBEIDSSOEKER_UTDANNING = "utdanning"
-    const val ARBEIDSSOEKER_UTDANNING_BESTATT = "utdanning_bestatt"
-    const val ARBEIDSSOEKER_UTDANNING_GODKJENT = "utdanning_godkjent"
-    const val ARBEIDSSOEKER_UTDANNING_OG_SITUASJON_SIST_ENDRET = "utdanning_og_situasjon_sist_endret"
+    val ARBEIDSSOEKER_PROFILERING_RESULTAT = PortefoljebrukerOpensearchModell::profilering_resultat.name
+    val ARBEIDSSOEKER_BRUKERS_SITUASJONER = PortefoljebrukerOpensearchModell::brukers_situasjoner.name
+    val ARBEIDSSOEKER_UTDANNING = PortefoljebrukerOpensearchModell::utdanning.name
+    val ARBEIDSSOEKER_UTDANNING_BESTATT = PortefoljebrukerOpensearchModell::utdanning_bestatt.name
+    val ARBEIDSSOEKER_UTDANNING_GODKJENT = PortefoljebrukerOpensearchModell::utdanning_godkjent.name
+    val ARBEIDSSOEKER_UTDANNING_OG_SITUASJON_SIST_ENDRET =
+        PortefoljebrukerOpensearchModell::utdanning_og_situasjon_sist_endret.name
 
-    const val HUSKELAPP = "huskelapp"
-    const val HUSKELAPP_FRIST = "frist"
-    const val HUSKELAPP_KOMMENTAR = "kommentar"
-    const val HUSKELAPP_ENDRET_AV = "endretAv"
-    const val HUSKELAPP_ENDRET_DATO = "endretDato"
-    const val HUSKELAPP_HUSKELAPP_ID = "huskelappId"
-    const val HUSKELAPP_ENHET_ID = "enhetId"
-    const val FARGEKATEGORI = "fargekategori"
-    const val FARGEKATEGORI_ENHET_ID = "fargekategori_enhetId"
+    val HUSKELAPP = PortefoljebrukerOpensearchModell::huskelapp.name
+    val HUSKELAPP_FRIST = HuskelappForBruker::frist.name
+    val HUSKELAPP_KOMMENTAR = HuskelappForBruker::kommentar.name
+    val HUSKELAPP_ENDRET_AV = HuskelappForBruker::endretAv.name
+    val HUSKELAPP_ENDRET_DATO = HuskelappForBruker::endretDato.name
+    val HUSKELAPP_HUSKELAPP_ID = HuskelappForBruker::huskelappId.name
+    val HUSKELAPP_ENHET_ID = HuskelappForBruker::enhetId.name
+    val FARGEKATEGORI = PortefoljebrukerOpensearchModell::fargekategori.name
+    val FARGEKATEGORI_ENHET_ID = PortefoljebrukerOpensearchModell::fargekategori_enhetId.name
 
-    const val AKTIVITETER_SISTE_ENDRINGER = "siste_endringer"
-    const val AKTIVITETER_SISTE_ENDRINGER_ER_SETT = "erSett"
-    const val AKTIVITETER_SISTE_ENDRINGER_TIDSPUNKT = "tidspunkt"
-    const val AKTIVITETER_SISTE_ENDRINGER_AKTIVTETID = "aktivtetId"
+    val AKTIVITETER_SISTE_ENDRINGER = PortefoljebrukerOpensearchModell::siste_endringer.name
+    val AKTIVITETER_SISTE_ENDRINGER_ER_SETT =
+        "erSett" // Denne har ingen korresponderende property i PortefoljebrukerOpensearchModell
+    val AKTIVITETER_SISTE_ENDRINGER_TIDSPUNKT = Endring::tidspunkt.name
+    val AKTIVITETER_SISTE_ENDRINGER_AKTIVTETID = Endring::aktivtetId.name
 
-    const val CV_HAR_DELT_CV = "har_delt_cv"
-    const val CV_CV_EKSISTERE = "cv_eksistere"
+    val CV_HAR_DELT_CV = PortefoljebrukerOpensearchModell::har_delt_cv.name
+    val CV_CV_EKSISTERE = PortefoljebrukerOpensearchModell::cv_eksistere.name
 
-    const val OPPFOLGING_MANUELL_BRUKER = "manuell_bruker"
-    const val OPPFOLGING_VEILEDER_ID = "veileder_id"
-    const val OPPFOLGING_NY_FOR_VEILEDER = "ny_for_veileder"
-    const val OPPFOLGING_TILDELT_TIDSPUNKT = "tildelt_tidspunkt"
+    val OPPFOLGING_MANUELL_BRUKER = PortefoljebrukerOpensearchModell::manuell_bruker.name
+    val OPPFOLGING_VEILEDER_ID = PortefoljebrukerOpensearchModell::veileder_id.name
+    val OPPFOLGING_NY_FOR_VEILEDER = PortefoljebrukerOpensearchModell::ny_for_veileder.name
+    val OPPFOLGING_TILDELT_TIDSPUNKT = PortefoljebrukerOpensearchModell::tildelt_tidspunkt.name
 
-    const val DIALOG_VENTER_PA_SVAR_FRA_BRUKER = "venterpasvarfrabruker"
-    const val DIALOG_VENTER_PA_SVAR_FRA_NAV = "venterpasvarfranav"
+    val DIALOG_VENTER_PA_SVAR_FRA_BRUKER = PortefoljebrukerOpensearchModell::venterpasvarfrabruker.name
+    val DIALOG_VENTER_PA_SVAR_FRA_NAV = PortefoljebrukerOpensearchModell::venterpasvarfranav.name
 
-    const val EGEN_ANSATT = "egen_ansatt"
-    const val SKJERMET_TIL = "skjermet_til"
+    val EGEN_ANSATT = PortefoljebrukerOpensearchModell::egen_ansatt.name
+    val SKJERMET_TIL = PortefoljebrukerOpensearchModell::skjermet_til.name
 
-    const val ENSLIGE_FORSORGERE_OVERGANGSSTONAD = "enslige_forsorgere_overgangsstonad"
-    const val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_VEDTAKSPERIODETYPE = "vedtaksPeriodetype"
-    const val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_HARAKTIVITETSPLIKT = "harAktivitetsplikt"
-    const val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_UTLOPSDATO = "utlopsDato"
-    const val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_YNGSTEBARNSFØDSELSDATO = "yngsteBarnsFødselsdato"
+    val ENSLIGE_FORSORGERE_OVERGANGSSTONAD = PortefoljebrukerOpensearchModell::enslige_forsorgere_overgangsstonad.name
+    val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_VEDTAKSPERIODETYPE =
+        EnsligeForsorgereOvergangsstonad::vedtaksPeriodetype.name
+    val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_HARAKTIVITETSPLIKT =
+        EnsligeForsorgereOvergangsstonad::harAktivitetsplikt.name
+    val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_UTLOPSDATO = EnsligeForsorgereOvergangsstonad::utlopsDato.name
+    val ENSLIGE_FORSORGERE_OVERGANGSSTONAD_YNGSTEBARNSFØDSELSDATO =
+        EnsligeForsorgereOvergangsstonad::yngsteBarnsFødselsdato.name
 
-    const val TILTAKSHENDELSE = "tiltakshendelse"
-    const val TILTAKSHENDELSE_ID = "id"
-    const val TILTAKSHENDELSE_LENKE = "lenke"
-    const val TILTAKSHENDELSE_OPPRETTET = "opprettet"
-    const val TILTAKSHENDELSE_TEKST = "tekst"
-    const val TILTAKSHENDELSE_TILTAKSTYPE = "tiltakstype"
+    val TILTAKSHENDELSE = PortefoljebrukerOpensearchModell::tiltakshendelse.name
+    val TILTAKSHENDELSE_ID = Tiltakshendelse::id.name
+    val TILTAKSHENDELSE_LENKE = Tiltakshendelse::lenke.name
+    val TILTAKSHENDELSE_OPPRETTET = Tiltakshendelse::opprettet.name
+    val TILTAKSHENDELSE_TEKST = Tiltakshendelse::tekst.name
+    val TILTAKSHENDELSE_TILTAKSTYPE = Tiltakshendelse::tiltakstype.name
 
-    const val GJELDENDE_VEDTAK_14A = "gjeldendeVedtak14a"
-    const val GJELDENDE_VEDTAK_14A_INNSATSGRUPPE = "innsatsgruppe"
-    const val GJELDENDE_VEDTAK_14A_HOVEDMAL = "hovedmal"
-    const val GJELDENDE_VEDTAK_14A_FATTET_DATO = "fattetDato"
+    val GJELDENDE_VEDTAK_14A = PortefoljebrukerOpensearchModell::gjeldendeVedtak14a.name
+    val GJELDENDE_VEDTAK_14A_INNSATSGRUPPE = GjeldendeVedtak14a::innsatsgruppe.name
+    val GJELDENDE_VEDTAK_14A_HOVEDMAL = GjeldendeVedtak14a::hovedmal.name
+    val GJELDENDE_VEDTAK_14A_FATTET_DATO = GjeldendeVedtak14a::fattetDato.name
 
-    const val HENDELSER = "hendelser"
-    const val HENDELSER_BESKRIVELSE = "beskrivelse"
-    const val HENDELSER_DATO = "dato"
-    const val HENDELSER_LENKE = "lenke"
-    const val HENDELSER_DETALJER = "detaljer"
+    val HENDELSER = PortefoljebrukerOpensearchModell::hendelser.name
+    val HENDELSER_BESKRIVELSE = HendelseInnhold::beskrivelse.name
+    val HENDELSER_DATO = HendelseInnhold::dato.name
+    val HENDELSER_LENKE = HendelseInnhold::lenke.name
+    val HENDELSER_DETALJER = HendelseInnhold::detaljer.name
 
-    const val AAP_KELVIN = "aap_kelvin"
-    const val AAP_KELVIN_TOM_VEDTAKSDATO = "aap_kelvin_tom_vedtaksdato"
-    const val AAP_KELVIN_RETTIGHETSTYPE = "aap_kelvin_rettighetstype"
+    val AAP_KELVIN = PortefoljebrukerOpensearchModell::aap_kelvin.name
+    val AAP_KELVIN_TOM_VEDTAKSDATO = PortefoljebrukerOpensearchModell::aap_kelvin_tom_vedtaksdato.name
+    val AAP_KELVIN_RETTIGHETSTYPE = PortefoljebrukerOpensearchModell::aap_kelvin_rettighetstype.name
 
-    const val TILTAKSPENGER = "tiltakspenger"
-    const val TILTAKSPENGER_VEDTAKSDATO_TOM = "tiltakspenger_vedtaksdato_tom"
-    const val TILTAKSPENGER_RETTIGHET = "tiltakspenger_rettighet"
+    val TILTAKSPENGER = PortefoljebrukerOpensearchModell::tiltakspenger.name
+    val TILTAKSPENGER_VEDTAKSDATO_TOM = PortefoljebrukerOpensearchModell::tiltakspenger_vedtaksdato_tom.name
+    val TILTAKSPENGER_RETTIGHET = PortefoljebrukerOpensearchModell::tiltakspenger_rettighet.name
 }
