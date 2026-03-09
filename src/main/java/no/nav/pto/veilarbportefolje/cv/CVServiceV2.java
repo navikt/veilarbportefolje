bde24d2b796f57dbf1b97c15f6af4a5cb16b7a55
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-03-09 15:34:47 +0100

    Følg mønster for sletting av data når oppfølging avsluttes. Bruker funksjon i CVServiceV2 med beskyttelse mot manglende fnr

--- src/main/java/no/nav/pto/veilarbportefolje/cv/CVServiceV2.java
+++ src/main/java/no/nav/pto/veilarbportefolje/cv/CVServiceV2.java
@@ -16,7 +16,6 @@ import no.nav.pto.veilarbportefolje.persononinfo.PdlIdentRepository;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.Optional;
 
 import static no.nav.pto.veilarbportefolje.util.SecureLog.secureLog;
@@ -34,28 +33,44 @@ public class CVServiceV2 extends KafkaCommonNonKeyedConsumerService<Melding> {
     public void behandleKafkaMeldingLogikk(Melding kafkaMelding) {
         AktorId aktoerId = AktorId.of(String.valueOf(kafkaMelding.getAktoerId()));
         Fnr fnr = pdlIdentRepository.hentFnrForAktivBruker(aktoerId);
-        boolean cvEksisterer = cvEksistere(kafkaMelding);
+        boolean erMeldingstypeEndreEllerOpprett = erMeldingstypeEndreEllerOpprett(kafkaMelding);
 
         if (fnr != null) {
-            if (cvEksisterer) {
-                Optional<Instant> sistEndret = Optional.ofNullable(kafkaMelding.getEndreCv()).map(EndreCv::getCv).map(Cv::getSistEndret);
-                Timestamp cvSistEndret = sistEndret.map(Timestamp::from).orElse(null);
+            if (erMeldingstypeEndreEllerOpprett) {
+                Timestamp cvSistEndret = Optional.ofNullable(kafkaMelding.getEndreCv())
+                        .map(EndreCv::getCv)
+                        .map(Cv::getSistEndret)
+                        .map(Timestamp::from)
+                        .orElse(null);
                 secureLog.info("Oppdater CV eksisterer i BRUKER_REGISTRERT_CV tabell for bruker med aktoerid: {}, eksisterer: {}", aktoerId.get(), true);
-                cvRepositoryV2.upsertCVEksistererINyTabell(fnr, cvSistEndret, true);
+                cvRepositoryV2.upsertCvRegistrert(fnr, cvSistEndret, true);
             } else {
                 secureLog.info("Slett CV eksisterer fra BRUKER_REGISTRERT_CV for bruker med aktoerid: {}", aktoerId.get());
-                cvRepositoryV2.slettCvEksistererFraNyTabell(fnr);
+                cvRepositoryV2.slettCvRegistrert(fnr);
             }
         }else {
             secureLog.error("Bruker med aktoerid {} er ikke aktiv. FNR ikke funnet", aktoerId);
         }
 
         if (FeatureToggle.brukNyCvTabell(defaultUnleash)) {
-            opensearchIndexerPaDatafelt.updateCvEksistere(aktoerId, cvEksisterer);
+            opensearchIndexerPaDatafelt.updateCvEksistere(aktoerId, erMeldingstypeEndreEllerOpprett);
         }
     }
 
-    private boolean cvEksistere(Melding melding) {
+    public void slettCvData(AktorId aktorId, Optional<Fnr> maybeFnr) {
+        if (maybeFnr.isEmpty()) {
+            secureLog.warn("Kunne ikke slette CV-data for bruker med Aktør-ID {}. Årsak: fødselsnummer-parameter var tom.", aktorId.get());
+            return;
+        }
+
+        try {
+            cvRepositoryV2.slettCvRegistrert(maybeFnr.get());
+        } catch (Exception e) {
+            secureLog.error("Feil ved sletting av CV-data for bruker med fnr: {}", maybeFnr.get(), e);
+        }
+    }
+
+    private boolean erMeldingstypeEndreEllerOpprett(Melding melding) {
         return melding.getMeldingstype() == Meldingstype.ENDRE || melding.getMeldingstype() == Meldingstype.OPPRETT;
     }
 }
