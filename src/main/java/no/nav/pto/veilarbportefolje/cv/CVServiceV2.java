1ec9908fdd3653a4fbd552e63189861fbb07ccf8
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-03-09 15:47:48 +0100

    Fjern feature-flagg for ny tabell

--- src/main/java/no/nav/pto/veilarbportefolje/cv/CVServiceV2.java
+++ src/main/java/no/nav/pto/veilarbportefolje/cv/CVServiceV2.java
@@ -1,6 +1,5 @@
 package no.nav.pto.veilarbportefolje.cv;
 
-import io.getunleash.DefaultUnleash;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.arbeid.cv.avro.Cv;
@@ -9,7 +8,6 @@ import no.nav.arbeid.cv.avro.Melding;
 import no.nav.arbeid.cv.avro.Meldingstype;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
-import no.nav.pto.veilarbportefolje.config.FeatureToggle;
 import no.nav.pto.veilarbportefolje.kafka.KafkaCommonNonKeyedConsumerService;
 import no.nav.pto.veilarbportefolje.opensearch.OpensearchIndexerPaDatafelt;
 import no.nav.pto.veilarbportefolje.persononinfo.PdlIdentRepository;
@@ -27,7 +25,6 @@ public class CVServiceV2 extends KafkaCommonNonKeyedConsumerService<Melding> {
     private final OpensearchIndexerPaDatafelt opensearchIndexerPaDatafelt;
     private final CVRepositoryV2 cvRepositoryV2;
     private final PdlIdentRepository pdlIdentRepository;
-    private final DefaultUnleash defaultUnleash;
 
     @Override
     public void behandleKafkaMeldingLogikk(Melding kafkaMelding) {
@@ -48,13 +45,11 @@ public class CVServiceV2 extends KafkaCommonNonKeyedConsumerService<Melding> {
                 secureLog.info("Slett CV eksisterer fra BRUKER_REGISTRERT_CV for bruker med aktoerid: {}", aktoerId.get());
                 cvRepositoryV2.slettCvRegistrert(fnr);
             }
-        }else {
+        } else {
             secureLog.error("Bruker med aktoerid {} er ikke aktiv. FNR ikke funnet", aktoerId);
         }
 
-        if (FeatureToggle.brukNyCvTabell(defaultUnleash)) {
-            opensearchIndexerPaDatafelt.updateCvEksistere(aktoerId, erMeldingstypeEndreEllerOpprett);
-        }
+        opensearchIndexerPaDatafelt.updateCvEksistere(aktoerId, erMeldingstypeEndreEllerOpprett);
     }
 
     public void slettCvData(AktorId aktorId, Optional<Fnr> maybeFnr) {
