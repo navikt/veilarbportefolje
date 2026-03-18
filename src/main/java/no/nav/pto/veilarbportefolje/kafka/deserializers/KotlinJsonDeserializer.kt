073615d40ed64a7e076d988fc1c6e509dc749356
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-03-18 15:35:03 +0100

    Legg til V3 DTOer og deserialiseringslogikk for siste-oppfolgingsperiode-v3
    Co-authored-by: Mads Lee Giil <mads.lee.giil@nav.no>
    Co-authored-by: Sigurd Grøneng <sigurd.groneng@nav.no>

--- src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/KotlinJsonDeserializer.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/kafka/deserializers/KotlinJsonDeserializer.kt
@@ -1,11 +1,12 @@
 package no.nav.pto.veilarbportefolje.kafka.deserializers
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.apache.kafka.common.serialization.Deserializer
 
 class KotlinJsonDeserializer<T>(private val klasse: Class<T>) : Deserializer<T> {
-    private val objectMapper = ObjectMapper().registerKotlinModule()
+    private val objectMapper = ObjectMapper().registerKotlinModule().registerModule(JavaTimeModule())
 
     override fun deserialize(topic: String?, data: ByteArray?): T? {
         if (data == null) return null
