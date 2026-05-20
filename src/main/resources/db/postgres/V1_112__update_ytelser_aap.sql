5bb273ec7a606713c3efccc5b7feadf09d2fb923
Kristina Skatun <kristinaskatun@gmail.com>
2026-05-20 14:32:15 +0200

    Rydd opp flyway script

--- src/main/resources/db/postgres/V1_112__update_ytelser_aap.sql
+++ src/main/resources/db/postgres/V1_112__update_ytelser_aap.sql
@@ -1,6 +1,5 @@
 ALTER TABLE YTELSER_AAP
     ADD COLUMN IF NOT EXISTS maksdato date,
-    DROP COLUMN IF EXISTS sakstatus,
     ADD COLUMN IF NOT EXISTS sakstatus text,
     DROP COLUMN IF EXISTS opphorsaarsak;
 
