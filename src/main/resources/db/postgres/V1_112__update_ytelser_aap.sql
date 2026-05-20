4564e793f6f6799c2888f0793fc18c0c9734aed6
Kristina Skatun <kristinaskatun@gmail.com>
2026-05-20 14:11:28 +0200

    Endre typen på kolonne

--- src/main/resources/db/postgres/V1_112__update_ytelser_aap.sql
+++ src/main/resources/db/postgres/V1_112__update_ytelser_aap.sql
@@ -1,5 +1,6 @@
 ALTER TABLE YTELSER_AAP
     ADD COLUMN IF NOT EXISTS maksdato date,
-    ADD COLUMN IF NOT EXISTS sakstatus text,
+    DROP COLUMN IF EXISTS sakstatus,
+    --ADD COLUMN IF NOT EXISTS sakstatus text,
     DROP COLUMN IF EXISTS opphorsaarsak;
 
