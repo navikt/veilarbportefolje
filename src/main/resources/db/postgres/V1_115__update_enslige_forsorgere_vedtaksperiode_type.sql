ce005c5ed3fffcd7226cd4790701e11e0c22bf52
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2026-06-17 10:39:23 +0200

    Endring overgangsstonad (#2235)
    * Tester virkning av endrete regler for overgangsstønad
    
    * Tester virkning av endrete regler for overgangsstønad, kun en ny stønad
    
    * Legger til alle stønader og endrer etter innspill fra Kristina
    
    * Har funnet feil i automatisk oppdatering ved nye typer. Legger til logging når det kommer inn en ukjent vedtaksperiodetype.
    
    * Legg til flyways script for å legge til nye enums for periodetype
    
    ---------
    
    Co-authored-by: Kristina Skatun <kristinaskatun@gmail.com>

---
+++ src/main/resources/db/postgres/V1_115__update_enslige_forsorgere_vedtaksperiode_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO enslige_forsorgere_vedtaksperiode_type(periode_type)
+VALUES ('BARN_UNDER_14_MÅNEDER'),
+       ('SÆRLIG_TILSYNSKREVENDE_BARN'),
+       ('FORBIGÅENDE_SYKDOM_HOS_BARNET');
