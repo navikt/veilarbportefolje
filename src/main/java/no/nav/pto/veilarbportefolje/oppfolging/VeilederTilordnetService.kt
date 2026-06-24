8ebff75ac606a51441320493931de683f5f10dc4
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2026-06-24 13:05:07 +0200

    Fjerner en sjekk midlertidig

--- src/main/java/no/nav/pto/veilarbportefolje/oppfolging/VeilederTilordnetService.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/oppfolging/VeilederTilordnetService.kt
@@ -3,7 +3,7 @@ package no.nav.pto.veilarbportefolje.oppfolging
 import no.nav.common.types.identer.AktorId
 import no.nav.common.types.identer.Fnr
 import no.nav.pto.veilarbportefolje.domene.VeilederId
-import no.nav.pto.veilarbportefolje.domene.VeilederId.Companion.of
+//import no.nav.pto.veilarbportefolje.domene.VeilederId.Companion.of
 import no.nav.pto.veilarbportefolje.fargekategori.FargekategoriService
 import no.nav.pto.veilarbportefolje.huskelapp.HuskelappService
 import no.nav.pto.veilarbportefolje.kafka.KafkaCommonNonKeyedConsumerService
@@ -17,7 +17,7 @@ import java.util.*
 
 @Service
 class VeilederTilordnetService(
-    private val oppfolgingClient: OppfolgingClient,
+//    private val oppfolgingClient: OppfolgingClient,
     private val oppfolgingRepositoryV2: OppfolgingRepositoryV2,
     private val huskelappService: HuskelappService,
     private val fargekategoriService: FargekategoriService,
@@ -66,8 +66,8 @@ class VeilederTilordnetService(
 //        check(erUnderOppfolgingIVeilarboppfolging) { "Fikk 'veileder melding' på bruker som enda ikke er under oppfølging i veilarboppfolging" }
 //    }
 
-    private fun hentVeileder(aktoerId: AktorId?): VeilederId? {
-        return oppfolgingRepositoryV2.hentVeilederForBruker(aktoerId)
-            .orElse(of(null))
-    }
+//    private fun hentVeileder(aktoerId: AktorId?): VeilederId? {
+//        return oppfolgingRepositoryV2.hentVeilederForBruker(aktoerId)
+//            .orElse(of(null))
+//    }
 }
