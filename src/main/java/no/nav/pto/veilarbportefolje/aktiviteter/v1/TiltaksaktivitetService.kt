1d2427906934c9053ff0c2e7d4d36305c35f93f5
Sneha Desai <sneha.desai@nav.no>
2026-07-08 14:08:23 +0200

    Legger til cache på tiltakstype-filter

--- src/main/java/no/nav/pto/veilarbportefolje/aktiviteter/v1/TiltaksaktivitetService.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/aktiviteter/v1/TiltaksaktivitetService.kt
@@ -1,17 +1,29 @@
 package no.nav.pto.veilarbportefolje.aktiviteter.v1
 
+import com.github.benmanes.caffeine.cache.Caffeine
+import no.nav.common.client.utils.CacheUtils.tryCacheFirst
 import no.nav.common.types.identer.EnhetId
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 @Component
 class TiltaksaktivitetService(
     val tiltaksaktivitetRepository: TiltaksaktivitetRepository,
 ) {
+    private val enhetTiltakCachePostgres = Caffeine.newBuilder()
+        .expireAfterWrite(30, TimeUnit.MINUTES)
+        .maximumSize(1000)
+        .build<EnhetId,TiltakskodeMapping>()
+
     fun hentTiltakstyper(
         enhetId: EnhetId,
     ): TiltakskodeMapping {
-        return tiltaksaktivitetRepository.hentTiltakstyperForEnhet(enhetId)
+
+        return tryCacheFirst(enhetTiltakCachePostgres, enhetId) {
+            tiltaksaktivitetRepository.hentTiltakstyperForEnhet(enhetId)
+        }
+
     }
 }
