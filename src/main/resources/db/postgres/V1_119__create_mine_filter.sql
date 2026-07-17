841f6b68db022b2044e65478cfbc2c970387f6f8
Kristina Skatun <kristinaskatun@gmail.com>
2026-07-17 13:34:50 +0200

    Fjern table

--- src/main/resources/db/postgres/V1_119__create_mine_filter.sql
+++ src/main/resources/db/postgres/V1_119__create_mine_filter.sql
@@ -1,15 +1,19 @@
-CREATE TABLE LAGREDE_FILTER_MINE_FILTER
+drop table if exists LAGREDE_FILTER_MINE_FILTER;;
+
+/*CREATE TABLE LAGREDE_FILTER_MINE_FILTER
 (
-    FILTER_ID          SERIAL      not null,
-    VEILEDER_IDENT     VARCHAR(32) not null,
-    FILTER_NAVN        text        not null,
-    AKTIVE_FILTER_VALG jsonb       not null,
-    SORT_ORDER         integer     not null default 0,
-    OPPRETTET          timestamp   not null,
-    RAD_SIST_ENDRET    timestamp   not null,
+    FILTER_ID              SERIAL      not null,
+    VEILEDER_IDENT         VARCHAR(32) not null,
+    FILTER_NAVN            text        not null,
+    AKTIVE_FILTER_VALG     jsonb       not null,
+    SORT_ORDER             integer     not null default 0,
+    AKTIV                  boolean     not null default true,
+    IKKE_AKTIV_BESKRIVELSE text        not null default '',
+    OPPRETTET              timestamp   not null,
+    RAD_SIST_ENDRET        timestamp   not null,
 
     PRIMARY KEY (FILTER_ID)
 );
 
 CREATE INDEX MINE_FILTER_VEILEDER_IDENT_IDX
-    ON LAGREDE_FILTER_MINE_FILTER (VEILEDER_IDENT);
+    ON LAGREDE_FILTER_MINE_FILTER (VEILEDER_IDENT);*/
