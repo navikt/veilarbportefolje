0c9917a2419c943e72f7d85cdbf45ff70d98a542
Kristina Skatun <kristinaskatun@gmail.com>
2026-08-17 13:10:06 +0200

    Fjern utdatert deserializeringlogikk for koltin

--- src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseModell.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/hendelsesfilter/HendelseModell.kt
@@ -1,47 +1,25 @@
 package no.nav.pto.veilarbportefolje.hendelsesfilter
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.common.types.identer.NorskIdent
 import java.net.URL
 import java.time.ZonedDateTime
 import java.util.*
 
-/* Kafka-spesifikke typer */
-/*
-* Her må vi bruke en kombinasjon av @JsonCreator og @JsonProperty
-* siden vi bruker Kotlin data classes og ikke kontrollerer deserialiseringen som skjer ved konsumering
-* av Kafka-records. I skrivende stund er det kafka-modulen fra common-java-modules
-* som styrer dette, hvor også FasterXML Jackson er det underliggende biblioteket som brukes.
-* Vi kunne i stor grad ha unngått bruken av disse annotasjonene dersom vi hadde mulighet til å
-* registrere jackson-module-kotlin i object-mapperen som brukes i common-java-modules.
-* */
-data class HendelseRecordValue @JsonCreator constructor(
-    @JsonProperty("personID")
+data class HendelseRecordValue(
     val personID: NorskIdent,
-    @JsonProperty("avsender")
     val avsender: String,
-    @JsonProperty("kategori")
     val kategori: Kategori,
-    @JsonProperty("operasjon")
     val operasjon: Operasjon,
-    @JsonProperty(value = "hendelse")
     val hendelse: HendelseInnhold
 ) {
-    data class HendelseInnhold @JsonCreator constructor(
+    data class HendelseInnhold(
         // Det er produsent som må bestemme kobling mellom beskrivelse og beskrivelseEnum.
         // Førstnenvte er tekst som vises i frontend, og enum er for lettere sortering og filtrering i backend.
-        @JsonProperty("beskrivelse")
         val beskrivelse: String,
-        @JsonProperty("beskrivelseEnum")
         val beskrivelseEnum: String?,
-        @JsonProperty("dato")
         val dato: ZonedDateTime,
-        @JsonProperty("datoFrist")
         val datoFrist: ZonedDateTime?,
-        @JsonProperty("lenke")
         val lenke: URL,
-        @JsonProperty("detaljer")
         val detaljer: String?
     )
 }
@@ -58,30 +36,19 @@ enum class Operasjon {
     OPPDATER
 }
 
-data class Hendelse @JsonCreator constructor(
-    @JsonProperty("id")
+data class Hendelse(
     val id: UUID,
-    @JsonProperty("personIdent")
     val personIdent: NorskIdent,
-    @JsonProperty("avsender")
     val avsender: String,
-    @JsonProperty("kategori")
     val kategori: Kategori,
-    @JsonProperty("hendelse")
     val hendelse: HendelseInnhold
 ) {
-    data class HendelseInnhold @JsonCreator constructor(
-        @JsonProperty("beskrivelse")
+    data class HendelseInnhold(
         val beskrivelse: String,
-        @JsonProperty("beskrivelseEnum")
         val beskrivelseEnum: String?,
-        @JsonProperty("dato")
         val dato: ZonedDateTime,
-        @JsonProperty("datoFrist")
         val datoFrist: ZonedDateTime?,
-        @JsonProperty("lenke")
         val lenke: URL,
-        @JsonProperty("detaljer")
         val detaljer: String?
     )
 }
