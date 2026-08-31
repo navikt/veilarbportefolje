dc2e7ffe6cec00371dd6bdbde41f83d352cde512
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2026-08-31 10:47:34 +0200

    Fjern aktiv og beskrivelse variabler fra lagrede filter (#2296)

--- src/main/java/no/nav/pto/veilarbportefolje/lagredefilter/minefilter/MineFilterRepository.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/lagredefilter/minefilter/MineFilterRepository.kt
@@ -40,7 +40,7 @@ class MineFilterRepository(private val db: JdbcTemplate) {
                 $RAD_SIST_ENDRET
             )
             VALUES (?, ?, ?, 0, now(), now())
-            RETURNING $FILTER_ID, $FILTER_NAVN, $AKTIVE_FILTER_VALG, $SORT_ORDER, $AKTIV, $IKKE_AKTIV_BESKRIVELSE
+            RETURNING $FILTER_ID, $FILTER_NAVN, $AKTIVE_FILTER_VALG, $SORT_ORDER
         """.trimIndent()
 
         return db.query(
@@ -66,7 +66,7 @@ class MineFilterRepository(private val db: JdbcTemplate) {
                 $AKTIVE_FILTER_VALG = ?,
                 $RAD_SIST_ENDRET = now()
             WHERE $FILTER_ID = ? AND $VEILEDER_IDENT = ?
-            RETURNING $FILTER_ID, $FILTER_NAVN, $AKTIVE_FILTER_VALG, $SORT_ORDER, $AKTIV, $IKKE_AKTIV_BESKRIVELSE
+            RETURNING $FILTER_ID, $FILTER_NAVN, $AKTIVE_FILTER_VALG, $SORT_ORDER
         """.trimIndent()
 
         return db.query(
@@ -157,9 +157,7 @@ class MineFilterRepository(private val db: JdbcTemplate) {
             filterValg = rekonstruerFiltervalgFraAktive(
                 JsonUtils.fromJson(getString(AKTIVE_FILTER_VALG), AktiveFiltervalg::class.java)
             ),
-            sortOrder = getInt(SORT_ORDER),
-            aktiv = getBoolean(AKTIV),
-            ikkeAktivBeskrivelse = getString(IKKE_AKTIV_BESKRIVELSE)
+            sortOrder = getInt(SORT_ORDER)
         )
 
     private fun AktiveFiltervalg.toJsonb(): PGobject =
