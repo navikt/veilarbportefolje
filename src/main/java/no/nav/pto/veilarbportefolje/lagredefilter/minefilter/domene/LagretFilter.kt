00b7e938bf7b46a44d8254bc39daed0e161e0fee
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2026-09-15 13:48:51 +0200

    Ny kolonne for lagra filter med info om sletta filtervalg (#2306)

--- src/main/java/no/nav/pto/veilarbportefolje/lagredefilter/minefilter/domene/LagretFilter.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/lagredefilter/minefilter/domene/LagretFilter.kt
@@ -6,5 +6,6 @@ data class LagretFilter(
     val filterId: Int,
     val filterNavn: String,
     val filterValg: Filtervalg,
-    val sortOrder: Int
+    val sortOrder: Int,
+    val infoOmSlettetFiltervalg: List<String>? = null //Denne settes i gcp når migrering av lagra filtre sletter ett av flere filtervalg
 )
