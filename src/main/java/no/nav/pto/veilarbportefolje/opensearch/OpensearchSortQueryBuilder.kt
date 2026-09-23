704b277526556f0d9d7094ff4d1f14d0e43c2baf
Kristina Skatun <kristinaskatun@gmail.com>
2026-09-23 13:28:18 +0200

    Fjern fnutter

--- src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchSortQueryBuilder.kt
+++ src/main/java/no/nav/pto/veilarbportefolje/opensearch/OpensearchSortQueryBuilder.kt
@@ -58,6 +58,7 @@ import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.ENSLI
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.ENSLIGE_FORSORGERE_OVERGANGSSTONAD_UTLOPSDATO
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.ENSLIGE_FORSORGERE_OVERGANGSSTONAD_VEDTAKSPERIODETYPE
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.ENSLIGE_FORSORGERE_OVERGANGSSTONAD_YNGSTE_BARNS_FØDSELSDATO
+import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.TILTAKSPENGER_RETTIGHET
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.TILTAKSPENGER_VEDTAKSDATO_TOM
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.UNGDOMSPROGRAM
 import no.nav.pto.veilarbportefolje.opensearch.domene.DatafeltKeys.Ytelser.UNGDOMSPROGRAM_FRA_OG_MED
@@ -380,7 +381,7 @@ class OpensearchSortQueryBuilder {
             }
 
             Sorteringsfelt.TILTAKSPENGER_RETTIGHET -> {
-                searchSourceBuilder.sort("TILTAKSPENGER_RETTIGHET", sorteringsrekkefolgeOpenSearch)
+                searchSourceBuilder.sort(TILTAKSPENGER_RETTIGHET, sorteringsrekkefolgeOpenSearch)
                 searchSourceBuilder
             }
 
